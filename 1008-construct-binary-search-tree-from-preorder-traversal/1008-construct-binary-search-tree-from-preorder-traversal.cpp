fe1487147fba772c1db409b4a363b68cb5010847
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-11 07:52:44 +0530

    Time: 4 ms (81.84%), Space: 13.8 MB (48.96%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,29 +11,21 @@
  */
 class Solution { 
 private:
-   TreeNode* solve(vector<int>&pre, int ind,TreeNode* root)
-   { 
-       if(root==NULL)
-       {
-           TreeNode* temp=new TreeNode(pre[ind]); 
-           temp->left=NULL;
-           temp->right=NULL;
-           return temp;
-       }
-       if(pre[ind]<root->val)
-           root->left=solve(pre,ind,root->left);
-       else if(pre[ind]>root->val)
-           root->right=solve(pre,ind,root->right);
-       return root;
-       
-   }
+    TreeNode* solve(vector<int>& pre,int &ind,int upper)
+    {
+        if(ind==pre.size() || pre[ind]>upper)
+            return  NULL;
+        TreeNode*  root=new  TreeNode(pre[ind++]); 
+        root->left=solve(pre,ind,root->val);
+        root->right=solve(pre,ind,upper);
+        return root;
+    }
 public:
     TreeNode* bstFromPreorder(vector<int>& pre) {
         if(pre.size()==0)
-        return NULL; 
-        TreeNode*  root=NULL;
-        for(int i=0;i<pre.size();i++)
-        root=solve(pre,i,root);
-        return root;
+            return NULL; 
+        int upper=INT_MAX; 
+        int i=0;
+        return solve(pre,i,upper);
     }
 };
