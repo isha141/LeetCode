ee97919de1a75420e130688448a07b559dfd2f94
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-11 15:41:04 +0530

    Time: 202 ms (28.81%), Space: 61.2 MB (91.74%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -0,0 +1,33 @@
+class Solution { 
+    private:
+    int count(vector<int>& nums,int mid)
+    {
+        int c=0;
+        for(auto itr: nums)
+        {
+            if(itr<=mid)
+                c++;
+        }
+        return c;
+    }
+public:
+    int findDuplicate(vector<int>& nums) { 
+        int n=nums.size();
+        int low=1;
+        int high=n-1;  
+        int dup=-1;
+        while(low<high)
+        {
+            int mid=(low+high)/2; 
+            cout<<mid<<" ";
+            if(count(nums,mid)<=mid)
+            {  
+                low=mid+1;
+            }
+            else{ 
+                high=mid;
+            }
+        }
+        return low;
+    }
+};
