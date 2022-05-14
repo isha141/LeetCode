4ba1e20485cbdefac9d130a3843c14bb73d700a2
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-14 11:02:35 +0530

    Time: 43 ms (68.76%), Space: 19.8 MB (65.94%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,11 +1,25 @@
-class Solution {
+class Solution { 
+    private:
+    string counters(string s)
+    {
+        char a[26]={0};
+        for(int i=0;i<s.size();i++)
+        {
+            a[s[i]-'a']++;
+        } 
+        string temp="";
+        for(int i=0;i<26;i++)
+        {
+            temp+=string(a[i],i+'a');
+        } 
+        return temp;
+    }
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         map<string,vector<string>>mp;
         for(auto itr: strs)
         {
-            string s=itr;
-            sort(s.begin(),s.end());
+            string s=counters(itr);
             mp[s].push_back(itr);
         }
         vector<vector<string>> ans;
