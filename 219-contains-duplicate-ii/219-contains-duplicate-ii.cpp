ba45ec1d26171a5d5de918164db4a8465cfb1eab
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-17 19:25:34 +0530

    Time: 291 ms (28.92%), Space: 72.1 MB (98.18%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         int n=nums.size();
-        set<int>s;
+       unordered_set<int>s;
         for(int i=0;i<nums.size();i++)
         {
             if(s.size()>k)
