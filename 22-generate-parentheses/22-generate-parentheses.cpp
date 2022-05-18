2cb1a38c46de9148a576bdd299a1e9c75e06ed8e
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-18 23:00:41 +0530

    Time: 9 ms (29.51%), Space: 11.3 MB (84.58%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,14 +1,21 @@
 class Solution { 
     private:
-    void solve(int n,int op,int cl,vector<string>&ans,string temp)
+    void solve(int n,int op,int cl,vector<string>&ans,string &temp)
     {
-        if(op==n && cl==n)
+        if(op==n && cl==n){
             ans.push_back(temp);
+            return ;}
         else{
-        if(op<n)
-            solve(n,op+1,cl,ans,temp+'(');
-        if(cl<op)
-            solve(n,op,cl+1,ans,temp+')');
+        if(op<n){ 
+            temp+='(';
+            solve(n,op+1,cl,ans,temp);
+            temp.erase(temp.size()-1);
+        }
+        if(cl<op){ 
+           temp+=')';
+            solve(n,op,cl+1,ans,temp);
+             temp.erase(temp.size()-1);
+        }
         }
     }
 public:
@@ -19,25 +26,3 @@ public:
         return ans;
     }
 };
-// class Solution {
-// public:
-//     vector<string> generateParenthesis(int n) {
-//         vector<string> parens;
-//         string paren;
-//         generate(n, 0, 0, paren, parens);
-//         return parens;
-//     }
-// private:
-//     void generate(int n, int l, int r, string paren, vector<string>& parens) {
-//         if (l == n && r == n) {
-//             parens.push_back(paren);
-//         } else {
-//             if (l < n) {
-//                 generate(n, l + 1, r, paren + '(', parens);
-//             }
-//             if (r < l) {
-//                 generate(n, l, r + 1, paren + ')', parens);
-//             }
-//         }
-//     }
-// };
