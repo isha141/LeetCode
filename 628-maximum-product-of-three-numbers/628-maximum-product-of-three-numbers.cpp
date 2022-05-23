1d66ff71b1336fdd7c75cff929f6636d77fbc7e8
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-23 11:54:30 +0530

    Time: 49 ms (81.30%), Space: 27.8 MB (36.87%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -1,9 +1,39 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-       sort(nums.begin(),nums.end());  
+      // sort(nums.begin(),nums.end());  
+        int mini1=INT_MAX;
+        int mini2=INT_MAX;
+        int maxi1=INT_MIN;
+        int maxi2=INT_MIN;
+        int maxi3=INT_MIN;
         int n=nums.size();
-        return max((nums[0]*nums[1] * nums[n-1]),nums[n-2]*nums[n-1]*nums[n-3]);
-        //return ans;
+        for(auto itr: nums)
+        {
+            if(itr<=mini1)
+            {
+                mini2=mini1;
+                mini1=itr;
+            } 
+            else if(itr<=mini2)
+            {
+                mini2=itr;
+            }
+            if(itr>= maxi1)
+            { 
+                maxi3=maxi2;
+                maxi2=maxi1;
+                maxi1=itr;
+            }
+            else if(itr>=maxi2)
+            {
+                maxi3=maxi2;
+                maxi2=itr;
+            } 
+            else if(itr>=maxi3)
+                maxi3=itr;
+        }
+        return max((mini1*mini2*maxi1),(maxi1*maxi2*maxi3));
+       
     }
 };
