8a0ef768d95a1030caba942902ca6b1976cda754
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-25 17:05:53 +0530

    Time: 996 ms (9.59%), Space: 143.3 MB (5.25%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int minimumLines(vector<vector<int>>& st) {
-        sort(st.begin(),st.end()); 
-        int n=st.size();
         int count=1; 
-        if(n==1)
+        if(st.size()==1)
             return 0;
-        for(int i=1;i<n-1;i++)
+        sort(st.begin(),st.end());
+        for(int i=2;i<st.size();i++)
         {
-            if(1LL*(st[i][1]-st[i-1][1])*(st[i+1][0]-st[i][0])!= 1LL *(st[i+1][1]-st[i][1])*(st[i][0]-st[i-1][0]))
-               count++;
+            vector<int>temp1=st[i];
+            vector<int>temp2=st[i-1];
+            vector<int>temp3=st[i-2];
+            int x0=temp1[0];
+            int y0=temp1[1];
+            int x1=temp2[0];
+            int y1=temp2[1];
+            int x2=temp3[0];
+            int y2=temp3[1];
+        if((1LL*(x1-x0)*(y2-y1))!= (1LL* (x2-x1)*(y1-y0)))
+            count++;
         }
-         return count;         
+        return count;
     }
 };
