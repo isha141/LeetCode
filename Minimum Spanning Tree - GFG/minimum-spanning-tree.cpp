8284d2199b84b4c37b54f4a95aac8ee2247f5d65
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-25 16:39:52 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -4,47 +4,71 @@ using namespace std;
 
  // } Driver Code Ends
 
+int findparent(int u,vector<int> &parent)
+{
+    if(parent[u]==u)
+     return u;
+   return parent[u]=findparent(parent[u],parent);  
+}
+void union1(int u,int v,vector<int> &parent,vector<int>&rank)
+{
+    u=findparent(u,parent);
+    v=findparent(v,parent); 
+    if(rank[u]<rank[v])
+    {
+        parent[u]=v;
+    }
+    else if(rank[v]<rank[u])
+    {
+        parent[v]=u;
+    }
+    else
+    {
+        parent[v]=u;
+        rank[u]++;
+    }
+    
+}
 
 class Solution
-{ 
-    private:
-    int solve(vector<int>&parent,vector<int>&key,vector<int>&mst,vector<vector<int>> adj[],int V)
-    {
-        key[0]=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,0}); 
-        int ans=0;
-        while(!pq.empty())
-        {
-        auto itr=pq.top();
-        pq.pop();
-        int temp=itr.second;
-        if(!mst[temp])
-        ans+=itr.first;
-        mst[temp]=1;
-        for(auto itt: adj[temp])
-        {
-            int node=itt[0];
-            int weight=itt[1];
-            if(mst[node]==false && key[node]>weight)
-            {
-                key[node]=weight;
-                parent[node]=temp;
-                pq.push({key[node],node});
-            }
-        }
-        }
-        return ans;
-    }
-	public:
+{
+public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-       vector<int>parent(V,-1);
-       vector<int>mst(V,0);
-       vector<int>key(V,INT_MAX);
-       return solve(parent,key,mst,adj,V);
- 
+        // code here 
+        vector<int>rank(V);
+        vector<int>parent(V);
+        for(int i=0;i<V;i++)
+        {
+            rank[i]=0; 
+            parent[i]=i;
+        }
+        int ans=0;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        for(int i=0;i<V;i++) 
+        {
+        for(auto itr: adj[i])
+       {
+          //vector<int>ds=itr;
+          pq.push({itr[1],{itr[0],i}});
+       }
+        }
+       while(!pq.empty())
+      {
+             auto itr=pq.top();
+             pq.pop();
+             int wt=itr.first;
+             int u=itr.second.first;
+             int v=itr.second.second; 
+             if(findparent(u,parent)!=findparent(v,parent))
+             { 
+                 union1(u,v,parent,rank);
+                 ans+=wt;
+             }
+      }
+      return ans;
+        
     }
 };
 
