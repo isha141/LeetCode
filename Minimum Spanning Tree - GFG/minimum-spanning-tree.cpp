424b62918795656187c283b916213acfb3bdd1d1
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-26 17:41:46 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -4,71 +4,70 @@ using namespace std;
 
  // } Driver Code Ends
 
-int findparent(int u,vector<int> &parent)
+int findparent(int u,vector<int>&parent)
 {
     if(parent[u]==u)
-     return u;
-   return parent[u]=findparent(parent[u],parent);  
+    return u;
+    return parent[u]=findparent(parent[u],parent);
 }
-void union1(int u,int v,vector<int> &parent,vector<int>&rank)
-{
+void union1(int u,int v,vector<int>&parent,vector<int>&rank)
+{ 
     u=findparent(u,parent);
-    v=findparent(v,parent); 
+    v=findparent(v,parent);
     if(rank[u]<rank[v])
     {
         parent[u]=v;
     }
     else if(rank[v]<rank[u])
-    {
-        parent[v]=u;
+    parent[v]=u;
+    else{
+        parent[u]=v;
+        rank[v]++;
     }
-    else
-    {
-        parent[v]=u;
-        rank[u]++;
-    }
-    
 }
-
 class Solution
-{
-public:
+{ 
+    private:
+    int kruskals(vector<int>&rank,vector<int>&parent,vector<vector<int>> adj[],int V)
+    { 
+        int ans=0;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        for(int i=0;i<V;i++)
+        {
+            for(auto itr: adj[i])
+            {
+                int n=itr[0];
+                int w=itr[1];
+                pq.push({w,{i,n}});
+            }
+        }
+        while(!pq.empty())
+        {
+            auto itr=pq.top();
+            int w=itr.first;
+            int n1=itr.second.first;
+            int n2=itr.second.second; 
+            pq.pop();
+            if(findparent(n1,parent)!=findparent(n2,parent))
+            {
+                union1(n1,n2,parent,rank);
+                ans+=w;
+            }
+        }
+        return ans;
+    }
+ 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here 
-        vector<int>rank(V);
+        vector<int>rank(V,0);
         vector<int>parent(V);
-        for(int i=0;i<V;i++)
-        {
-            rank[i]=0; 
-            parent[i]=i;
-        }
-        int ans=0;
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        for(int i=0;i<V;i++) 
-        {
-        for(auto itr: adj[i])
-       {
-          //vector<int>ds=itr;
-          pq.push({itr[1],{itr[0],i}});
-       }
-        }
-       while(!pq.empty())
-      {
-             auto itr=pq.top();
-             pq.pop();
-             int wt=itr.first;
-             int u=itr.second.first;
-             int v=itr.second.second; 
-             if(findparent(u,parent)!=findparent(v,parent))
-             { 
-                 union1(u,v,parent,rank);
-                 ans+=wt;
-             }
-      }
-      return ans;
-        
+        for(int i=0;i<V;i++){
+        rank[i]=0;
+        parent[i]=i;
+         } 
+         return kruskals(rank,parent,adj,V);
     }
 };
 
