ec1df663812a2f3d36eca7a2e07ed5490ece66b8
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-27 07:01:29 +0530

    Time: 7 ms (6.14%), Space: 5.9 MB (26.66%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfSteps(int num) { 
+        int count=0;
+        while(num!=0)
+        {
+            if(num%2)
+            {
+                num-=1;
+            }
+            else
+                num=num/2;
+            count++;
+        }
+        return count;
+    }
+};
