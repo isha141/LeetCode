847fe4c9a9cf1f262f618b752087dde950b24272
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-27 20:48:42 +0530

    Time: 4 ms (66.71%), Space: 7.6 MB (73.37%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,22 @@
+class Solution { 
+    private:
+    void solve(int ind,vector<int>& nums,vector<vector<int>>&ans,vector<int>&ds)
+    {
+        ans.push_back(ds);
+         for(int i=ind;i<nums.size();i++){ 
+             if(i>ind && nums[i]==nums[i-1])
+                 continue;
+             ds.push_back(nums[i]);
+            solve(i+1,nums,ans,ds);
+           ds.pop_back();
+         }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans; 
+        sort(nums.begin(),nums.end());
+        vector<int>ds;
+        solve(0,nums,ans,ds);
+        return ans;
+    }
+};
