893eb4657534be333f86aea1e058c592fc644ccc
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-28 18:56:11 +0530

    Time: 3 ms (58.84%), Space: 6.1 MB (17.66%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,25 @@
+class Solution { 
+public:
+    string getPermutation(int n, int k) {
+        string ans=""; 
+        vector<int>ds; 
+        int fact=1;
+        for(int i=1;i<n;i++){
+            ds.push_back(i);  
+            fact=fact*i;
+        }
+        ds.push_back(n);
+        k=k-1;
+        while(true)
+        { 
+            //k=k/fact; 
+            ans+=to_string(ds[k/fact]); 
+            ds.erase(ds.begin()+k/fact);
+            if(ds.size()==0)
+                break;
+            k=k%fact; 
+            fact=fact/ds.size();
+        }
+       return ans;     
+    }
+};
