5a8b54e552aac538ef44abbbfac1396ef4734309
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-31 18:13:37 +0530

    Added solution - LeetHub

--- M-Coloring Problem - GFG/mcoloring-problem.cpp
+++ M-Coloring Problem - GFG/mcoloring-problem.cpp
@@ -5,42 +5,39 @@ using namespace std;
 
  // } Driver Code Ends
 // Function to determine if graph can be coloured with at most M colours such
-// that no two adjacent vertices of graph are coloured with same colour.  
-bool ispossible(int node, bool graph[101][101],int k,int n,vector<int>&col)
+// that no two adjacent vertices of graph are coloured with same colour. 
+bool ispossible(int node,int k,int n,int m,vector<int>&col,bool graph[101][101])
 {
-    for(int itr=0 ;itr<n;itr++)
-    {
-        if(itr!=node && graph[itr][node]==1 && col[itr]==k)
-        {
-          return 0;
-        }
-    }
-    return true;
+    // if(node==n)
+    // return 1;
+   for(int i=0;i<n;i++)
+   { 
+      if(i!=node && graph[i][node]==1 && col[i]== k)
+      return 0;
+   }
+   return 1;
 }
-bool solve(int node,bool graph[101][101],int m,int n, vector<int>&col)
+bool solve(int node,bool graph[101][101],int n,int m, vector<int>&col)
 {
-    if(node==n)
-    return true;
+    if(node==n) return true;
     for(int i=1;i<=m;i++)
-    {
-        if(ispossible(node,graph,i,n,col))
-        {
-            col[node]=i;
-            if(solve(node+1,graph,m,n,col))
-            return true; 
-            col[node]=0;
-        }
+    {  
+       if(ispossible(node,i,n,m,col,graph)){
+        col[node]=i;
+        if(solve(node+1,graph,n,m,col))
+                  return 1;
+        col[node]=0;
+       }
     }
-    return false;
+    return 0;
 }
 bool graphColoring(bool graph[101][101], int m, int n) {
-    // your code here
+    // your code here  
     vector<int>col(n,0);
-     if(solve(0,graph,m,n,col))
-     return 1;
-     return 0;
-    
-}
+        if(solve(0,graph,n,m,col))
+            return 1;
+    return 0;
+} 
 
 // { Driver Code Starts.
 
