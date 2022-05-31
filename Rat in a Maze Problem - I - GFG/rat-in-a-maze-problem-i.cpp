ed101065aa81bd73f459de18fc761217024bb0f4
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-31 19:24:41 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -10,44 +10,39 @@ using namespace std;
 
 class Solution{ 
     private:
-    void solve(int i,int j, vector<vector<int>> &m,string temp,vector<string>&ans,int n,vector<vector<int>>&vis)
+    void solve(int i,int j,int *di,int *dj,vector<vector<int>> &m,int n,vector<string>&ans,string temp,vector<vector<int>>&v)
     {
-        if(i==n-1 && j==n-1){ 
-            if(m[i][j]==1)
-                 ans.push_back(temp);
-        return ;
-       }   
-      if(m[i][j]==0)
-      return ;
-       if(i+1<n && !vis[i+1][j]){ 
-           vis[i][j]=1;
-       solve(i+1,j,m,temp+'D',ans,n,vis); 
-       vis[i][j]=0;
-       }
-       if(j+1<n &!vis[i][j+1] ){ 
-           vis[i][j]=1;
-       solve(i,j+1,m,temp+'R',ans,n,vis);  
-       vis[i][j]=0;
-       }
-       if(j-1>=0 && !vis[i][j-1]){ 
-           vis[i][j]=1;
-        solve(i,j-1,m,temp+'L',ans,n,vis);  
-        vis[i][j] =0;
-       }
-        if(i-1>=0 && !vis[i-1][j]){ 
-            vis[i][j]=1;
-        solve(i-1,j,m,temp+'U',ans,n,vis);
-        vis[i][j]=0;
+        if(i==n-1 && j==n-1)
+        {
+            ans.push_back(temp); 
+//cout<<temp<<" ";
+            return ;
+        }
+        string s="DLRU"; 
+        //cout<<temp<<" ";
+        for(int k=0;k<4;k++)
+        {
+            int x=i+di[k];
+            int y=j+dj[k];
+            if(x>=0  && x<n && y>=0  && y<n && m[x][y]==1 && v[x][y]==0)
+            { 
+                v[i][j]=1;
+                solve(x,y,di,dj,m,n,ans,temp+s[k],v);
+                v[i][j]=0;
+            }
         }
     }
     public:
-    vector<string> findPath(vector<vector<int>> &m, int n) {
-        // Your code goes here 
-        vector<string> ans;
-        string temp=""; 
-        vector<vector<int>> vis(n,vector<int>(n,0));
-        solve(0,0,m,temp,ans,n,vis);
-        return ans;
+    vector<string> findPath(vector<vector<int>> &maze, int n) {
+        // Your code goes here  
+        vector<string> ans; 
+        vector<vector<int>>v(n,vector<int>(n,0));
+        string temp;
+        int di[4]={1,0,0,-1};
+        int dj[4]={0,-1,1,0};
+        if(maze[0][0]==1) 
+        solve(0,0,di,dj,maze,n,ans,temp,v);
+         return ans;
         
     }
 };
