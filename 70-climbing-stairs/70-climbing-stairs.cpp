b373876e26504e3122394f8d528dbdeef9880b08
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-06-01 23:26:58 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (30.41%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,17 +1,13 @@
-class Solution { 
-    private:
-    int count(int n,vector<int>&dp)
-    { 
-        ///memoization 
-        if(n<=1)
-            return 1;
-        if(dp[n]!=-1)
-            return dp[n];
-        return dp[n]=count(n-1,dp)+ count(n-2,dp);
-    }
-public:
+class Solution {
+public: 
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return count(n,dp);
+       vector<int>dp(n+1,-1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+           dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
