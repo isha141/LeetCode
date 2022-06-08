b41c809025e76b1dfd8ebbfa74c4aaa854bba929
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-06-08 22:47:11 +0530

    Time: 7 ms (74.86%), Space: 9.1 MB (9.99%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,7 +3,7 @@ class Solution {
     int solve(int i,int j,int m,vector<vector<int>>& t,vector<vector<int>>&dp){
         if(i==m-1)
             return t[i][j]; 
-        if(i>=m || j>=t[i].size()+1) return INT_MAX; 
+      //  if(i>=m || j>=t[i].size()+1) return INT_MAX; 
         if(dp[i][j]!=-1) return dp[i][j];
         int left=t[i][j]+solve(i+1,j,m,t,dp);
         int right=t[i][j]+solve(i+1,j+1,m,t,dp);
