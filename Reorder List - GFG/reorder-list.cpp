3542a053584532da886eb8341a0eb321f97ff6e6
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-06-09 14:45:34 +0530

    Added solution - LeetHub

---
+++ Reorder List - GFG/reorder-list.cpp
@@ -0,0 +1,107 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void reorderList(struct Node* head);
+
+/* Function to create a new Node with given data */
+struct Node* newNode(int data) {
+    struct Node* new_Node = new Node(data);
+    new_Node->data = data;
+    new_Node->next = NULL;
+    return new_Node;
+}
+
+void printList(struct Node* Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head != NULL) {
+
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main(void) {
+    int t, n, m, i, x;
+
+    cin >> t;
+    while (t--) {
+        struct Node* temp, *head;
+        cin >> n;
+        cin >> x;
+        head = new Node(x);
+        temp = head;
+        for (i = 0; i < n - 1; i++) {
+            cin >> x;
+            temp->next = new Node(x);
+            temp = temp->next;
+        }
+
+        reorderList(head);
+        printList(head);
+        freeList(head);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/* Following is the Linked list node structure */
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void reorderList(Node* head) {
+    // Your code here  
+    Node *temp=head;
+    stack<Node *>s; 
+    int n=0;
+    while(head!=NULL){
+        s.push(head);
+        n++; 
+        head=head->next; 
+    }  
+    head=temp;
+    for(int j=0;j<n/2;j++){
+        Node *prev=s.top();
+        s.pop();
+        prev->next=temp->next;
+        temp->next=prev;
+        temp=temp->next->next;
+    }
+     temp->next=NULL;
+   // return temp;
+    
+}
