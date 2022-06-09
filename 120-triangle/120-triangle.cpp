6fd6446d87a6d9948ce285bed8bb256ab3ad22a7
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-06-09 05:46:26 +0530

    Time: 8 ms (60.66%), Space: 8.9 MB (41.06%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,33 +1,23 @@
 class Solution { 
-    private:
-    int solve(int i,int j,int m,vector<vector<int>>& t,vector<vector<int>>&dp){
-        if(i==m-1)
-            return t[i][j]; 
-       if(i>=m || j>=t[i].size()+1) return INT_MAX; 
-        if(dp[i][j]!=-1) return dp[i][j];
-        int left=t[i][j]+solve(i+1,j,m,t,dp);
-        int right=t[i][j]+solve(i+1,j+1,m,t,dp);
-        return dp[i][j]=min(left,right);
-        
-    }
 public:
     int minimumTotal(vector<vector<int>>& t) {
        int n =t.size();
-       vector<vector<int>>dp(n,vector<int>(n,0)); 
-        dp[0][0]=t[0][0]; 
+       vector<int>prev(n);
         for(int j=0;j<n;j++){
-            dp[n-1][j]=t[n-1][j];
+            prev[j]=t[n-1][j];
         }
-        for(int i=n-2;i>=0;i--){
-            for(int j=0;j<=i;j++){
+        for(int i=n-2;i>=0;i--){ 
+            vector<int>temp(n);
+            for(int j=i;j>=0;j--){
                 long left=INT_MAX;
                  long  right=INT_MAX;
-            left=t[i][j]+ dp[i+1][j]; 
+            left=t[i][j]+ prev[j]; 
             // if(j>0 && i>0)
-           right=t[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(left,right);
+           right=t[i][j]+prev[j+1];
+                temp[j]=min(left,right);
             }
+            prev=temp;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
