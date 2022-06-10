94ee22c4f09e77358732840142d489de7387e34b
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-06-10 05:44:30 +0530

    Time: 22 ms (82.32%), Space: 5.9 MB (95.95%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,27 +1,23 @@
 class Solution {  
-    int dp[31][1001];
     private:
-   int solve(int sum,int n,int k,int target){
-        if(n==0 && sum==target) {
+    int dp[32][1002]={0}; 
+    #define MOD 1000000007
+   int solve(int n,int k,int target){
+        if(n==0 && target==0) {
               return 1;
         } 
-       if(n<0) return 0;
-       if(sum>target) return 0;
-        if(dp[n][sum]) return dp[n][sum]-1;
+       if(n<=0 || target<=0) return 0;
+        if(dp[n][target] !=-1) return dp[n][target];
        int res=0;
-        for(int i=1;i<=k;i++){ 
-           // sum+=i;
-           res=(res+ solve(sum+i,n-1,k,target))%1000000007; 
-            dp[n][sum]=res+1;
-            //sum-=i;
-        }  
-        return res;
+        for(int i=1;i<=k;++i){
+           res=((res%MOD+ solve(n-1,k,target-i))%MOD)%MOD; 
+        }
+            dp[n][target]=res; 
+        return dp[n][target];
     }
 public: 
-    int numRollsToTarget(int n, int k, int target) { 
-        int sum=0;
-     //   dp[0][tagret]==1;
-      return solve(sum,n,k,target);  
-        //return dp[99][10000]
+    int numRollsToTarget(int n, int k, int target) {  
+        memset(dp,-1,sizeof(dp));
+     return solve(n,k,target);   
     }
 };
