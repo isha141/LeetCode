645c84b82294ff4b090a87d67a089972869f2abe
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-06-10 23:49:02 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -7,35 +7,57 @@ using namespace std;
 class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
-    {
-        if(m<n) 
-        return  kthElement(arr2,arr1,m,n,k);
-        int l=max(0,k-m);
-        int h=min(k,n); 
-        int ans=0;
-        while(l<=h)
-        {
-            int cut1=(l+h)>>1;
+    { 
+        if(m<n)
+        return kthElement(arr2,arr1,m,n,k);
+        int low=max(0,k-m);
+        int high=min(n,k); 
+        while(low<=high){
+            int cut1=(low+high)>>1;
             int cut2=k-cut1;
             int l1=cut1==0?INT_MIN:arr1[cut1-1];
-            int l2=cut2==0?INT_MIN: arr2[cut2-1];
+            int l2=cut2==0?INT_MIN:arr2[cut2-1];
             int r1=cut1==n?INT_MAX:arr1[cut1];
-            int r2=cut2==m?INT_MAX:arr2[cut2];
+            int r2=cut2==m?INT_MAX:arr2[cut2]; 
             if(l1<=r2 && l2<=r1)
             {
-             return max(l1,l2);   
+                return max(l1,l2);
             }
             else if(l1>r2)
-            {
-                 h=cut1-1;
-            }
-            else { 
-                 l=cut1+1;
-               
-            }
+            high=cut1-1;
+            else
+            low=cut1+1;
         }
-        return ans;
+        return -1;
     }
+    // if(m<n) 
+    //     return  kthElement(arr2,arr1,m,n,k);
+    //     int l=max(0,k-m);
+    //     int h=min(k,n); 
+    //     int ans=0;
+    //     while(l<=h)
+    //     {
+    //         int cut1=(l+h)>>1;
+    //         int cut2=k-cut1;
+    //         int l1=cut1==0?INT_MIN:arr1[cut1-1];
+    //         int l2=cut2==0?INT_MIN: arr2[cut2-1];
+    //         int r1=cut1==n?INT_MAX:arr1[cut1];
+    //         int r2=cut2==m?INT_MAX:arr2[cut2];
+    //         if(l1<=r2 && l2<=r1)
+    //         {
+    //          return max(l1,l2);   
+    //         }
+    //         else if(l1>r2)
+    //         {
+    //              h=cut1-1;
+    //         }
+    //         else { 
+    //              l=cut1+1;
+               
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
 
 // { Driver Code Starts.
