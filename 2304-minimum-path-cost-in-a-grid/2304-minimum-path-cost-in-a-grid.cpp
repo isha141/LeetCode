4f06be3cc8d0b969d6ab2808a95b57850c421369
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-06-13 01:51:30 +0530

    Time: 586 ms (100.00%), Space: 80.5 MB (100.00%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -16,11 +16,18 @@ public:
         int ans=INT_MAX; 
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>>dp(100,vector<int>(100,-1));
-        for(int i=0;i<grid[0].size();i++)
-        {
-            ans=min(ans,solve(0,i,grid,move,dp));
+        vector<vector<int>>dp(m,vector<int>(n,INT_MAX)); 
+        for(int i=0;i<n;i++)
+            dp[0][i]=grid[0][i];
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){ 
+                for(int k=0;k<n;k++){ 
+                dp[i][j]=min(dp[i][j],(grid[i][j]+move[grid[i-1][k]][j]+dp[i-1][k]));
+            } 
         }
+        } 
+        for(int i=0;i<n;i++)
+            ans=min(ans,dp[m-1][i]);
         return ans;
     }
 };
