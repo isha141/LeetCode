304ba7133751f6306b7add1f5b27afef2bd1a169
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-06-15 13:10:36 +0530

    Time: 233 ms (5.01%), Space: 43.9 MB (19.65%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -3,11 +3,10 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
         map<int,int>mp;
-        int sum=0;  
+        int sum=0; 
         int count=0;
-        for(int i=0;i<n;i++)
-        {
-            sum+=nums[i];  
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
             if(sum==k)
                 count++;
             if(mp.count(sum-k))
@@ -16,4 +15,4 @@ public:
         }
         return count;
     }
-};
+}; 
