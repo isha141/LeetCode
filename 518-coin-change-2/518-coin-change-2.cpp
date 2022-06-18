fa657ffd1dd3c6c93d7a6c41b71aa4272c44cdf1
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-06-18 17:10:07 +0530

    Time: 85 ms (16.96%), Space: 18.3 MB (45.73%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,20 +1,21 @@
 class Solution { 
-    private:
-    int solve(int n,vector<int>&coins,int amount,vector<vector<int>>&dp){  
-          if(amount<0) return 0;
-        if(amount==0) return 1;
-        if(n==0){
-            return (amount%coins[0]==0);
-        } 
-        if(dp[n][amount]!=-1) return dp[n][amount];
-        int left=solve(n,coins,amount-coins[n],dp);
-        int right=solve(n-1,coins,amount,dp); 
-        return dp[n][amount]=left+right;
-    }
 public:
     int change(int amount, vector<int>& coins) {
         int n=coins.size(); 
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        return solve(n-1,coins,amount,dp);
+        vector<vector<int>>dp(n,vector<int>(amount+1,0)); 
+        for(int i=0;i<=amount;++i){
+            if(i%coins[0]==0) 
+                    dp[0][i]=1; 
+        }
+      for(int i=1;i<n;i++){
+          for(int j=0;j<=amount;++j){
+              int take=0;
+              if(coins[i]<=j)
+                 take=dp[i][j-coins[i]];
+              int not_take=dp[i-1][j];
+              dp[i][j]=take+not_take;
+          }
+      }
+        return dp[n-1][amount];
     }
 };
