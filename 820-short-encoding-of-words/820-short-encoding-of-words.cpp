43cc53b5411482234e513fe1b21898829e675d2a
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-06-23 10:48:24 +0530

    Time: 135 ms (50.41%), Space: 13.8 MB (94.80%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -3,7 +3,7 @@ public:
     int minimumLengthEncoding(vector<string>& words,int res=0) {
         for(int i=0;i<words.size();++i)
             reverse(words[i].begin(),words[i].end());
-        sort(words.begin(),words.end());
+             sort(words.begin(),words.end());
         for(int i=0;i<words.size()-1;++i){
             int c=words[i]==words[i+1].substr(0,words[i].size())?0: words[i].size()+1; 
             cout<<c<<" ";
