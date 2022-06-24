9e1feac52d64b53438221f02dd9a496a6299d5de
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-06-24 08:09:05 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (50.81%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(auto itr: s)
-        {
-            if(itr=='('|| itr=='{'|| itr=='[')
+       stack<char>st;
+        for(auto itr: s){
+            if(itr=='(')
                 st.push(itr);
-            else if(itr==']')
-            {
-                if(!st.empty() && st.top()=='[')
+            else if(itr=='{')
+                st.push(itr);
+          else  if(itr=='[')
+              st.push(itr);
+            else{ 
+                if(st.empty()) return 0;
+                if(itr==')')
+                {
+                   if(st.top()!='(')
+                       return 0; 
                     st.pop();
-                else
-                    return false;
-            }
-            else if(itr=='}')
-            {
-                if(!st.empty() && st.top()=='{')
-                    st.pop(); 
-                else
-                    return false;
-            }
-            else if(itr==')')
-            {
-                if(!st.empty() && st.top()=='(')
+                }
+              else if(itr=='}'){
+                      if(st.top()!='{')
+                          return 0;
+                      st.pop();
+                  } 
+                else if(itr==']'){
+                    if(st.top()!='[')
+                        return 0;
                     st.pop();
-                else
-                    return false;
-            }
+                }
+              }
         }
         return st.empty();
     }
