ad25242444f1c41a23330b6e85550afadec1488c
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-06-25 22:04:19 +0530

    Time: 57 ms (15.22%), Space: 27.1 MB (53.59%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
-        int n=nums.size(); 
-        int count=0;
-        for(int i=1;i<n;++i){
-            if(nums[i]<nums[i-1]){
-                count++;
-                if(i>=2 && nums[i-2]>nums[i]){
-                    nums[i]=nums[i-1];
-                }
-                else{
-                  nums[i-1]=nums[i];  
-                }
+        int n=nums.size();
+        for(int i=0;i<n-1;++i){
+            if(nums[i]>nums[i+1]){
+                int prev=nums[i];
+                nums[i]=nums[i+1];
+                if(is_sorted(nums.begin(),nums.end()))
+                    return 1;
+                nums[i]=prev;
+                nums[i+1]=nums[i];
+                if(is_sorted(nums.begin(),nums.end()))
+                    return 1;
+                else
+                    return 0;
             }
         }
-        return count<=1;        
+        return 1;
     }
 };
