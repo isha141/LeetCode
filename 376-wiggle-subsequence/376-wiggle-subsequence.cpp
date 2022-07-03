eaafe2ca9d41ada4ec96f9c3c3fe7f0de20e12a2
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-07-04 02:16:04 +0530

    Time: 43 ms (6.00%), Space: 19.1 MB (5.05%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,28 +1,29 @@
+int dp[1005][1005][3];
 class Solution { 
-    //nums[i]>nums[i-1]=down;
-    //nums[i]<nums[i-1]=up
-public:
-    int wiggleMaxLength(vector<int>& nums) { 
-        int n=nums.size();
-        vector<int>down(n,0);
-        vector<int>up(n,0); 
-        up[0]=1;
-        down[0]=1;
-        for(int i=1;i<n;++i){
-            if(nums[i]>nums[i-1]){
-                down[i]=up[i-1]+1;
-                up[i]=up[i-1];
-            }
-            else if(nums[i]<nums[i-1]){
-                up[i]=down[i-1]+1;
-                down [i]=down[i-1];
-            }
-            else
-            {
-                up[i]=up[i-1];
-                down[i]=down[i-1];
-            }
+    vector<int>nums;
+    private:
+    int solve(int ind,int flag,int val){
+        if(ind>=nums.size())
+            return 0; 
+        if(val==-1)
+            return max(solve(ind+1,flag,nums[ind]),solve(ind+1,flag,val));
+        if(dp[ind][val][flag]!=-1)
+            return dp[ind][val][flag];
+        int take=0;
+        if(flag && nums[ind]-val>0){
+            take=solve(ind+1,1^flag,nums[ind])+1;
         }
-        return max(up[n-1],down[n-1]);
+        if(!flag && nums[ind]-val<0){
+            take=solve(ind+1,1^ flag,nums[ind])+1;
+        }
+        take=max(take,solve(ind+1,flag,val)); 
+       return dp[ind][val][flag]=take;
+    }
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n=nums.size();  
+        memset(dp,-1,sizeof(dp));
+        this->nums=nums;
+        return max(solve(0,0,-1),solve(0,1,-1))+1;
     }
 };
