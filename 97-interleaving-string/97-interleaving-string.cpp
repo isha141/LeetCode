e0d0b0213c0d0125e163972f479e0eae38fca4ba
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-07-07 22:40:01 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (80.49%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,19 +1,19 @@
 int dp[105][105];
 class Solution { 
-    int solve(string &s1,string &s2,string &s3,int i=0,int j=0,int k=0)
+    int solve(int i,int j,int k,string &s1,string &s2,string &s3)
     {
-        if(i>=s1.size() && j>=s2.size() && k>=s3.size())
+        if(i==s1.size() && j==s2.size() && k==s3.size())
             return 1;  
         int left=0;
         int right=0; 
         if(dp[i][j]!=-1) return dp[i][j];
         if(i<s1.size() && s1[i]==s3[k]){ 
-          left|=solve(s1,s2,s3,i+1,j,k+1);
+          left=solve(i+1,j,k+1,s1,s2,s3);
         }
         if(j<s2.size() && s2[j]==s3[k]){
-         left|=solve(s1,s2,s3,i,j+1,k+1);
+         right=solve(i,j+1,k+1,s1,s2,s3);
          }
-      return dp[i][j]=left;
+      return dp[i][j]=(left|| right);
     }
 public:
     bool isInterleave(string s1, string s2, string s3) {
@@ -21,6 +21,6 @@ public:
         int n=s2.size();
         int o=s3.size(); 
         memset(dp,-1,sizeof(dp));
-        return solve(s1,s2,s3);
+        return solve(0,0,0,s1,s2,s3);
     }
 };
