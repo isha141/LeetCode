960892065dc3265295614260d1163b1bc74b7a38
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-07-09 23:45:22 +0530

    Time: 605 ms (21.27%), Space: 18.2 MB (6.10%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,21 +1,27 @@
-class Solution {
+class Solution { 
+    private:
+    int solve(int ind,vector<int>&nums,int n,vector<int>&dp)
+    {  
+        
+        if(ind>=n-1)
+            return 0;  
+        if(dp[ind]!=-1)
+            return dp[ind];
+       int temp=1e9;
+        int steps=nums[ind];
+        for(int i=1;i<=steps;++i){
+            temp=min(1+solve(i+ind,nums,n,dp),temp); 
+            // co//ut<<temp<<" in"<<endl;;
+        }  
+        //cout<<temp<<"out"<<endl;
+        // cout<<endl; 
+        
+        return dp[ind]=temp;
+    }
 public:
     int jump(vector<int>& nums) {
-        int n=nums.size();
-        int count=0;
-        int start=0;
-        int end=0;
-        int maxreach=0;
-        while(end<n-1){
-            count++;
-            for(int i=start;i<=end;++i){
-                maxreach=max(maxreach,nums[i]+i);
-            } 
-            if(maxreach>=n-1)
-                return count;
-            start=end+1;
-            end=maxreach;
-        }
-        return count;
+        int n=nums.size(); 
+        vector<int>dp(n,-1);
+        return solve(0,nums,n,dp);
     }
 };
