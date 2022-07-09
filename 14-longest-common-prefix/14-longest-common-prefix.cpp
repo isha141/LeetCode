1051ee51dcac0bf770ba49025f6abc4e5e1e268d
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-07-09 15:31:42 +0530

    Time: 8 ms (43.24%), Space: 9.3 MB (51.09%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        
+        string ans="";  
         sort(strs.begin(),strs.end());
-        string ans="";
         string a=strs[0];
-        string b=strs[strs.size()-1];
-        int n=a.size();
-        for(int i=0;i<n;i++)
-        {
+        string b=strs[strs.size()-1]; 
+        // cout<<a<<" "<<b<<endl;
+         int n=a.size();
+        for(int i=0;i<n;++i){ 
             if(a[i]==b[i])
                 ans+=a[i];
             else
                 break;
+            // cout<<"hy"<<endl;
         }
         return ans;
+        
     }
 };
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         sort(strs.begin(),strs.end());
+//         string ans="";
+//         string a=strs[0];
+//         string b=strs[strs.size()-1];
+//         int n=a.size(); 
+//         cout<<a<<" hy"<<b<<endl;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(a[i]==b[i])
+//                 ans+=a[i];
+//             else
+//                 break;
+//         }
+//         return ans;
+//     }
+// };
