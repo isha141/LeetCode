adf8fb22f0170b7ab63fa4c1126719ddd107fe7d
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-07-12 12:35:04 +0530

    Time: 2 ms (52.43%), Space: 6.2 MB (61.19%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
-    int strStr(string  stack1, string needle) { 
-        int n=stack1.size();
-        return stack1.find(needle);
-        
+    int strStr(string a, string b) {
+        int m=a.size();
+        int n=b.size();
+        for(int i=0;i<=m-n;++i){ 
+            int j=i,k=0;
+            while(j<a.size() && k<b.size()){
+                if(a[j]!=b[k])
+                    break;
+                j++,k++;
+            }
+            if(k==n)
+                return i;
+        }
+        return -1;
     }
 };
