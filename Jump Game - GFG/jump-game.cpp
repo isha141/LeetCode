67dda50a59373532397cda6cdbca5bf60a319391
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-07-15 12:20:07 +0530

    Added solution - LeetHub

--- Jump Game - GFG/jump-game.cpp
+++ Jump Game - GFG/jump-game.cpp
@@ -9,20 +9,20 @@ using namespace std;
 
 class Solution { 
     private:
-    int solve(int ind,int A[],int N)
-    {
-        if(ind+1>=N) return 1;
+    bool solve(int ind,int A[],int N){
+        if(ind>=N) return 0;
+        if(ind==N-1) return 1; 
         if(A[ind]==0) return 0;
-        for(int i=1;i<=A[ind];i++)
-        {
-            if(solve(ind+i,A,N))
-            return 1; 
+        for(int i=1;i<=A[ind];++i){
+          if(solve(i+ind,A,N))
+          return 1;
         }
         return 0;
     }
   public:
     int canReach(int A[], int N) {
         // code here 
+        if(A[0]==0) return 0;
         return solve(0,A,N);
     }
 };
