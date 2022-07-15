847ce362bd805f2ce1b25c2c20e7d39b78bc6e42
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-07-15 23:53:43 +0530

    Time: 28 ms (60.48%), Space: 26.7 MB (15.50%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,33 +1,39 @@
-class Solution {  
-    int m,n;
+class Solution { 
     private:
-    void solve(int i,int j,vector<vector<int>>& grid,int &temp)
-    {
-        int dx[4]={1,0,-1,0};
-        int dy[4]={0,-1,0,1};
-        for(int k=0;k<4;++k){
-            int xx=dx[k]+i;
-            int yy=dy[k]+j;
-            if(xx>=0 && xx<m && yy>=0 && yy<n && grid[xx][yy]==1){ 
-                grid[xx][yy]=0;
-                temp++;
-                solve(xx,yy,grid,temp);
-            } 
-        }
-        return ;
+    void solve(int i,int j,vector<vector<int>>&grid,int &area){
+        int m=grid.size();
+        int n=grid[0].size(); 
+        grid[i][j]=0;
+        queue<pair<int,int>>q;
+        q.push({i,j}); 
+        while(!q.empty()){
+                auto temp=q.front();
+                q.pop();
+                int x=temp.first;
+                int y=temp.second;
+                int dx[4]={1,0,0,-1};
+                int dy[4]={0,1,-1,0};
+                for(int i=0;i<4;++i){
+                 int xx=dx[i]+x;
+                 int yy=dy[i]+y; 
+                if(xx>=0 && xx<m && yy>=0 && yy<n && grid[xx][yy]==1)
+                { 
+                    area++; 
+                    grid[xx][yy]=0;
+                    q.push({xx,yy});
+                }
+            }
+            }
     }
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m=grid.size();
-        int n=grid[0].size(); 
-        this->m=m;
-        this->n=n;
+        int n=grid[0].size();
         int ans=0;
         for(int i=0;i<m;++i){
             for(int j=0;j<n;++j){
                 if(grid[i][j]==1){ 
-                    int temp=1;  
-                    grid[i][j]=0;
+                    int temp=1;
                     solve(i,j,grid,temp);
                     ans=max(ans,temp);
                 }
