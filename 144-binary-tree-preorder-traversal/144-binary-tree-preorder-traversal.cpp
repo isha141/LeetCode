7fffe82927ffa566262b845579f6d24eb917f73a
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-07-17 19:33:50 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (93.24%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -14,17 +14,28 @@ public:
     vector<int> preorderTraversal(TreeNode* root) {
         if(root==NULL)
             return {};
-        vector<int>ans;
-        stack<TreeNode *>s;
-        s.push(root);
-        while(!s.empty()){
-            TreeNode *temp=s.top();
-            s.pop();
-            ans.push_back(temp->val);
-            if(temp->right)
-                s.push(temp->right);
-            if(temp->left)
-                s.push(temp->left);
+        vector<int>ans; 
+        TreeNode *curr=root;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode *temp=curr->left; 
+                while(temp->right!=NULL  && temp->right!=curr)
+                    temp=temp->right;
+                if(temp->right==NULL)
+                { 
+                     ans.push_back(curr->val);
+                    temp->right=curr;
+                    curr=curr->left;
+                }
+                 else{
+                     temp->right=NULL;
+                     curr=curr->right;
+                 }
+            }
         }
         return ans;
     }
