cd887cccbebe24e324614ad2e9f6a535cce1e331
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-07-22 03:02:24 +0530

    Time: 19 ms (92.19%), Space: 17.2 MB (19.40%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -1,41 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
 class Solution {
 public:
-    Node* connect(Node* root) {
-        vector<Node *>v;
-        if(root==NULL)
-            return root; 
-        Node *head=root;
+    Node* connect(Node* root) { 
+        Node *temp=root;
+        if(root==NULL) return root;
         queue<Node *>q;
-        q.push(root); 
-     //  vector<Node *>v;
+        q.push(root);
         while(!q.empty()){
-           int n=q.size();
-            while(n--){ 
+            int n=q.size();
+            while(n--){
                 Node *temp=q.front();
-                q.pop();
-                v.push_back(temp);
+                 q.pop();   
+                temp->next=NULL;
+                if(n==0)
+                    temp->next=NULL;
+                else
+                    temp->next=q.front();
                 if(temp->left)
                     q.push(temp->left);
                 if(temp->right)
                     q.push(temp->right);
             } 
-            v.push_back(NULL);
-        } 
-        // for(auto itr: v)
-        // {
-        //     if(itr!=NULL)
-        //         cout<<itr->val<<" ";
-        //     else
-        //         cout<<"NULL"<<" ";
-        // }
-       for(int i=0;i<v.size()-1;++i){
-           if(v[i]!=NULL){
-               v[i]->next=v[i+1];
-           }
-               else{
-                   continue;
-               }
-       }
-       return head; 
-    } 
+        }
+        return temp;
+    }
 };
