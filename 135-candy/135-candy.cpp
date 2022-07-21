fb500ca3f4a533f73c9b6569922cd17353bdfdc7
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-07-21 13:27:01 +0530

    Time: 24 ms (87.07%), Space: 17.6 MB (75.15%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,21 +1,21 @@
-#define mod 100000007+9
 class Solution {
 public:
-    int candy(vector<int>& rat) { 
-        int n=rat.size();
-        vector<int>l(n,1);
-        int sum=0;
+    int candy(vector<int>& ratings) {
+      int n=ratings.size();
+        int ans=0;
+        vector<int>left(n,1);
         for(int i=1;i<n;++i){
-            if(rat[i]>rat[i-1]){
-                l[i]=l[i-1]+1;
+            if(ratings[i]>ratings[i-1]){
+                left[i]=left[i-1]+1;
             }
-        } 
-        sum=l[n-1];
+        }
+        int sum=0;
+        sum=left[n-1];
         for(int i=n-2;i>=0;--i){
-            if(rat[i]>rat[i+1]){
-                l[i]=max(l[i],l[i+1]+1);
+            if(ratings[i]>ratings[i+1]){
+                left[i]=max(left[i],left[i+1]+1);
             }
-           sum+=l[i]; 
+            sum+=left[i];
         }
         return sum;
     }
