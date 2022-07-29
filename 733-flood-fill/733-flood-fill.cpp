7104b53c01d2a00689bc522c00600fb81ed888f3
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-07-29 19:36:44 +0530

    Time: 37 ms (5.31%), Space: 14 MB (79.42%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,42 +1,22 @@
 class Solution { 
     private:
-    void solve(vector<vector<int>>& image, int sr, int sc, int color)
+    void solve(vector<vector<int>>& image, int sr, int sc, int &color,int &t)
     {
-        int m=image.size();
-        int p=image[0].size(); 
-        vector<vector<int>>vis(m,vector<int>(p,0)); 
-        int dx[4]={1,0,-1,0};
-        int dy[4]={0,1,0,-1}; 
-        vis[sr][sc]=1;  
-        int col= image[sr][sc]; 
-        cout<<col<<" ";
+        if(sr<0 || sr>=image.size() || sc<0 || sc>=image[0].size() || image[sr][sc]!=t)
+            return ; 
         image[sr][sc]=color;
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        while(!q.empty()){ 
-            int n=q.size();
-            while(n--){ 
-                auto itr=q.front();
-                q.pop();
-                int x=itr.first;
-                int y=itr.second;
-                for(int i=0;i<4;++i){
-                 int xi=dx[i]+x;
-                 int yi=dy[i]+y;  
-                    // cout<<xi<<" "<<yi<<endl;
-                if(xi>=0 && xi<m && yi>=0 && yi<p && image[xi][yi]==col && !vis[xi][yi] ){
-                    q.push({xi,yi});
-                    vis[xi][yi]=1; 
-                    // cout<<"hy";
-                    image[xi][yi]=color;
-                }
-             }
-        }
-        } 
+        solve(image,sr+1,sc,color,t);
+        solve(image,sr,sc+1,color,t);
+        solve(image,sr-1,sc,color,t);
+        solve(image,sr,sc-1,color,t);
+        
     }
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        solve(image,sr,sc,color);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) { 
+        int t=image[sr][sc]; 
+        if(image[sr][sc]==color)
+            return image;
+        solve(image,sr,sc,color,t); 
         return image;
     }
 };
