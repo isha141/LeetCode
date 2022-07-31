0f5c386e2ff704a650041ed9136458b035dcc789
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-07-31 21:55:52 +0530

    Time: 175 ms (100.00%), Space: 56.6 MB (100.00%) - LeetHub

---
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximumGroups(vector<int>& grades) {
+        sort(grades.begin(),grades.end());
+        int k=0;
+        int n=grades.size();
+        int total=0;
+        while(total+k+1<=n)
+            total+=++k;
+        return k;
+    }
+};
