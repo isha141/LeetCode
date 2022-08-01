da81f6094eb2b98c53f4efbc78febd855d7d3c72
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-08-01 12:01:39 +0530

    Time: 3 ms (44.81%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,22 +1,23 @@
-class Solution {
+class Solution {  
+    private:
+    int m,n;
+    int solve(int i,int j,vector<vector<int>>&dp)
+    {
+        if(i>=m || j>=n || i<0 || j<0) 
+            return 0; 
+        if(i==0 && j==0)
+            return 1; 
+        if(dp[i][j]!=-1) return dp[i][j];
+        int l=solve(i-1,j,dp);
+        int r=solve(i,j-1,dp);
+        return dp[i][j]=l+r;
+            
+    }
 public:
-    int uniquePaths(int m, int n) {
-        int dp[m][n];
-        int count=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0){
-                    dp[i][j]=1; continue; 
-                }
-                int up=0;
-                int right=0;
-                if(i-1>=0)
-                up=dp[i-1][j];
-                if(j-1>=0)
-                    right=dp[i][j-1];
-            dp[i][j]=up+right;
-            }
-        }
-        return dp[m-1][n-1];
+    int uniquePaths(int m, int n) { 
+        this->m=m;
+        this->n=n; 
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,dp);
     }
 };
