1dc23047ba303a224c5beddb7178216b3f1fe2fe
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-08-06 03:24:30 +0530

    Time: 18 ms (97.28%), Space: 12.9 MB (98.97%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,37 +1,35 @@
-class Solution { 
-    private:
-    bool solve(int node, int par,vector<int>adj[],vector<int>&vis,vector<int>&dfsvis)
-    {
-        vis[node]=1;
-        dfsvis[node]=1;
-        for(auto itr: adj[node]){
-            if(vis[itr]==0){
-               if( solve(itr,node,adj,vis,dfsvis))
-                   return 1;
-            }
-            else if(dfsvis[itr])
-                return 1;
-        }
-        dfsvis[node]=0;
-        return 0;
-    }
+class Solution {
 public:
-    bool canFinish(int num, vector<vector<int>>& pre) {
-        int n=pre.size(); 
+    bool canFinish(int num, vector<vector<int>>& pre) {  
         vector<int>adj[num];
         for(int i=0;i<pre.size();++i){
-            // adj[pre[i][0]].push_back(pre[i][1]);
-            adj[pre[i][1]].push_back(pre[i][0]);
+                adj[pre[i][1]].push_back(pre[i][0]);
         }
-        vector<int>vis(num+1,0); 
-        vector<int>dfsvis(num+1,0);
-      for(int i=0;i<num;++i){
-          if(vis[i]==0)
-          {
-              if(solve(i,-1,adj,vis,dfsvis))
-                  return 0;
-          }
-      }
-        return 1;
+        queue<int>q;
+        vector<int>indegree(num,0); 
+        for(int i=0;i<num;++i){
+            for(auto itr: adj[i])
+            {
+                indegree[itr]++;
+            }
+        }
+        for(int i=0;i<num;++i){
+            if(indegree[i]==0)
+                q.push(i);
+        } 
+        // cout<<"hy"<<endl;
+        int cnt=0;
+        while(!q.empty())
+        {
+            int temp=q.front();
+            q.pop();
+            cnt++;
+            for(auto itr: adj[temp]){
+                indegree[itr]--;
+                if(indegree[itr]==0)
+                    q.push(itr);
+            }
+        }
+        return cnt==num;
     }
 };
