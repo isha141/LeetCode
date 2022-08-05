0cbc9267135fe1bbda3ad0e2a9ba7a0c0047fde6
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-08-06 01:37:35 +0530

    Time: 34 ms (68.85%), Space: 13.5 MB (84.47%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,33 +1,29 @@
 class Solution { 
     private:
-    bool solve(int i,vector<vector<int>>& graph,int m,vector<int>&visited)
+    bool solve(int node,vector<vector<int>>& graph,vector<int>&color)
     {
-        for(auto itr: graph[i])
-        {
-            if(visited[itr]==-1)
-            {
-                visited[itr]=!visited[i];
-                if(!solve(itr,graph,m,visited))
-                    return false;
+        for(auto itr: graph[node]){
+            if(color[itr]==-1){
+                color[itr]=1^color[node];
+              if(!solve(itr,graph,color))
+                  return 0;
             }
-            else if(visited[itr]==visited[i])
-                 return false;
+            else if(color[itr]==color[node])
+                return 0;
         }
-        return true;
+        return 1;
     }
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int m=graph.size(); 
-        vector<int> visited(m,-1);
-        for(int i=0;i<m;i++)
-        {
-            if(visited[i]==-1)
-            { 
-                visited[i]=0;
-              if(!solve(i,graph,m,visited))
-                  return false;
+    bool isBipartite(vector<vector<int>>& graph) { 
+        int n=graph.size();
+        vector<int>color(n,-1);
+        for(int i=0;i<n;++i){
+            if(color[i]==-1){ 
+                color[i]=1;
+            if(!solve(i,graph,color))
+                return 0;
             }
         }
-        return true;
+        return 1;
     }
 };
