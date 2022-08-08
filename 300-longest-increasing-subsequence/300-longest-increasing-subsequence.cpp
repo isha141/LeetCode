5da3d3dd36bee2b4b2a719e1faf9e5791527f159
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-08-09 05:21:17 +0530

    Time: 912 ms (16.70%), Space: 292.3 MB (8.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
     private:
     int solve(int i,vector<int>&nums,int n,int prev,vector<vector<int>>&dp)
     {
-        if(i>=n || prev>=n+1)
+        if(i>=n || prev>=n)
             return 0;  
         if( dp[i][prev+1]!=-1) return dp[i][prev+1];
         int take=0;
@@ -14,11 +14,20 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        int prev=-1; 
-        int maxi=0;
-        for(auto itr:nums)
-            maxi=max(maxi,itr);
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(0,nums,n,prev,dp);
+        int prev=-1;
+       vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=n-1;i>=0;--i){
+            for(int j=i-1;j>=-1;--j){
+                int take=0;
+                int not_take=0;
+                if(j==-1 || nums[j]<nums[i]){
+                    take=dp[i+1][i+1]+1;
+                }
+                not_take=dp[i+1][j+1]; 
+                dp[i][j+1]=max(take,not_take);
+            }
+            
+        } 
+        return dp[0][0];
     }
 };
