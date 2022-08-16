9ac0ba52f569ce2220b07a182c71b3b0ca4aa031
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-08-17 02:15:48 +0530

    Time: 24 ms (45.97%), Space: 9.1 MB (22.78%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -17,9 +17,21 @@ class Solution {
     }
 public:
     int minDistance(string w1, string w2) {
-        int m=w1.size()-1;
-        int n=w2.size()-1;  
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return solve(m,n,w1,w2,dp);
+        int m=w1.size();
+        int n=w2.size();  
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0)); 
+        for(int i=0;i<=m;++i)
+            dp[i][0]=i; 
+        for(int j=0;j<=n;++j)
+            dp[0][j]=j;
+       for(int i=1;i<=m;++i){
+           for(int j=1;j<=n;++j){
+               if(w1[i-1]==w2[j-1])
+                   dp[i][j]=dp[i-1][j-1]; 
+               else
+                 dp[i][j]=1+min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]));
+           }
+       }
+        return dp[m][n];
     }
 };
