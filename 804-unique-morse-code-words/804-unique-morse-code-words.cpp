2f0ecee52b86aa63829d236440ba6b7b5194a63f
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-08-17 22:45:17 +0530

    Time: 7 ms (46.48%), Space: 8.6 MB (30.73%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -1,40 +1,14 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        map<char,string>mp;
-        mp['a']=".-";
-        mp['b']="-...";
-        mp['c'] =       "-.-.";
-        mp['d']= "-..";
-        mp['e']= ".";
-        mp['f']=        "..-.";
-        mp['g']="--.";
-        mp['h']=  "....";
-                mp['i'] =    "..";
-         mp['j']=         ".---";
-        mp['k']=  "-.-"; 
-        mp['l']=".-..";
-        mp['m']="--";
-        mp['n']="-.";
-        mp['o']="---";
-        mp['p']=".--.";
-        mp['q']="--.-";
-        mp['r']=".-.";
-        mp['s']= "...";
-        mp['t']="-";
-        mp['u']="..-";
-        mp['v']="...-";
-        mp['w']=".--";
-        mp['x']="-..-";
-        mp['y']="-.--";
-        mp['z']= "--.."; 
-        map<string,int>mpp;
+     vector<string>ds={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        map<string,int>mp;
         for(auto itr: words){
             string temp="";
             for(auto itt: itr)
-                temp+=mp[itt];
-            mpp[temp]++;
+                temp+=ds[itt-'a'];
+            mp[temp]++;
         }
-       return mpp.size();
+        return mp.size();
     }
 };
