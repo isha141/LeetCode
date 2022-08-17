561e1adadfa2e0f8243ba0669eb9cbc7d1b8d0a3
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-08-18 02:29:03 +0530

    Time: 367 ms (44.76%), Space: 84.1 MB (17.64%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -9,20 +9,24 @@ public:
         if(sum%2)
             return 0;
         sum/=2;
-        vector<vector<int>>dp(n,vector<int>(sum+1,0)); 
-    for(int i=0;i<n;++i)
-        dp[i][0]=1; 
+        // vector<vector<int>>dp(n,vector<int>(sum+1,0)); 
+    // for(int i=0;i<n;++i)
+    //     dp[i][0]=1;  
+        vector<int>prev(sum+1,0);
         if(nums[0]<=sum)
-        dp[0][nums[0]]=1;
+        prev[nums[0]]=1;
         for(int i=1;i<n;++i){
+            vector<int>curr(sum+1,0); 
+            curr[0]=1;
             for(int k=1;k<=sum;++k){
                 int pick=0;
-                int not_pick=dp[i-1][k];
+                int not_pick=prev[k];
                 if(nums[i]<=k)
-                    pick=dp[i-1][k-nums[i]];
-                dp[i][k]=(pick|| not_pick);
-            }
+                    pick=prev[k-nums[i]];
+                curr[k]=(pick|| not_pick);
+            } 
+            prev=curr;
         }
-        return dp[n-1][sum];
+        return prev[sum];
     }
 };
