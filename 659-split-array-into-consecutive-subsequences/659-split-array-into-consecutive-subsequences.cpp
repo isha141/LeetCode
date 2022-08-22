09e58d13ac832787e824382861bc2d2af1e0e635
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-08-23 02:46:55 +0530

    Time: 130 ms (72.96%), Space: 59.2 MB (20.90%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -1,28 +1,33 @@
-class Solution {
-public:
-    bool isPossible(vector<int>& nums) {
-        int n=nums.size();
+class Solution { 
+    bool solve(int n,vector<int>&nums)
+    {
         map<int,int>mp;
-        for(auto itr: nums)
-            mp[itr]++; 
         map<int,int>mp1;
+        for(auto itr: nums)
+            mp[itr]++;
         for(int i=0;i<n;++i){
-           if(mp[nums[i]]==0)
-               continue;
+            if(mp[nums[i]]==0)
+                continue;
             mp[nums[i]]--;
-             if(mp1[nums[i]-1]>0){
-                mp1[nums[i]-1]--; 
+            if(mp1[nums[i]-1]>0){
+                mp1[nums[i]-1]--;
                 mp1[nums[i]]++;
             }
             else if(mp[nums[i]+1]>0 && mp[nums[i]+2]>0){
                 mp[nums[i]+1]--;
-                mp[nums[i]+2]--;
+                mp[nums[i]+2]--; 
                 mp1[nums[i]+2]++;
-            }
+            } 
             
             else
                 return 0;
-            }
+        }
         return 1;
+    }
+public:
+    bool isPossible(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        return solve(n,nums);
     }
 };
