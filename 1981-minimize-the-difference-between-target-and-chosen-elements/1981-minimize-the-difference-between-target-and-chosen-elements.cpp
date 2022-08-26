c84542268d9203b3741d225ecae6d468fbfb6551
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-08-27 01:02:55 +0530

    Time: 1625 ms (63.14%), Space: 14.4 MB (81.68%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,17 +1,19 @@
 int dp[72][5000];
 class Solution { 
     int m;
-    int n; 
+    int n;
     private:
-    int solve(int i,int sum,vector<vector<int>>& mat, int target)
+    int solve(int i,int sum,vector<vector<int>>& mat, int &target)
     {
         if(i==m)
-            return abs(sum-target);
-         if(dp[i][sum]!=-1)
-             return dp[i][sum];
+        return abs(sum-target);
+        if(dp[i][sum]!=-1)
+            return dp[i][sum];
         int mini=INT_MAX;
-        for(int k=0;k<n;++k){
-            mini=min(mini,solve(i+1,sum+mat[i][k],mat,target));
+        for(int k=0;k<n;++k){ 
+            // sum+=mat[i][k];
+            mini=min(solve(i+1,sum+mat[i][k],mat,target),mini); 
+            // sum-=mat[i][k];
         }
         return dp[i][sum]=mini;
     }
@@ -20,6 +22,7 @@ public:
         m=mat.size();
         n=mat[0].size(); 
         memset(dp,-1,sizeof(dp));
-        return solve(0,0,mat,target);
+        int sum=0;
+        return solve(0,sum,mat,target);
     }
 };
