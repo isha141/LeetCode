a46d96725efb7e88eb9d138afc518d14d9b4fd0a
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-09-03 13:20:50 +0530

    Time: 357 ms (78.65%), Space: 112.7 MB (84.61%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,18 +1,19 @@
 class Solution { 
     int maxi=0;
     int ans=0;
-    unordered_map<int,vector<int>>adj;
     private:
-    void solve(int head,int sum,vector<int>&inform)
+    void solve(int head,int sum,vector<int>adj[],
+vector<int>&inform)
     {
          maxi=max(maxi,sum);
         for(auto itr: adj[head]){   
-            solve(itr,sum+inform[itr],inform);
+            solve(itr,sum+inform[itr],adj,inform);
         }
         return ;
     }
 public:
     int numOfMinutes(int n, int head, vector<int>& man, vector<int>& inform) {
+            vector<int>adj[n];
         for(int i=0;i<n;++i){
            int manager=man[i];
             if(manager!=-1){
@@ -20,7 +21,7 @@ public:
             }
         } 
         int sum=inform[head];
-        solve(head,sum,inform);
+        solve(head,sum,adj,inform);
         return maxi;
     }
         
