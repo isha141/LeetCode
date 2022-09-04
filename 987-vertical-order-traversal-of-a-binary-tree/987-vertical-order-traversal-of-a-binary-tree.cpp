dcd2132e8fd8737c373e37d76bbe92f5343b362f
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-09-05 00:26:51 +0530

    Time: 19 ms (5.20%), Space: 15.8 MB (5.35%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -9,34 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution { 
+    private:
+    void solve(TreeNode *root,int x,int y,map<int,map<int,multiset<int>>>&mp)
+    {
+        if(root==NULL)
+            return ;
+        mp[x][y].insert(root->val);
+        solve(root->left,x-1,y+1,mp); 
+        solve(root->right,x+1,y+1,mp);
+    }
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
          vector<vector<int>>ans;
-        if(root==NULL)
+        if(root==NULL){
             return ans;
-        map<int,map<int,multiset<int>>>mp;
-        queue<pair<TreeNode *,pair<int,int>>>q;
-        q.push({root,{0,0}});
-        while(!q.empty()){
-            int n=q.size();
-            while(n--){
-                auto itr=q.front();
-                TreeNode *temp=itr.first;
-                int x=itr.second.first;
-                int y=itr.second.second; 
-                q.pop();
-                mp[x][y].insert(temp->val);
-                if(temp->left)
-                    q.push({temp->left,{x-1,y+1}});
-                if(temp->right)
-                    q.push({temp->right,{x+1,y+1}});
-            }
         }
+    map<int,map<int,multiset<int>>>mp;
+        solve(root,0,0,mp);
         for(auto itr: mp){
             vector<int>ds;
-            for(auto itt: itr.second){
-                ds.insert(ds.end(),itt.second.begin(),itt.second.end());
+            for(auto it: itr.second){
+                ds.insert(ds.end(),it.second.begin(),it.second.end());
             }
             ans.push_back(ds);
         }
