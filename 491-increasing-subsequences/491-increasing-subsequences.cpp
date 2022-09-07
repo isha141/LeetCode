e414077cb7c8b13cae9efeeb8097947436456e0b
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-09-07 23:56:28 +0530

    Time: 56 ms (91.49%), Space: 16.4 MB (99.73%) - LeetHub

--- 491-increasing-subsequences/491-increasing-subsequences.cpp
+++ 491-increasing-subsequences/491-increasing-subsequences.cpp
@@ -1,10 +1,10 @@
 class Solution { 
     private:
-    void solve(int i,vector<int>&nums,int prev,vector<int>&ds,set<vector<int>>&ans)
+    void solve(int i,vector<int>&nums,int prev,vector<int>&ds,vector<vector<int>>&ans)
     {
         if(i>=nums.size()){
             if(ds.size()>=2){
-                ans.insert(ds);
+                ans.push_back(ds);
             }
             return ;
         }
@@ -12,18 +12,19 @@ class Solution {
             ds.push_back(nums[i]);
             solve(i+1,nums,nums[i],ds,ans);
             ds.pop_back();
-        } 
+        }  
+        if(ds.size()==0 || ds[ds.size()-1]!=nums[i])
         solve(i+1,nums,prev,ds,ans);
     }
 public:
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        set<vector<int>>ans;
+        vector<vector<int>>ans;
         int n=nums.size();
         vector<int>ds;
         solve(0,nums,INT_MIN,ds,ans); 
-        vector<vector<int>>res;
-        for(auto itr: ans)
-            res.push_back(itr);
-        return res;
+        // vector<vector<int>>res;
+        // for(auto itr: ans)
+            // res.push_back(itr);
+        return ans;
     }
 };
