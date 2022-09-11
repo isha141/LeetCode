daa554f3642014fd93bf4128230317691d931d23
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-09-11 09:32:32 +0530

    Time: 89 ms (5.25%), Space: 13 MB (44.48%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,36 +1,27 @@
-  class Solution { 
-      private:
-      int solve(int i,int j,string &t1,string &t2, vector<vector<int>>&dp)
-      {
-          if(i<0 || j<0)
-              return 0;
-          int take=0;
-          int not_take=0;
-          if(dp[i][j]!=-1)
-              return dp[i][j];
-          if(t1[i]==t2[j])
-            take=1+solve(i-1,j-1,t1,t2,dp); 
-          else
-              not_take=max(solve(i-1,j,t1,t2,dp),solve(i,j-1,t1,t2,dp));
-          return dp[i][j]=max(take,not_take);
-      }
-public:
-    int longestCommonSubsequence(string t1, string t2) {
-        int m=t1.size();
-        int n=t2.size(); 
-        vector<int>prev(n+1,0);
-        for(int i=1;i<=m;++i){ 
-            vector<int>curr(n+1,0);
-            for(int j=1;j<=n;++j){ 
-                int take=0;
-                int not_take=0;
-                if(t1[i-1]==t2[j-1])
-                    take=1+prev[j-1];
-                not_take=max(prev[j],curr[j-1]);
-                curr[j]=max(take,not_take);
-            } 
-            prev=curr;
+class Solution { 
+    private:
+    int solve(int i,int j,string &a,string &b,vector<vector<int>>&dp)
+    {
+        if(i<0 || j<0)
+            return 0;
+         if(i==0 && j==0)
+             return a[0]==b[0];
+        int l=0;
+        int r=0; 
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        if(a[i]==b[j])
+            l=1+solve(i-1,j-1,a,b,dp);
+        else{
+            r=max(solve(i-1,j,a,b,dp),solve(i,j-1,a,b,dp));
         }
-        return prev[n];
+        return dp[i][j]=max(l,r);
+    }
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m=text1.size();
+        int n=text2.size(); 
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,text1,text2,dp);
     }
 };
