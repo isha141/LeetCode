13cf13e45d073da8c770c77884972ede4c84ad4c
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-09-13 18:00:37 +0530

    Time: 1908 ms (5.05%), Space: 144.5 MB (9.28%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,23 +1,21 @@
 class Solution { 
-    static bool cmp(vector<int>&a,vector<int>&b){
+     static bool cmp(vector<int>&a,vector<int>&b){
         if(a[0]!=b[0])
             return a[0]<b[0];
-        else 
-            return a[1]>b[1];
-    }
+         return a[1]>b[1];
+     }
 public:
-    int numberOfWeakCharacters(vector<vector<int>>& pro){
+    int numberOfWeakCharacters(vector<vector<int>>& pro) {
         int n=pro.size();
         sort(pro.begin(),pro.end(),cmp);
         int c=0; 
         priority_queue<int,vector<int>,greater<int>>pq;
-        int mini=INT_MIN;
-        for(auto itr: pro){
-           while(!pq.empty() && pq.top()<itr[1]){
-               pq.pop(); 
-               c++;
-           }
-            pq.push(itr[1]);
+        for(auto it: pro){
+          while(!pq.empty() && pq.top()<it[1]){
+                c++;
+                pq.pop();
+            }
+            pq.push(it[1]);
         }
         return c;
     }
