59bd73328e9a1780e191477a642eb843c9be212f
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-09-14 15:43:10 +0530

    Time: 138 ms (81.07%), Space: 36.9 MB (23.79%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -0,0 +1,25 @@
+class Solution { 
+    private:
+    int mod=1e9+7;
+public:
+    int maxPerformance(int n, vector<int>& s, vector<int>& e, int k) {
+        vector<pair<int,int>>ds;
+        for(int i=0;i<n;++i){
+            ds.push_back({e[i],s[i]});
+        }
+        sort(ds.rbegin(),ds.rend());
+        long long  ans=0; 
+        long long sum=0;
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(auto&[x,y]: ds){
+            sum+=y;
+            pq.push(y);
+            if(pq.size()>k){ 
+                sum-=pq.top();
+                pq.pop();
+            }
+            ans=max(ans,sum*x);
+        }
+        return ans%(int) mod;
+    }
+};
