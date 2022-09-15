99d3c782ddcfc549ea2f1f28a0932dabf81c2d7b
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-09-15 16:56:35 +0530

    Time: 1192 ms (31.85%), Space: 101.1 MB (44.15%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,19 +1,20 @@
-bool cmp(pair<int,int>a,pair<int,int>b){
-    if(a.first==b.first)
-        return a.second<b.second;
-    return a.first<b.first;
-};
 class Solution { 
+    static bool cmp(vector<int>&a,vector<int>&b){
+        if(a[0]!=b[0]){
+            return a[0]<b[0];
+        }
+        return a[1]>b[1];
+    }
 public:
-    int minGroups(vector<vector<int>>& in){
-        int n=in.size();
-        sort(in.begin(),in.end());
-        int ans=0; 
+    int minGroups(vector<vector<int>>& in) {
+        sort(in.begin(),in.end()),cmp; 
+        int c=0;
         priority_queue<int,vector<int>,greater<int>>pq;
         for(auto it: in){
-            if(!pq.empty() && pq.top()<it[0])
+            if(pq.size() && pq.top()<it[0]){
                 pq.pop();
-            pq.push(it[1]);
+            }
+               pq.push(it[1]);
         }
         return pq.size();
     }
