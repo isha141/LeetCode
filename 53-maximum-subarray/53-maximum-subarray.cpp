b21a9c0e6fc249da78688bd10af9fde0a6416067
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-09-23 17:03:43 +0530

    Time: 275 ms (11.16%), Space: 67.8 MB (10.82%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        int ans=INT_MIN;
+        for(int i=0;i<n;++i){
+            sum+=nums[i];
+            ans=max(ans,sum);
+            if(sum<0)
+                sum=0;
+        }
+        return ans;
+    }
+};
