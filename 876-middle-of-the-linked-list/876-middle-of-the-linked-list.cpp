f184f33a2f24230e3f08338bdddc389b6e50690c
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-09-24 09:47:39 +0530

    Time: 3 ms (59.80%), Space: 7.2 MB (22.12%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -10,15 +10,19 @@
  */
 class Solution {
 public:
-    ListNode* middleNode(ListNode* head) { 
-        if(head==NULL) return head;
-        ListNode*  slow=head;
-        ListNode*  fast=head;
+    ListNode* middleNode(ListNode* head) {
+        if(head==NULL)
+            return head;
+        ListNode *slow=head;
+        ListNode *fast=head;
         while(slow && fast && fast->next)
         {
             slow=slow->next;
             fast=fast->next->next;
+            if(slow==fast)
+               break;
         }
         return slow;
+        
     }
 };
