2f6faeca84c6b57d44ff169540a3ea4645c013fd
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-09-26 22:44:40 +0530

    Time: 12 ms (44.63%), Space: 11.4 MB (44.49%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,54 @@
+class Solution { 
+    private:
+    vector<int>par;
+    vector<int>rank; 
+ private:
+    int findparent(int a) 
+    {
+        if(par[a]==a)
+            return a;
+        return par[a]=findparent(par[a]);
+    }
+    void Union(int a,int  b)
+    {
+        int u=findparent(a);
+        int v=findparent(b); 
+        if(u==v)
+            return;
+        if(rank[u]<rank[v]){
+            par[u]=v; 
+            // rank[u]++;
+        }
+        else if(rank[v]<rank[u]) {
+            par[v]=u;
+            // rank[v]++;
+        }
+        else{
+          par[u]=v;
+            rank[v]++;
+        }
+    }
+public:
+    bool equationsPossible(vector<string>& e) {
+        int n=e.size(); 
+        par.resize(26);
+        rank.resize(26); 
+        for(int i=0;i<26;++i)
+        {
+            par[i]=i ;
+            rank[i]=0;
+        }
+        for(auto itr: e){
+            if(itr[1]=='='){
+                Union(itr[0]-'a',itr[3]-'a');
+            }
+        }
+        for(auto itr: e){
+            if(itr[1]=='!'){ 
+            if(findparent(itr[0]-'a')==findparent(itr[3]-'a'))
+                return 0; 
+        }
+        }
+        return 1;
+    }
+};
