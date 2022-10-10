ecf67090e09f57ca8c08faa7c7fd0fe588942e7a
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-10-10 23:37:02 +0530

    Time: 2 ms (51.67%), Space: 6.3 MB (5.00%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -13,36 +13,20 @@ class Solution {
     }
 public:
     string breakPalindrome(string pal) {
-        if(pal.size()==1)
+        int n=pal.size(); 
+        bool flag=0; 
+        if(n==1)
             return "";
-        bool flag=0;  
-        if(pal.size()==3){
-            if(pal[pal.size()-1]=='a'){
-                pal[pal.size()-1]='b';
-            return pal;
+        for(int i=0;i<n;++i){
+            if(pal[i]!='a'){
+                char t=pal[i];
+                pal[i]='a';
+             if(!palin(pal))
+                 return pal;
+                 pal[i]=t;
             }
         }
-        for(auto itr: pal){
-            if(itr!='a')
-                flag=1;
-        }
-        if(!flag){
-            pal[pal.size()-1]='b';
-        return pal;
-         }
-        char ch='a';
-    for(int i=0;i<pal.size();++i){
-        if(pal[i]!='a'){
-           char t=pal[i];
-            pal[i]='a';
-            if(!palin(pal))
-                 return pal;
-            pal[i]=t;
-        }
-     }
-        if(pal[pal.size()-1]=='a'){
-            pal[pal.size()-1]='b'; 
-        }
+        pal[n-1]='b';
         return pal;
     }
 };
