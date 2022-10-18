19bd801ed37ca3b45fcf4a1a025e0735e34151e2
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-10-18 17:20:14 +0530

    Time: 1398 ms (33.85%), Space: 181.9 MB (94.09%) - LeetHub

---
+++ 2039-the-time-when-the-network-becomes-idle/2039-the-time-when-the-network-becomes-idle.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int networkBecomesIdle(vector<vector<int>>& e, vector<int>& pat) {
+        int n=pat.size();
+        vector<int>adj[n+1]; 
+        // cout<<"hy";
+        for(int i=0;i<e.size();++i){
+            adj[e[i][0]].push_back(e[i][1]);
+            adj[e[i][1]].push_back(e[i][0]);
+        } 
+        // for(int i=0;i<n;++i){ 
+        //     cout<<i<<" ";
+        //     for(auto it: adj[i])
+        //         cout<<it<<" ";
+        //     cout<<endl;
+        // }
+        queue<int>q;
+        vector<int>time(n+1,-1);
+        q.push(0);
+        time[0]=0;
+        while(!q.empty()){
+            int t=q.size();
+            while(t--){
+                auto itr=q.front();
+                q.pop();
+                for(auto it: adj[itr]){
+                    if(time[it]==-1){
+                        q.push(it);
+                        time[it]=time[itr]+1;
+                    }
+                }
+            }
+        }
+        int ans=0;
+        for(int i=1;i<n;++i){
+            int rec=2*time[i];
+            int wait=rec-1;
+            int resend=wait/pat[i];
+            int maxi=rec+resend*pat[i];
+            ans=max(ans,maxi);
+        }
+        return ans+1;
+    }
+};
