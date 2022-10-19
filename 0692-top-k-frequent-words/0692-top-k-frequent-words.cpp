92a582635dd55878fa1cf6ca1b243faa3440be20
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-10-19 11:42:06 +0530

    Time: 38 ms (11.15%), Space: 12.4 MB (95.61%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -2,26 +2,28 @@ class Solution {
     struct cmp{
     bool operator()(pair<int,string>a , pair<int,string>b){
         if(a.first==b.first)
-            return a.second>b.second;
-        return a.first<b.first;
+            return a.second<b.second;
+        return a.first>b.first;
     }
     };
         
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-          vector<string>ans;  
+          vector<string>ans(k);  
         map<string,int>mp;
         for(auto itr: words){
             mp[itr]++;
         }
         priority_queue<pair<int,string>,vector<pair<int,string>>,cmp>pq;
         for(auto itr: mp){
-            pq.push({itr.second,itr.first});
-        }
+            pq.push({itr.second,itr.first});  
+            if(pq.size()>k)
+                pq.pop();
+        } 
         while(k--){
             auto itr= pq.top();
             pq.pop();
-            ans.push_back(itr.second);
+            ans[k]=itr.second;
         } 
         // sort(ans.begin(),ans.end());
         return ans;
