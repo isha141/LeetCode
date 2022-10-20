4d4362af50639fa79194c3faada40fce1a87d44d
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-10-20 15:08:34 +0530

    Time: 937 ms (8.75%), Space: 133.7 MB (12.07%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -0,0 +1,37 @@
+class Solution {  
+//     struct cmp{
+    
+//         bool operator(pair<int,int>&a,pair<int,int>&b){
+//             if(a.first==a.)
+//         }
+        
+        
+//     }
+    private: 
+    int n;
+    int solve(int i,vector<pair<int,int>>&ds,int prev,vector<vector<int>>&dp)
+    {
+        if(i>=n)
+            return 0; 
+        if(dp[i][prev+1]!=-1)
+            return dp[i][prev+1];
+        int take=0;
+        int not_take=0;
+        if(prev==-1 ||  ds[i].second>=ds[prev].second){
+            take=ds[i].second+solve(i+1,ds,i,dp);
+        }
+        not_take=solve(i+1,ds,prev,dp);
+        return dp[i][prev+1]=max(take,not_take);
+    }
+public:
+    int bestTeamScore(vector<int>& s, vector<int>& a) {
+         n=s.size();
+        vector<pair<int,int>>ds;
+        for(int i=0;i<n;++i){
+            ds.push_back({a[i],s[i]});
+        }
+        sort(ds.begin(),ds.end()); 
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solve(0,ds,-1,dp);
+    }
+};
