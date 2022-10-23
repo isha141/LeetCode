fa59c525834b60f88843e0c17d242576f86da0ec
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-10-23 09:19:46 +0530

    Time: 35 ms (90.78%), Space: 21.3 MB (95.87%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-      vector<int>ans;
-        map<int,int>mp;
+          vector<int>ans;
         int n=nums.size();
-        for(auto itr: nums){
-            if(mp.find(itr)!=mp.end())
-                ans.push_back(itr);
-           mp[itr]++;
-        }
-        // int 
+        int xe=0;
+        for(int i=1;i<=n;++i)
+            xe^=i;
+        // int numsxe=0;
+        for(auto itr: nums)
+            xe^=itr; 
+        int x=0;
+        int y=0; 
+        int set_bits= xe & ~(xe-1);
         for(int i=1;i<=n;++i){
-            if(mp.find(i)==mp.end())
-                ans.push_back(i);
+            if(i& set_bits)
+                x^=i;
+            else  
+                y^=i;
         }
-        return ans;
-        
+        for(auto itr: nums){
+            if(itr & set_bits)
+                x^=itr;
+            else
+                y^=itr;
+        }
+         bool found=0;
+        for(auto itr: nums){
+            if(itr==x){
+                found=1;
+            }
+        }
+        if(found)
+            return {x,y};
+        else
+            return {y,x};
         
     }
+    
 };
