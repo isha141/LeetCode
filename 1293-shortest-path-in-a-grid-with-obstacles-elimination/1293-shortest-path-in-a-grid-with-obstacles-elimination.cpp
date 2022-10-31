9fb4105ab12462cf656c4f5212fce6ba446182ab
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-10-31 06:12:45 +0530

    Time: 229 ms (52.05%), Space: 50.8 MB (31.69%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,51 +1,41 @@
-class Solution {
+class Solution { 
     private:
-    int n,m;  
-    int dx[4]={-1,0,1,0};
-    int dy[4]={0,1,0,-1};
-    int solve(vector<vector<int>>& grid, int l){
-       queue<vector<int>>q; 
-        // if(grid[0][0]==1)
-        //     l-=1;
-         q.push({0,0,l});
-        vector<vector<int>>vis(n,vector<int>(m,INT_MIN)); 
-        int count=0; 
-        vis[0][0]=l;
+    int n,m;
+    int solve(vector<vector<int>>& grid, int k)
+    {
+        queue<vector<int>>q;
+        q.push({0,0,0,k});
+        vector<vector<int>>vis(n,vector<int>(m,-1)); 
         while(!q.empty()){
-            int t=q.size(); 
-            bool flag=0;
-            while(t--){
-                auto itr= q.front();
-                q.pop(); 
-                int x=itr[0];
-                int y=itr[1];
-                int l=itr[2];
-                // vis[x][y]=1;
-                if(x==n-1 && y==m-1)
-                    return count; 
-                for(int k=0;k<4;++k){
-                    int nx=x+dx[k];
-                    int ny=y+dy[k]; 
-                    // cout<<nx<<" "<<ny<<endl;
-                if(nx<0 || ny<0 || nx>=n || ny>=m )
-                       continue;  
-                int remain=l-grid[nx][ny];
-                    if(remain>=0 && vis[nx][ny]<remain){
-                        q.push({nx,ny,remain});
-                    vis[nx][ny]=remain;
-                    }
-                }
-            } 
-            count++; 
+            auto itr=q.front();
+            q.pop();
+            int x=itr[0];
+            int y=itr[1]; 
+            if(x==n-1 && y==m-1)
+                return  itr[2];
+            if(x<0|| y<0 || x>=n || y>=m)
+                continue;
+            if(grid[x][y]==1){
+             if(itr[3]>=1)
+                 itr[3]-=1;
+            else
+                continue;
+           } 
+            if(vis[x][y]!=-1 && vis[x][y]>=itr[3])
+                continue;
+            vis[x][y]=itr[3];
+            q.push({x-1,y,itr[2]+1,itr[3]});
+            q.push({x+1,y,itr[2]+1,itr[3]});            
+            q.push({x,y-1,itr[2]+1,itr[3]});
+            q.push({x,y+1,itr[2]+1,itr[3]});
         }
         return -1;
+        
     }
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
-         n=grid.size();
-         m=grid[0].size(); 
-   
-        // cout<<"hy";
+        n=grid.size();
+        m=grid[0].size();
         return solve(grid,k);
     }
 };
