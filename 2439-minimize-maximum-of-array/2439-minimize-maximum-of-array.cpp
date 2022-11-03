9484dcccad5cb3d0e6b6a7a5ac825d995ec15f9b
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-11-04 04:12:19 +0530

    Time: 403 ms (38.26%), Space: 71.3 MB (86.74%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,12 +1,29 @@
 class Solution {
+    int solve(vector<int>&nums,long long mid){
+        if(nums[0]>mid)
+            return 0;
+        long long sum=nums[0];
+        for(int i=1;i<nums.size();++i){
+           sum+=nums[i];
+            if(sum>(i+1)*mid)
+                return 0;
+        }
+        return 1;
+    }
 public:
     int minimizeArrayValue(vector<int>& nums) {
         int n=nums.size();
-        long long ans=0;
-        long long sum=0;
-        for(int i=0;i<n;++i){
-            sum+=nums[i];
-            ans=max((sum+i)/(i+1),ans);
+        long long low=0;
+        int ans=1e9;
+        long long high=1e9;
+        while(low<=high){
+            long long mid=(low+high)>>1;
+            if(solve(nums,mid)){
+                ans=mid;
+                high=mid-1;
+            }
+            else
+                low=mid+1;
         }
         return ans;
     }
