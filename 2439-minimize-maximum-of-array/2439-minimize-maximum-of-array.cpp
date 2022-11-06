443d3dcfbf4f22b22f803b1bd74f3f8928f115a3
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-11-06 19:33:40 +0530

    Time: 745 ms (7.15%), Space: 156.4 MB (16.80%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,26 +1,33 @@
-class Solution {
-    int solve(vector<int>&nums,long long mid){
-        if(nums[0]>mid)
-            return 0;
-        long long sum=nums[0];
-        for(int i=1;i<nums.size();++i){
-           sum+=nums[i];
-            if(sum>(i+1)*mid)
-                return 0;
+class Solution { 
+    private:
+    int solve(long long mid,vector<int>nums){
+     // if(nums[0]>mid)
+         // return 0;
+        long long ans=0; 
+        long long ex=0;
+        // if(nums[0]<mid)
+        //     ex+=mid-nums[0];
+        for(int i=0;i<nums.size();++i){
+             if(nums[i]>mid && ex<nums[i]-mid)
+                 return 0; 
+            if(nums[i]>mid)
+              ex-=nums[i]-mid;
+            else
+                ex+=mid-nums[i];
         }
         return 1;
     }
 public:
     int minimizeArrayValue(vector<int>& nums) {
-        int n=nums.size();
-        long long low=0;
-        int ans=1e9;
-        long long high=1e9;
+       long long low=0;
+        long long high=1000000000;
+        long long ans=0;
         while(low<=high){
             long long mid=(low+high)>>1;
-            if(solve(nums,mid)){
-                ans=mid;
-                high=mid-1;
+            if(solve(mid,nums)){
+                ans=mid; 
+                cout<<mid<<" ";
+               high=mid-1;
             }
             else
                 low=mid+1;
