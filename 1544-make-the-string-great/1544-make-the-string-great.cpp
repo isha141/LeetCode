0b648e0fac19c7021e05d772a05b3f9c9eaabcfe
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-11-08 10:11:12 +0530

    Time: 18 ms (5.98%), Space: 7.2 MB (8.20%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -2,38 +2,27 @@ class Solution {
 public:
     string makeGood(string s) {
         int n=s.size();
-        string ans="";
-        while(s.size()){
+        string ans=""; 
+        while(1) { 
             bool flag=0;
-            for(int i=0;i<s.size()-1;++i){
-                if(islower(s[i]) && isupper(s[i+1])){
-                    int ch1=s[i]-'a';
-                    int ch2=s[i+1]-'A';
-                    // cout<<i<<" "<<ch1<<" "<<ch2<<endl;
-                    if(ch1==ch2){
-                        s.erase(i,2);
-                        // s.erase(i+1,1);
-                        // cout<<s<<endl;
-                        flag=1;
-                        break;
-                    }
-                }
-                 else if(isupper(s[i]) && islower(s[i+1])){
-                    int ch1=s[i]-'A';
-                    int ch2=s[i+1]-'a';
-                    // cout<<i<<" "<<ch1<<" "<<ch2<<endl;
-                    if(ch1==ch2){
-                        s.erase(i,2);
-                        // s.erase(i+1,1);
-                        // cout<<s<<endl;
-                        flag=1;
-                        break;
-                    }
+        for(int i=0;i<n-1;++i){
+            if(islower(s[i]) && isupper(s[i+1]) || islower(s[i+1]) && isupper(s[i]) ){
+                char ch=tolower(s[i+1]);  
+                char t=tolower(s[i]);
+                if(ch==t)
+                {
+                    string temp=s.substr(0,i) +s.substr(i+2);
+                    flag=1;
+                    s=temp;
+                    break;
                 }
             }
-            if(!flag)
-                return s;
+            if(flag)
+                break;
         }
-        return "";
+        if(!flag)
+            return s;
+        }
+    return s;
     }
 };
