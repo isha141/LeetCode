a2ebe27ce6187170ec2e10953483c9f1dac69d6d
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-11-13 16:19:25 +0530

    Time: 32 ms (39.65%), Space: 20.1 MB (20.01%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+         int n=s.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0;i<n;++i){
+            dp[i][i]=1;
+        }
+        for(int i=0;i<n-1;++i){
+            if(s[i]==s[i+1])
+                dp[i][i+1]=1;
+        }
+        for(int i=2;i<n;++i){
+            for(int j=0;j<=n-i;++j){
+                int first=j;
+                int last=j+i;
+                if(s[first]==s[last] && dp[first+1][last-1]==1){
+                    dp[first][last]=1;
+                }
+            }
+        }
+        int c=0;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<n;++j){
+                if(dp[i][j])
+                    c++;
+            }
+        }
+        return c;
+    }
+};
