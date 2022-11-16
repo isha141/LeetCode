15dc9bef70d7399b855cecbf90e200e75548281d
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-11-16 12:06:46 +0530

    Added solution - LeetHub

---
+++ Swapping pairs make sum equal - GFG/swapping-pairs-make-sum-equal.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+	public:
+	int findSwapValues(int a[], int n, int b[], int m)
+	{
+        // Your code goes here 
+      long long suma=0;
+      long long sumb=0;
+      for(int i=0;i<n;++i)
+      suma+=a[i];
+      for(int j=0;j<m;++j)
+      sumb+=b[j];
+      long long temp=(suma-sumb)/2; 
+      if((suma-sumb)%2)
+      return -1;
+      map<long,long>mp;
+      for(auto i=0;i<m;++i)
+      mp[b[i]]++;
+      for(int i=0;i<n;++i){
+          if(mp.find(a[i]-temp)!=mp.end()){
+              return 1;
+          }
+      }
+      return -1;
+	}
+ 
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n,m;
+        cin>>n>>m;
+        int a[n];
+        int b[m];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        for(int i=0;i<m;i++)
+            cin>>b[i];
+        
+
+        Solution ob;
+        cout <<  ob.findSwapValues(a, n, b, m);
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+
+
+
+// } Driver Code Ends
