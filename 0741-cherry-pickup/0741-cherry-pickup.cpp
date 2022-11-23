9dc144733a5b01803c79153416aea106562b3077
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-11-23 08:27:43 +0530

    Time: 53 ms (93.57%), Space: 9.5 MB (94.78%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -1,32 +1,32 @@
-int dp[52][52][52][52];
+int dp[52][52][52];
 class Solution {
     private:
     int n,m;
     int ans=0;
-    int solve(int r1,int c1,int r2,int c2,int sum,vector<vector<int>>&grid){
-        if(r1>=n || c1>=m || r2>=n || c2>=m || grid[r1][c1]==-1 || grid[r2][c2]==-1)
+    int solve(int r1,int c1,int r2,int sum,vector<vector<int>>&grid){
+        if(r1>=n || c1>=m || r2>=n || (r1+c1)-r2 >=n || grid[r1][c1]==-1 || grid[r2][(r1+c1)-r2]==-1)
                return -1e9;
         if((r1==n-1 && c1==m-1)){
             return grid[r1][c1];
         } 
-        if((r2==n-1 && c2==m-1)){
-            return grid[r2][c2];
-        } 
+        // if((r2==n-1 && c2==m-1)){
+            // return grid[r2][c2];
+        // } 
         int ch=0;
-        if((r1==r2) && (c1==c2)){
+        if((r1==r2) && (c1==(r1+c1)-r2)){
             ch+=grid[r1][c1];
         }
         else {
-            ch+=grid[r1][c1]+grid[r2][c2];
+            ch+=grid[r1][c1]+grid[r2][(r1+c1)-r2];
         }
-        if(dp[r1][c1][r2][c2]!=-1)
-            return dp[r1][c1][r2][c2];
-        int l=solve(r1+1,c1,r2+1,c2,ch,grid); // vv
-        int r=solve(r1,c1+1,r2,c2+1,ch,grid); // hh
-        int u=solve(r1,c1+1,r2+1,c2,ch,grid); // hv
-        int d=solve(r1+1,c1,r2,c2+1,ch,grid); // vh
+        if(dp[r1][c1][r2]!=-1)
+            return dp[r1][c1][r2];
+        int l=solve(r1+1,c1,r2+1,ch,grid); // vv
+        int r=solve(r1,c1+1,r2,ch,grid); // hh
+        int u=solve(r1,c1+1,r2+1,ch,grid); // hv
+        int d=solve(r1+1,c1,r2,ch,grid); // vh
         ch+=max(l,max(r,max(u,d)));
-        return dp[r1][c1][r2][c2]=ch;
+        return dp[r1][c1][r2]=ch;
     }
 public:
     int cherryPickup(vector<vector<int>>& grid) {
@@ -35,7 +35,7 @@ public:
         if(n==1 && m==1)
             return grid[0][0]; 
         memset(dp,-1,sizeof(dp));
-       int ans= solve(0,0,0,0,0,grid);
+       int ans= solve(0,0,0,0,grid);
         return ans<=0?0:ans;
         // return ans;
     }
