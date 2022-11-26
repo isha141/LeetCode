1c4090cc11b148096f444bca9ff1990087830823
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-11-26 19:15:56 +0530

    Time: 79 ms (71.83%), Space: 36.1 MB (25.47%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,25 +1,23 @@
 class Solution { 
     private: 
     int n;
-    int solve(int i,vector<int>&arr,vector<int>&dp,vector<int>&vis){
-        if(i<0 || i>=n || vis[i]) return 0; 
-        // cout<<i<<" ..";
+    int solve(int i,vector<int>&arr,vector<int>&dp){
+        if(i<0 || i>=n || arr[i]<0) return 0; 
         if(dp[i]!=-1) return dp[i];
         if(arr[i]==0)
             return 1;
         bool take=0;
         bool not_take=0; 
-        vis[i]=1;
-        take=solve(i-arr[i],arr,dp,vis);
-        vis[i]=0;
-        not_take=solve(i+arr[i],arr,dp,vis);
+        arr[i]=-1*arr[i];
+        take=solve(i-abs(arr[i]),arr,dp);
+         arr[i]=-1*arr[i];
+        not_take=solve(i+abs(arr[i]),arr,dp);
         return dp[i]=(take || not_take);
     }
 public:
     bool canReach(vector<int>& arr, int start) {
          n=arr.size(); 
         vector<int>dp(n+1,-1); 
-        vector<int>vis(n+1,0);
-        return solve(start,arr,dp,vis);
+        return solve(start,arr,dp);
     }
 };
