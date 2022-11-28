925c77a022d3c3aaf703075773845e7d34ef4286
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-11-28 12:00:01 +0530

    Added solution - LeetHub

--- Maximum Sub Array - GFG/maximum-sub-array.cpp
+++ Maximum Sub Array - GFG/maximum-sub-array.cpp
@@ -9,33 +9,46 @@ class Solution{
 public:
 	vector<int> findSubarray(int a[], int n) {
 	    // code here 
-	    vector<int>ans;
+	    int left=0;
+	    int i=-1e9;
+	    int j=-1e9;
+	    int right=0;
 	    int sum=0;
 	    int maxi=0;
-	    vector<int>ds;
-	    for(int i=0;i<n;++i){
-	     if(a[i]>=0){
-	    sum+=a[i];
-	     ds.push_back(a[i]);
-	   }
-	   else{
-	        if(sum>maxi){
-	          maxi=sum;
-	          ans=ds;
-	      }
-	      else if(sum==maxi && ds.size()>ans.size()){
-                ans=ds;
-                maxi=sum;
-	      }	   
-	       sum=0;
-	       ds.clear();
-	   }
-	}
-	if(sum>maxi) ans=ds;
-	if(sum==maxi && ans.size()>ds.size())
-	ans=ds;
-    if(ans.size()>0) return ans;
-    return {-1};
+	    while(right<n){
+	        if(a[right]>=0)
+	        sum+=a[right];
+	        else{ 
+	            if(sum==0){ 
+	                left=right+1;
+	                right++;
+	                continue;
+	            }
+	            if(sum>maxi){
+	                maxi=sum;
+	                i=left;
+	                j=right-1;
+	            }
+	            if(sum!=0 && sum==maxi && (right-left)>(j-i)){
+	                i=left;
+	                j=right-1;
+	            }
+	            sum=0;
+	            left=right+1;
+	        }
+	        right++;
+	    }
+	    if(sum!=0 && sum>maxi){
+	        i=left;
+	        j=right-1;
+	    } 
+	    if(i==-1e9 || j==-1e9) return {-1};
+	  vector<int>ans;
+	  for(int k=i;k<=j;++k)
+	  ans.push_back(a[k]);
+	  if(ans.size()>0)
+	  return ans;
+	  return {-1};
 	}
 };
 
