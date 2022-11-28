5ccda51278d5dc45d426c6edb2d3ebca6fa26f97
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-11-28 11:31:57 +0530

    Time: 1274 ms (64.30%), Space: 135.2 MB (85.30%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n=nums.size(); 
+        int c=0;
+        vector<map<long long,int>>dp(n);
+        for(int i=0;i<n;++i){
+            for(int j=0;j<i;++j){ 
+                int ans=0;
+                long long diff=(long long)nums[i]-(long long)nums[j];
+                dp[i][diff]+=1;
+                if(dp[j].count(diff)){
+                    dp[i][diff]+=dp[j][diff];
+                    c+=dp[j][diff];
+                }
+                // dp[i][diff]=ans+1;
+                // if(ans>=1){
+                    
+                // }
+            }
+        }
+        return c;
+    }
+};
