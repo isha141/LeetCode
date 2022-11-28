d03b4e00b58d5caa1789812e40188d7cf47e4c38
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-11-29 00:43:28 +0530

    Time: 1510 ms (32.21%), Space: 182.2 MB (39.91%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -5,22 +5,19 @@ public:
          vector<vector<int>>ans;
         map<int,int>mp,mp1;
         int maxi=0;
-        for(auto itr: m){
+        for(auto itr: m){ 
+            if(!mp.count(itr[0])) mp[itr[0]]=0;
             mp[itr[1]]++;
-            maxi=max(maxi,itr[0]);
+            // maxi=max(maxi,itr[0]);
         }
       vector<int>win,loss;
         for(auto itr: mp){
             if(itr.second==1){
                 loss.push_back(itr.first);
             }
-        }
-        for(auto itr: m){
-            mp1[itr[0]]++;
-        }
-        for(int i=1;i<=maxi;++i){
-            if(mp.find(i)==mp.end() && mp1.find(i)!=mp1.end())
-                 win.push_back(i);
+             if(itr.second==0){
+                win.push_back(itr.first);
+            }
         }
         return {win,loss};
     }
