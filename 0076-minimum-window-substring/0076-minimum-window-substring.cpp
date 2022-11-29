93c6cfac2ae22f6f62ebaa48b9d4a66be04953fe
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-11-29 16:53:51 +0530

    Time: 41 ms (42.40%), Space: 8.2 MB (27.66%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,54 +1,44 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-       int ans=1e9;
         int n=s.size();
-        int i=0;
-        int j=0;
+        int m=t.size();
         unordered_map<char,int>mps,mpt;
-        for(auto itr: t)
-            mpt[itr]++;
-        int count=0;  
-        int l=0;
-        int r=n;
-        int target=t.size();
+        for(int i=0;i<m;++i)
+            mpt[t[i]]++; 
+        if(n<m) return "";
+        int j=0; 
+        int i=0;
+        int left=-1;
+        int right=-1;
+        string ans="";
+        int count=0;
         while(j<n){ 
             mps[s[j]]++;
             if(mpt.find(s[j])!=mpt.end()){
-                if(mpt[s[j]]>=mps[s[j]])
-                    count++;
+                if(mps[s[j]]<=mpt[s[j]])
+                    count++; 
             }
-            if(count==target){ 
-                int  temp=j-i; 
-                // cout<<i<<" "<<j<<endl;
-                if(ans>temp){
-                    ans=temp;
-                    l=i;
-                    r=j;
-                }
-                while(count==target && i<=j){
-                    mps[s[i]]--;  
-                     i++;
-                    if(mpt.find(s[i-1])!=mpt.end()){
-                        if(mps[s[i-1]]<mpt[s[i-1]]){
-                            count-=1; 
-                            continue;
-                        }
-                    }  
-                 // cout<<i<<" "<<j<<endl;
-                    int temp=(j-i);
-                     if(ans>temp){
-                         ans=temp;
-                         l=i;
-                         r=j;
-                     }
-                   
+            if(count==m){
+                while(i<=j && count==m){ 
+                    // cout<<s.substr(i,j-i+1)<<"(())";
+                    if((left==-1 && right==-1 ) || (right-left+1)>(j-i+1)){
+                        left=i;
+                        right=j;
+                    }
+                    mps[s[i]]--;
+                    if(mpt.find(s[i])!=mpt.end()){
+                        if(mps[s[i]]<mpt[s[i]])
+                            count--;
+                    }
+                    if(mps[s[i]]<=0)
+                        mps.erase(s[i]);
+                    i++;
                 }
             }
             j++;
-        } 
-        if(ans==1e9)
-            return "";
-        return s.substr(l,r-l+1);
+        }
+        if(left==-1 || right==-1) return "";
+        return s.substr(left,right-left+1);
     }
 };
