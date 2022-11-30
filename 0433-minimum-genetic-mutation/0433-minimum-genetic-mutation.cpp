1c1ab653b507c029c40a08aadfdeb19a91e7efaa
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-11-30 17:39:53 +0530

    Time: 3 ms (48.43%), Space: 6.7 MB (16.62%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,49 +1,39 @@
 class Solution { 
     private:
-    int solve(string start,string end,map<string,int>&mp){
-        queue<string>q;
+    int solve(string &start,string &end,vector<string>&bank){
+        queue<string>q; 
+        unordered_set<string>s;
+        for(auto itr: bank)
+            s.insert(itr);
         q.push(start);
+        string temp="ACGT";
+        map<string,int>mp;
         int c=0;
-        set<string>s;
-        s.insert(start);
-        string st="ACGT";
         while(!q.empty()){
             int n=q.size();
             while(n--){
-                auto itr=q.front();
-                q.pop();
-                if(itr==end)
-                    return c;
-                for(int i=0;i<itr.size();++i){ 
-                    // char ch=itr[i];
-                   bool flag=0;
-                    for(auto it: st){
-                        string temp=itr;
-                        temp[i]=it;
-                        if(mp.find(temp)!=mp.end() && s.find(temp)==s.end()){
-                            q.push(temp);
-                            s.insert(temp); 
-                            // cout<<temp<<" ,"<<c<<endl;
-                          flag=1;
-                        }
-                        // if(flag)
-                        //     break;
-                        // itr[i]=ch;
-                    }
-                  // if(flag)
-                  //   break;
+            auto itr=q.front();
+            q.pop();
+            if(itr==end)
+                return c;
+            for(int i=0;i<itr.size();++i){
+                for(int j=0;j<temp.size();++j){
+                    char ch=itr[i];
+                    itr[i]=temp[j];
+                    if(s.find(itr)!=s.end() && mp.find(itr)==mp.end()){
+                        q.push(itr);
+                        mp[itr]++;
+                    }  
+                    itr[i]=ch;
                 }
             }
-            c++; 
-            
+            }
+            c++;
         }
         return -1;
     }
 public:
     int minMutation(string start, string end, vector<string>& bank) {
-        map<string,int>mp;
-        for(auto itr: bank)
-            mp[itr]++;
-        return solve(start,end,mp);
+        return solve(start,end,bank);
     }
 };
