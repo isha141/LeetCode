637360b0dc6b5fd534ad773ba49082fe05ae6b6d
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-12-03 08:50:39 +0530

    Time: 514 ms (5.05%), Space: 20.8 MB (17.21%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     bool closeStrings(string w1, string w2) {
-        if(w1.size()!=w2.size())
-            return 0;
-        map<char,int>mp1,mp2;
+        vector<int>v1(26,0),v2(26,0);
         set<char>s1,s2;
-        for(auto itr : w1){
-           s1.insert(itr);
-            mp1[itr]++;
+        for(auto itr: w1){
+            v1[itr-'a']++;
+            s1.insert(itr);
         }
         for(auto itr: w2){
-           s2.insert(itr);
-        mp2[itr]++;
+            v2[itr-'a']++;
+             s2.insert(itr);
         }
-        if(s1!=s2)
-            return  0;
-        vector<int>v1,v2;
-        for(auto itr: mp1){
-           v1.push_back(itr.second);
-        }
-        for(auto itr: mp2){
-          v2.push_back(itr.second);
-        }
+        if(s1!=s2) return 0;
         sort(v1.begin(),v1.end());
-        sort(begin(v2),end(v2));
+        sort(v2.begin(),v2.end());
         return v1==v2;
-        // return 1;
-        
     }
 };
