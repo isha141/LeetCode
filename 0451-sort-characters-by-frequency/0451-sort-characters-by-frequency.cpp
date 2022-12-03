07381a71aecb5c972e9d50b0ee3ba531d452d2f8
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-12-03 20:25:04 +0530

    Time: 31 ms (39.68%), Space: 8.4 MB (67.07%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,31 +1,22 @@
-class Compare
-{
-public:
-    bool operator() (pair<int,char>&a, pair<int,char>&b)
-    {
-        if(a.first!=b.first)
-        return a.first<b.first;
-       else
-        return a.second>b.second;
-    }
-};
-class Solution { 
+class Solution {
 public:
     string frequencySort(string s) {
         int n=s.size();
         map<char,int>mp;
-        for(auto itr: s)
-        mp[itr]++;
-        priority_queue<pair<int,char>,vector<pair<int,char>>,Compare>pq;
+        for(auto itr: s){
+            mp[itr]++;
+        }
+        priority_queue<pair<int,char>>pq;
         for(auto itr: mp){
-        pq.push({itr.second,itr.first});
+            pq.push({itr.second,itr.first});
         }
         string ans="";
         while(!pq.empty()){
-            char ch=pq.top().second;
-            int val=pq.top().first;
+            auto itr=pq.top();
             pq.pop();
-            ans.append(val,ch);
+            while((itr.first)--){
+                ans+=itr.second;
+            }
         }
         return ans;
     }
