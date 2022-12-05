c074f1384e80c5ea39bba789b11721e11d257e1b
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-12-05 07:12:03 +0530

    Time: 116 ms (66.67%), Space: 55.1 MB (8.33%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    long long dividePlayers(vector<int>& skill) {
-      int n=skill.size();
-        long long sum=0;
-        for(auto itr: skill)
-            sum+=itr;
-        int teams=n/2; 
-        if(sum%teams)
-            return -1;
-        int total=sum/teams;
+    long long dividePlayers(vector<int>& s) {
+        vector<int>ds;
+        sort(s.begin(),s.end());
+        int i=0;
+        int n=s.size();
+        int j=n-1;
         long long ans=0;
-        map<int,int>mp;
-        for(auto itr: skill)
-            mp[itr]++;
-        sort(skill.begin(),skill.end());
-        int c=0;
-        for(auto itr: skill){
-            if(c>=teams)
-                return ans;
-            if(mp.find(total-itr)!=mp.end()){
-                mp[itr]--;
-                mp[total-itr]--;
-                ans+=(total-itr)*itr;
-               if(mp[itr]<=0)
-                   mp.erase(itr);
-                if(mp[total-itr]<=0)
-                    mp.erase(total-itr);
-                c++;
-            }
-            else
+        while(i<=j){
+            ds.push_back(s[i]+s[j]);
+            ans+=(s[i]*s[j]);
+            i++;
+            j--;
+        }
+        for(int i=1;i<ds.size();++i){
+            if(ds[i]!=ds[i-1])
                 return -1;
         }
-        return ans;
+        // cout<<"hy";
+        if(ds.size()==n/2)
+            return ans;
+        return -1;
     }
 };
