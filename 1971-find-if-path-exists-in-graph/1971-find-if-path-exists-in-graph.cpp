f59c89c4eb4521a488910566243856e4698b4e9a
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-12-19 21:13:10 +0530

    Time: 1361 ms (43.09%), Space: 210.2 MB (27.29%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,34 +1,28 @@
 class Solution { 
-    private:
-    int n;
-   bool solve(int s,int des,vector<int> adj[]){
-       queue<int>q;
-       q.push(s); 
-       vector<int>vis(n+1,0);
-       while(!q.empty()){
-           int temp=q.front();
-           q.pop();
-           if(temp==des)
-               return 1;
-           for(auto itr: adj[temp]){
-               if(vis[itr]==0){
-                   q.push(itr);
-                   vis[itr]=1;
-               }
-           }
-       }
-       return 0;
-   }
+    bool solve(int s,vector<int>adj[],int d,vector<int>&vis){
+        if(s==d)
+            return 1;
+        vis[s]=1;
+        for(auto itr: adj[s]){
+            if(vis[itr]==0){
+                if(solve(itr,adj,d,vis))
+                    return 1;
+                // vis[itr]=0;
+            }
+        }
+        return 0;
+    }
 public:
     bool validPath(int n, vector<vector<int>>& e, int s, int d) {
-        vector<int>adj[n+1]; 
-        this->n=n;
+        vector<int>adj[n+1];
         for(int i=0;i<e.size();++i){
             adj[e[i][0]].push_back(e[i][1]);
             adj[e[i][1]].push_back(e[i][0]);
         }
-        if(solve(s,d,adj))
-            return 1;
+        vector<int>vis(n+1,0); 
+        vis[s]=1;
+      if(solve(s,adj,d,vis))
+          return 1;
         return 0;
     }
 };
