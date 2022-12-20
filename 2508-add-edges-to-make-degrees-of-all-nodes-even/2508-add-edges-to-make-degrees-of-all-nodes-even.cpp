b60e29b145000e4cc617bd44b08ce54a0b10f189
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-12-20 10:21:50 +0530

    Time: 1538 ms (20.38%), Space: 169.2 MB (65.60%) - LeetHub

---
+++ 2508-add-edges-to-make-degrees-of-all-nodes-even/2508-add-edges-to-make-degrees-of-all-nodes-even.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isPossible(int n, vector<vector<int>>& e) {
+        set<int>adj[n+1];
+        vector<int>indeg(n+1,0);
+        for(int i=0;i<e.size();++i){
+                indeg[e[i][0]]++;
+                indeg[e[i][1]]++;
+            adj[e[i][0]].insert(e[i][1]);
+             adj[e[i][1]].insert(e[i][0]);
+        } 
+        // for(int i=1;i<=n;++i){
+        //     cout<<i<<" "<<indeg[i]<<endl;
+        // }
+        vector<int>odd;
+        for(int i=1;i<=n;++i){
+            if(1 & indeg[i])
+                odd.push_back(i);
+        }
+        if(odd.size()%2)
+            return 0;
+        if(odd.size()==0) return 1;
+        if(odd.size()>4) return 0;
+        if(odd.size()==2){
+            for(int i=1;i<=n;++i){
+                 if(adj[odd[0]].find(i)==adj[odd[0]].end() && adj[odd[1]].find(i)==adj[odd[1]].end())
+                     return 1;
+        }
+      }
+      if(odd.size()==4){
+        if(adj[odd[0]].find(odd[1])==adj[odd[0]].end() && adj[odd[2]].find(odd[3])==adj[odd[2]].end())
+            return 1;
+        if(adj[odd[0]].find(odd[2])==adj[odd[0]].end() && adj[odd[1]].find(odd[3])==adj[odd[1]].end())
+            return 1;
+        if(adj[odd[0]].find(odd[1])==adj[odd[0]].end() && adj[odd[2]].find(odd[3])==adj[odd[2]].end())
+           return 1;
+        if(adj[odd[0]].find(odd[3])==adj[odd[0]].end() && adj[odd[1]].find(odd[2])==adj[odd[1]].end())
+          return 1;
+
+      }
+        return 0;
+    }
+}; 
