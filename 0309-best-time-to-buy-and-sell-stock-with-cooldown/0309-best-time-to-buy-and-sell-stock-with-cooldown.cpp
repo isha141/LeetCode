11301667a3412157864774bc97e31e55595a462d
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-12-23 17:56:15 +0530

    Time: 14 ms (24.10%), Space: 11.8 MB (46.95%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,22 +1,35 @@
-int dp[5003][3];
+// int dp[5003][3];
 class Solution { 
     private:
-    int solve(int i,int buy,vector<int>&p,int n){
-        if(i>=n)
-            return 0;
-        int take=0;
-        int not_take=0;
-        if(dp[i][buy]!=-1) return dp[i][buy];
-        if(buy==0)
-        take=max(-p[i]+solve(i+1,1,p,n),solve(i+1,0,p,n));
-        else
-        not_take=max(p[i]+solve(i+2,0,p,n),solve(i+1,1,p,n));
-        return dp[i][buy]=max(take,not_take);
-    }
+    // int solve(int i,int buy,vector<int>&p,int n){
+    //     if(i>=n)
+    //         return 0;
+    //     int take=0;
+    //     int not_take=0;
+    //     if(dp[i][buy]!=-1) return dp[i][buy];
+    //     if(buy==0)
+    //     take=max(-p[i]+solve(i+1,1,p,n),solve(i+1,0,p,n));
+    //     else
+    //     not_take=max(p[i]+solve(i+2,0,p,n),solve(i+1,1,p,n));
+    //     return dp[i][buy]=max(take,not_take);
+    // }
 public:
     int maxProfit(vector<int>& p) {
         int n=p.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,p,n);
+       vector<vector<int>>dp(n+2,vector<int>(3,0));
+        for(int i=n-1;i>=0;--i){
+            for(int j=0;j<=1;++j){ 
+                 int take=0;
+                 int not_take=0;
+                if(!j){
+                    take=max(-p[i]+dp[i+1][1],dp[i+1][0]);
+                }
+                else{
+              not_take=max(p[i]+dp[i+2][0],dp[i+1][1]);
+                }
+            dp[i][j]=max(take,not_take);
+            }
+        }
+        return dp[0][0];
     }
 };
