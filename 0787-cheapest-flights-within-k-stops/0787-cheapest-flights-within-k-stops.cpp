35596f36a53e0584024fa29750c0d9ccc807eee4
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-12-31 01:05:26 +0530

    Time: 22 ms (94.44%), Space: 12.9 MB (89.10%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& f, int src, int dst, int k) {
+        vector<pair<int,int>>adj[n+1];
+        for(int i=0;i<f.size();++i){
+            adj[f[i][0]].push_back({f[i][1],f[i][2]});
+        }
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq; 
+        int ans=1e9;
+        vector<int>vis(n,INT_MAX);
+        vis[src]=0;
+        k+=2;
+        pq.push({1,{src,0}});
+        while(!pq.empty()){
+            auto itr=pq.top();
+            pq.pop();
+            int n=itr.second.first;
+            int dis=itr.second.second;
+            int stop=itr.first; 
+            if(stop<k){
+                if(n==dst){
+                    ans=min(ans,dis);
+                }
+            }
+            if(stop>=k){
+                 if(n==dst){
+                    ans=min(ans,dis);
+                }
+                continue;
+            }
+            for(auto itr: adj[n]){
+                if(vis[itr.first]>dis+itr.second){
+                    vis[itr.first]=dis+itr.second;
+                    pq.push({stop+1,{itr.first,dis+itr.second}});
+                }
+            }
+        }
+        return ans==1e9?-1:ans;
+    }
+};
