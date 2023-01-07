817a9db41e9dcb790595cf86f653e87e3a3dcaea
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-01-07 13:01:22 +0530

    Time: 16 ms (80.82%), Space: 13.3 MB (23.15%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int i=n-1;
+        int j=m-1;
+        bool flag=1;
+        for(int i=0;i<n;++i){ 
+            if(matrix[i][0]==0){
+                flag=0;
+            }
+            for(int j=1;j<m;++j){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        // for(int i=0;i<n;++i){
+        //     for(int j=0;j<m;++j){
+        //         cout<<matrix[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        for(int i=n-1;i>=0;--i){
+            for(int j=m-1;j>=1;--j){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
+            }
+            if(!flag){
+                matrix[i][0]=0;
+            }
+        }
+        return ;
+    }
+};
