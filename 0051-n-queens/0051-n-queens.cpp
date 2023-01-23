72910a5958d4e3c7089dfa1713ebd9dc9822b16a
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-01-24 02:28:04 +0530

    Time: 8 ms (63.74%), Space: 7.6 MB (41.03%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,44 +1,23 @@
-class Solution {  
-     vector<vector<string>>ans;
+class Solution { 
+    private:
     int n;
-    private: 
-    bool isvalid(vector<string>&ds,int row,int col){
-         int i=row;
-         int j=col;
-        while(j>=0){
-            if(ds[i][j]=='Q')
-                 return 0;
-            j--;
-        }
-        i=row;
-        j=col;
-        while(i>=0 && j>=0){
-            if(ds[i][j]=='Q') 
-                return 0;
-            i--; 
-            j--;
-        } 
-        i=row;
-        j=col;
-        while(i<n && j>=0){
-            if(ds[i][j]=='Q') 
-                return 0;
-            i++;
-            j--;
-        }
-        return 1;
-    }
-    
-    void solve(int col,vector<string>&ds){
-        if(col==n){
+    vector<vector<string>>ans;
+    void solve(int col,vector<string>&ds,vector<int>&left,vector<int>&right, vector<int>&up){
+        if(col>=n){
             ans.push_back(ds);
             return ;
         }
         for(int i=0;i<n;++i){
-            if(isvalid(ds,i,col)){
-                ds[i][col]='Q';
-                solve(col+1,ds);
+            if(left[i]==0 && up[i+col]==0 && right[n-1+col-i]==0){
+                ds[i][col]='Q'; 
+                left[i]=1;
+                up[i+col]=1;
+                right[n-1+col-i]=1;
+                solve(col+1,ds,left,right,up);
                 ds[i][col]='.';
+                left[i]=0;
+                up[i+col]=0;
+                right[n-1+col-i]=0;
             }
         }
         return ;
@@ -47,11 +26,14 @@ public:
     vector<vector<string>> solveNQueens(int n) { 
         this->n=n;
         vector<string>ds(n);
-        string ss(n,'.');
+        string s(n,'.');
         for(int i=0;i<n;++i){
-             ds[i]=ss;
+            ds[i]=s;
         }
-        solve(0,ds);
+        vector<int>left(n,0);
+        vector<int>up(2*n+1,0);
+        vector<int>right(2*n+1,0);
+        solve(0,ds,left,right,up); 
         return ans;
     }
 };
