7999b57edf233681a9daf4ce5accf0e2acbd73f7
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-02-04 04:36:53 +0530

    Time: 13 ms (61.72%), Space: 10.8 MB (31.22%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,51 +1,23 @@
 class Solution {
 public:
-    string convert(string s, int num) { 
-        int n=s.size(); 
-        if(num==1)
-            return s;
-      vector<vector<char>>ds(num,vector<char>(n+1,'*'));
-      bool flag=0;
-      int i=0;
-        int j=0;
-        int k=0; 
-        int l=0;
-        while(k<s.size()){
-            if(!flag){  
-                while(i<num && k<s.size()){
-                    ds[i][j]=s[k];
-                    // cout<<i<<".."<<j<<"..."<<s[k]<<endl;
-                    i++;  
-                    // cout<<s[k]<<"..";
-                    k++;
-                }
-                flag=!flag;
-                // cout<<endl;
-            }
-            else{   
-                i--;
-                while(i>0  && k<s.size()){ 
-                    i--;
-                    j++;
-                    ds[i][j]=s[k];
-                    // cout<<i<<"***"<<j<<"**"<<s[k]<<endl;
-                    if(i>0  )
-                    k++; 
-                    l=1;
-                }
-                flag=!flag;
-                // cout<<endl;
+    string convert(string s, int num) {
+         vector<string>ds(num);
+          int j=0; 
+        bool flag=0;
+        for(int i=0;i<s.size();++i){
+              ds[j]+=s[i];
+            if((j==0 || flag==0) && j!=num-1){
+                j++; 
+                flag=0;
+            } 
+            else if((j==num-1 || flag) && j!=0){
+                  j--;
+                flag=1;
             }
         }
         string ans="";
-        for(int i=0;i<num;++i){
-            for(int j=0;j<n;++j) {
-                if(ds[i][j]!='*')
-                    ans+=ds[i][j];
-                // cout<<ds[i][j];
-            }
-            // cout<<endl;
-        }
+        for(auto itr: ds) ans+=itr;
         return ans;
+        
     }
 };
