1348728c6fbb7fc752ea05ea2238ddbc0b41c272
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-02-08 04:22:43 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (41.98%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=(low+high)>>1;
+            // cout<<mid<<"(**)"<<endl;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<nums[mid]){
+                    high=mid-1;
+                }
+                else
+                    low=mid+1;
+            }
+            else{
+                 if(nums[mid]<nums[high]){
+                     if(target>nums[mid] && target<=nums[high])
+                         low=mid+1;
+                     else
+                         high=mid-1;
+                 }
+                else
+                    high=mid-1;
+            }
+        }
+        return -1;
+    }
+};
