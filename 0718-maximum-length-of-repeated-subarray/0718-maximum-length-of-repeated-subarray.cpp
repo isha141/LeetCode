415c6772b3dabcaab853d25b890733eaec0fc283
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-02-11 07:04:12 +0530

    Time: 280 ms (49.51%), Space: 85.6 MB (21.85%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -20,8 +20,17 @@ public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         n=nums1.size();
         m=nums2.size(); 
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-         solve(0,0,nums1,nums2,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=n-1;i>=0;--i){ 
+            for(int j=m-1;j>=0;--j){
+                 int c=0;
+                if(nums1[i]==nums2[j]){
+                    c=1+dp[i+1][j+1];
+                    ans=max(ans,c);
+                }
+                dp[i][j]=c;
+            }
+        }
         return ans;
     }
 };
