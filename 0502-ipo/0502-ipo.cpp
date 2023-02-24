09f27fc613d24f9ac9c0a0b69330eae613bd53ed
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-02-24 05:53:55 +0530

    Time: 232 ms (68.17%), Space: 82.1 MB (53.68%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        int n = profits.size();
-        vector<pair<int, int>> projects;
-        for (int i = 0; i < n; i++) {
-            projects.emplace_back(capital[i], profits[i]);
+    int findMaximizedCapital(int k, int w, vector<int>& p, vector<int>& c) {
+        int n=p.size();
+        int j=0; 
+        vector<pair<int,int>>ds; 
+        for(int  i=0;i<n;++i){
+            ds.push_back({c[i],p[i]});
         }
-        sort(projects.begin(), projects.end());
-        priority_queue<int> q;
-        int ptr = 0;
-        for (int i = 0; i < k; i++) {
-            while (ptr < n && projects[ptr].first <= w) {
-                q.push(projects[ptr++].second);
+        priority_queue<int>pq;
+        sort(ds.begin(),ds.end());
+        for(int i=0;i<k;++i){
+            while(j<n && ds[j].first<=w){
+                pq.push(ds[j++].second);
             }
-            if (q.empty()) {
+            if(pq.size()==0)
                 break;
-            }
-            w += q.top();
-            q.pop();
+            w+=pq.top();
+            pq.pop();
         }
         return w;
     }
