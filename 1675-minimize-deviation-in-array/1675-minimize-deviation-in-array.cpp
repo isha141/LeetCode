237800e11442c51a91fde88a7c4018f83d405457
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-02-25 14:27:34 +0530

    Time: 595 ms (43.70%), Space: 56.8 MB (72.27%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumDeviation(vector<int>& nums) {
+        int n=nums.size(); 
+        priority_queue<int>pq;
+        int mini=1e9;
+        for(auto itr: nums){
+            if(itr%2){
+                itr=itr*2;
+                //pq.push(itr*2);
+               // mini=min(mini,itr*2);
+            }
+            //else{
+                mini=min(mini,itr);
+               pq.push(itr);
+           // }
+        }
+        int ans=1e9;
+        while(pq.top()%2!=1){
+            auto itr=pq.top();
+             // pq.pop();
+              ans=min(ans,pq.top()-mini);
+            mini=min(mini,itr/2);
+            pq.pop();
+            pq.push(itr/2);
+        }
+        return min(ans,pq.top()-mini);
+    }
+};
