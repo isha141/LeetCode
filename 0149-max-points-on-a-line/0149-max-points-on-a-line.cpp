80499568f941d135b34309bfccfe0082ab656674
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-02-25 15:57:02 +0530

    Time: 27 ms (87.52%), Space: 12.7 MB (54.57%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -2,20 +2,28 @@ class Solution {
 public:
     int maxPoints(vector<vector<int>>& p) {
         int n=p.size();
-        int ans=0; 
+        int ans=0;
         // sort(p.begin(),p.end());
         for(int i=0;i<n;++i){
-            map<double,int>mp;
+              map<double,int>mp; 
             for(int j=i+1;j<n;++j){
-                 int y=(p[j][0]-p[i][0]);  
-                if(y==0)
-                { mp[INT_MAX]++; ans=max(ans,mp[INT_MAX]);
-                 continue;
-                }
-                 double slope=(double)((p[j][1]-p[i][1]))/(y);
-                 mp[slope]++;
-                 ans=max(ans,mp[slope]);
-            }
+                // if(i!=j){
+                    int x1=p[i][0];
+                    int y1=p[i][1];
+                    int x2=p[j][0];
+                    int y2=p[j][1]; 
+                    if((x2-x1)==0) {
+                        mp[INT_MAX]++;
+                         ans=max(ans,mp[INT_MAX]); 
+                        continue;
+                    }
+                    double slope=(double)(y2-y1)/(x2-x1);
+                    mp[slope]++;
+                     ans=max(ans,mp[slope]); 
+                } 
+            // cout<<i<<"************"<<endl;
+              // for(auto itr: mp)
+                  // cout<<itr.first<<" "<<itr.second<<endl;
         }
         return ans+1;
     }
