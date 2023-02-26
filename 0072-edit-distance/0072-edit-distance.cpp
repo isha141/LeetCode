2e5e8fe1478faee5b0f59a126c0f0103345b4299
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-02-26 17:40:19 +0530

    Time: 54 ms (21.00%), Space: 7.2 MB (88.46%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -23,7 +23,23 @@ public:
     int minDistance(string w1, string w2) {
             n=w1.size();
             m=w2.size(); 
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,w1,w2);
+        memset(dp,0,sizeof(dp));  
+        for(int i=0;i<=n;++i){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;++j){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;++i){ 
+            for(int j=1;j<=m;++j){
+                if(w1[i-1]==w2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                } 
+                else{
+                    dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
