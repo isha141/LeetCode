a3ed14ea73de65103ec4080aff6fe5b379cbfa01
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-03-02 10:55:42 +0530

    Time: 6 ms (44.86%), Space: 11.7 MB (74.13%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution { 
+    private:
+    vector<string>ans; 
+    void solve(int open,int close,int n,string &s){
+        if(s.size()>=2*n){
+            ans.push_back(s);
+            return ;
+        }
+        if(open<n){
+            s+='(';
+            solve(open+1,close,n,s); 
+            s.pop_back();
+        }
+        if(open!=0 && open>close && close<n){
+            s+=')';
+            solve(open,close+1,n,s);
+            s.pop_back();
+        }
+        return ;
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        if(n==0)
+              return {}; 
+        string s="";
+        solve(0,0,n,s);
+        return ans;
+    }
+};
