16d7f0514721d7d43eed65335a11fb2372591f0e
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-03-13 10:23:52 +0530

    Time: 296 ms (74.83%), Space: 123.6 MB (12.50%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) { 
-         vector<vector<int>>ans;
-        int i=0;
-        int j=0;
-        int n=nums1.size();
-        int m=nums2.size();
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        for(int i=0;i<n;++i){
-            pq.push({nums1[i]+nums2[0],{i,0}});
+    vector<vector<int>> kSmallestPairs(vector<int>& n1, vector<int>& n2, int k) {
+         vector<vector<int>> ans; 
+priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        for(int i=0;i<n2.size();++i){
+            pq.push({n1[0]+n2[i],{0,i}});
         }
-        j=1;
         while(!pq.empty() && ans.size()!=k){
-            auto itr=pq.top();
-            pq.pop();
-            int x=itr.second.first;
-            int y=itr.second.second;
-            ans.push_back({nums1[x],nums2[y]});
-            if(y!=m-1){
-                pq.push({nums1[x]+nums2[y+1],{x,y+1}});
-                // j++;
+             auto itr=pq.top();
+              pq.pop();
+             int i=itr.second.first;
+            int j=itr.second.second;
+           vector<int>ds={n1[i],n2[j]}; 
+             if(i+1<n1.size()){
+                pq.push({n2[j]+n1[i+1],{i+1,j}});
             }
-        }
+            ans.push_back(ds);  
+        } 
         return ans;
     }
 };
