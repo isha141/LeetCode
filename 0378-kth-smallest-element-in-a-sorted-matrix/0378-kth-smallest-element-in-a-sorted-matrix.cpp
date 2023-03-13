c61090647a72786466ef3067bbf4588c3a2feaa3
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-03-13 06:38:39 +0530

    Time: 29 ms (86.52%), Space: 13.2 MB (84.10%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,41 +1,39 @@
-class Solution { 
-    private: 
-    int n; 
-    int m;
-    int count(int mid,vector<vector<int>>&mat){
-         int c=0;
-         int j=0;
-          for(int i=0;i<n;++i){
-              if(mat[i][m-1]<=mid){
-                  c+=m;
-              } 
-              else {
-                  int j=m-1;
-                  while(j>=0 && mat[i][j]>mid)
-                      j--;
-                  c+=j+1;
-              }
-          }
-        return c;
+class Solution {  
+    private:
+    int n,m;
+    int ok(int mid,vector<vector<int>>&mat){
+        int ans=0;
+        for(int i=0;i<n;++i){
+            if(mat[i][m-1]<=mid){
+                ans+=m;
+            }
+            else{
+                int j=m-1;
+                while(j>=0 && mat[i][j]>mid)
+                    j--;
+                ans+=(j+1);
+            }
+        }
+        return ans;
     }
 public:
     int kthSmallest(vector<vector<int>>& mat, int k) {
-         n=mat.size();
-         m=mat[0].size(); 
+            n=mat.size();
+            m=mat[0].size();
         int low=mat[0][0];
-        int high=mat[n-1][n-1]; 
+        int high=mat[n-1][m-1]; 
         int ans=0;
         while(low<=high){
-            int mid=(low+high)>>1; 
-            if(count(mid,mat)>=k){
+            int mid=(low+high)>>1;  
+            if(ok(mid,mat)>=k){ 
+                // cout<<mid<<"::"<<endl;
                 ans=mid;
                 high=mid-1;
             }
             else{
-                  low=mid+1;
+                low=mid+1;
             }
         }
         return ans;
-        
     }
 };
