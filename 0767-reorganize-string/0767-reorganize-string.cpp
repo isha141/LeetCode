4292891d6f8ca6f10cf14536ab5d8f2643eae24a
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-03-15 13:01:44 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (35.85%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -3,39 +3,41 @@ public:
     string reorganizeString(string s) {
         int n=s.size();
         map<char,int>mp;
-        for(auto itr: s){ 
+        for(auto itr: s)
             mp[itr]++;
-        if(mp[itr]>(n+1)/2)
-                return "";
+        for(auto itr:mp){
+            if(itr.second>(n+1)/2)
+                  return "";
         }
-        priority_queue<pair<int,char>>pq; 
+        string ans(n,'.');  
+        priority_queue<pair<int,char>>pq;
         for(auto itr: mp){
             pq.push({itr.second,itr.first});
-        }
-        string ans(n,'.'); 
-             auto itr=pq.top();
-             pq.pop();
-        int val=itr.first;
-        char ch=itr.second;
-        int i=0;
-            while(pq.size() || val){ 
-               if(ans[i]=='.'){
-                   ans[i]=ch;
-                   i=(i+2)%n;
-                   val--;
-                if(val==0) {
-                    if(pq.size()){
-                        auto itr=pq.top();
-                        pq.pop();
-                        ch=itr.second;
-                        val=itr.first;
-                    }
-                    else break;
+        }  
+        int k=0;
+        int res=2; 
+         int j=k; 
+        while(!pq.empty()){
+            auto itr=pq.top();
+            pq.pop();
+            char ch=itr.second;
+            int m=itr.first;
+            bool flag=0;
+            while(j<n && m){
+                  ans[j]=ch;
+                j+=res;
+                m--; 
+                // cout<<j<<" "<<m<<endl;
+                if(j>=n && m>0){
+                    j=++k;
+                    flag=1;
                 }
-               }
-                else
-                    i=(i+1)%n;
-            }  
+            }
+            if(!flag && j>=n){
+                k++; 
+                j=k;
+            }
+        }
         return ans;
     }
 };
