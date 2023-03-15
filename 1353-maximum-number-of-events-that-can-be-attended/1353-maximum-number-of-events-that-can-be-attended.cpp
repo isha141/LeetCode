a3f335e0e1cb44683b0a8d1536799692bfaca7fb
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-03-16 01:39:50 +0530

    Time: 2419 ms (7.01%), Space: 428.2 MB (6.11%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& e) {
-       int n=e.size();
-        int ans=0;
-        vector<pair<int,int>>ds;
-        for(auto itr: e){
-            ds.push_back({itr[0],itr[1]});
-        }
-        sort(ds.begin(),ds.end(),[&](pair<int,int>&a,pair<int,int>&b){ 
-            if(a.second!=b.second)
-            return a.second<b.second; 
-            return a.first<b.first;
-        }); 
+        sort(e.begin(),e.end() ,[&](vector<int>&a,vector<int>&b){
+            if(a[0]==b[0])
+                 return a[1]<b[1];
+            return a[1]<b[1];
+        });
         set<int>s;
-        for(int i=1;i<=100000;++i){
+        for(int i=1;i<=100000;++i)
             s.insert(i);
-        }
-        for(auto itr: ds){
-            int start=itr.first;
-            int end=itr.second;
-            auto itt=s.lower_bound(start);
-            if(itt==s.end() || (*itt)>end) continue;
-            // cout<<start<<"::"<<(*itt)<<endl;
-            s.erase(*(itt));
+        int ans=0;
+        for(auto itr: e){
+            int start=itr[0];
+            int end=itr[1];
+            auto it=s.lower_bound(start);
+            if(it==s.end() || (*it)>end) continue;
             ans++;
-          } 
+            s.erase((*it));
+        }
         return ans;
     }
 };
