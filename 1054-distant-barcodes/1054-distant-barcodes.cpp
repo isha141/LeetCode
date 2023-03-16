31c57dc49f1ef895563c2e000e5db98697311e3e
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-03-16 08:53:48 +0530

    Time: 111 ms (66.58%), Space: 42.4 MB (67.26%) - LeetHub

---
+++ 1054-distant-barcodes/1054-distant-barcodes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> rearrangeBarcodes(vector<int>& b) {
+        int n=b.size();
+        vector<int> ans(n,0);
+        map<int,int>mp;
+        for(auto itr: b)
+            mp[itr]++;
+        priority_queue<pair<int,int>>pq;
+        for(auto itr: mp){
+            pq.push({itr.second,itr.first});
+        }
+        int val=pq.top().first;
+        int a=pq.top().second;
+        pq.pop(); 
+        int i=0;
+        while(!pq.empty() || val){  
+            // cout<<i<<"::"<<val<<endl;
+              if(ans[i]==0){
+                  ans[i]=a;
+                  i=(i+2)%n;
+                  val--;
+              if(val==0){ 
+                  if(pq.size()){
+                  a=pq.top().second;
+                  val=pq.top().first;  
+                      pq.pop();
+                  } 
+                  else
+                      break;
+              } 
+              }
+              else{
+                  i=(i+1)%n;
+              }
+        }
+        return ans;
+        
+    }
+};
