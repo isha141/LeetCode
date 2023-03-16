c46025b38ac5f523967c728a53ac1e1d1b9d365d
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-03-16 19:51:16 +0530

    Time: 241 ms (37.08%), Space: 47.4 MB (17.92%) - LeetHub

--- 1705-maximum-number-of-eaten-apples/1705-maximum-number-of-eaten-apples.cpp
+++ 1705-maximum-number-of-eaten-apples/1705-maximum-number-of-eaten-apples.cpp
@@ -3,10 +3,14 @@ public:
     int eatenApples(vector<int>& a, vector<int>& days) {
         int n=a.size();
         int ans=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq; 
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;  
+        int i=0;
         int day=1;
-        for(int i=0;i<n;++i){ 
-            pq.push({days[i]+i,a[i]}); 
+         while(1){ 
+             if(i<n){
+            pq.push({days[i]+i,a[i]});  
+                 i++;
+             }
              while(!pq.empty() && pq.top().first<day)
                  pq.pop();
             if(pq.size()){
@@ -19,28 +23,12 @@ public:
                     pq.push({valid,app-1});
                 }
             }
+             if(i>=n && pq.size()==0) return ans;
             day++;
         } 
-        while(!pq.empty()){
-            while(!pq.empty() && pq.top().first<day)
-                pq.pop();
-            if(pq.size()){
-                ans+=1;
-                auto itr=pq.top();
-                pq.pop();
-                int app=itr.second;
-                int valid=itr.first;
-                if(app>1){
-                    pq.push({valid,app-1});
-                }
-            }
-            day++;
-        }
-        return ans;
-        // for(int i=1;i<=100000;++i){
-        //     while(!pq.empty() && pq.top().first<i){
+        // while(!pq.empty()){
+        //     while(!pq.empty() && pq.top().first<day)
         //         pq.pop();
-        //     }
         //     if(pq.size()){
         //         ans+=1;
         //         auto itr=pq.top();
@@ -51,9 +39,8 @@ public:
         //             pq.push({valid,app-1});
         //         }
         //     }
-        //     else
-        //         return ans;
+        //     day++;
         // }
-        // return ans;
+        return ans;
     }
 };
