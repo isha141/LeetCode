cf803974e4c37a354133d02c620b1e2ffa8d767c
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-03-21 00:46:48 +0530

    Time: 1021 ms (23.08%), Space: 36.3 MB (23.08%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,25 @@
+class Solution { 
+    private:
+    int n;  
+    unordered_map<int,int>mp;
+    int solve(int i,vector<int>&nums,int &k){ 
+          int l=0; 
+        int r=0;
+        if(i>=n) return 1; 
+        else{
+        if(mp[nums[i]-k]==0 && !mp[nums[i]+k]){ 
+            mp[nums[i]]++;
+         l=solve(i+1,nums,k);
+         mp[nums[i]]--;
+        }
+         r=solve(i+1,nums,k);  
+        }
+        return l+r;
+    }
+public:
+    int beautifulSubsets(vector<int>& nums, int k) {
+        n=nums.size();  
+        vector<int>ds;
+        return solve(0,nums,k)-1;
+    }
+};
