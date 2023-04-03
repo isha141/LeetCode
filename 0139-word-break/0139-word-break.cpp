5889d8a70f116a96e575f40459acebef42bf0fd2
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-04-04 01:21:26 +0530

    Time: 8 ms (69.08%), Space: 8.2 MB (78.17%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,32 +1,29 @@
+int dp[1007];
 class Solution { 
-    private: 
-    int n;
-     map<string,int>mp;
-    bool solve(int i,string &s,vector<int>&dp){
-        if(i>=n)
-             return 1; 
-        if(dp[i]!=-1) 
+    private:
+     map<string,int>mp; 
+    bool solve(int i,string &s){
+        if(i>=s.size())
+             return 1;  
+        if(dp[i]!=-1)
             return dp[i];
         string temp="";
-        for(int j=i;j<n;++j){
-            temp+=s[j];
+        for(int k=i;k<s.size();++k){
+             temp+=s[k];
             if(mp.find(temp)!=mp.end()){
-                if(solve(j+1,s,dp)){
-                    dp[i]=1;
-                    return dp[i];
-                }
-                    
+                dp[i]=1;
+                if(solve(k+1,s))    
+                return dp[i];
             }
         }
         return dp[i]=0;
     }
 public:
-    bool wordBreak(string s, vector<string>& word) { 
-        n=s.size();
-        for(auto itr: word){
+    bool wordBreak(string s, vector<string>& word) {
+        // map<string,int>mp;  
+        memset(dp,-1,sizeof(dp));
+        for(auto itr: word)
             mp[itr]++;
-        } 
-        vector<int>dp(n,-1);
-        return  solve(0,s,dp);
+        return solve(0,s);
     }
 };
