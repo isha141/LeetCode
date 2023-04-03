91f63605a208e0a2e978a8e3f678baffa93839c5
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-04-03 11:29:52 +0530

    Time: 114 ms (5.33%), Space: 42 MB (11.29%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,44 +1,40 @@
 class Solution { 
-    private: 
-    int n;
+    private:
     bool ok(int mid,vector<int>&p,int limit){
         int c=0;
-        int w=0;
-        int i=0;
-        int j=n-1;
-        while(i<=j){
-            if(p[i]+p[j]<=limit){
-                i++;
-                j--;
-                c++;
-                continue;
-            }
-            else{
+        int sum=0; 
+        int n=p.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            if(low!=high && p[low]+p[high]<=limit){
+                low++;
+                high--;
+               c++; 
+            } 
+            else {
+                high--;
                 c+=1;
-                j--;
             }
         }
         return c<=mid;
     }
 public:
-    int numRescueBoats(vector<int>& p, int limit) { 
-        n=p.size();
-        sort(p.begin(),p.end());
+    int numRescueBoats(vector<int>& p, int limit) {
+        int n=p.size();
         int low=1;
-        int high=n;
-        int ans=0;
-        // for(auto itr: p) high+=itr;
+        int high=n; 
+        int ans=n; 
+        sort(p.begin(),p.end());
         while(low<=high){
             int mid=(low+high)>>1;
             if(ok(mid,p,limit)){
                 ans=mid;
                 high=mid-1;
             }
-            else{
+            else 
                 low=mid+1;
-            }
         }
         return ans;
-        
     }
 };
