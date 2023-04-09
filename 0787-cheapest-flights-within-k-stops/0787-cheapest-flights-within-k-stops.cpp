07d8dc7462ab5dbb37c18222e34e9f4fc485fe59
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-04-09 17:51:31 +0530

    Time: 21 ms (82.43%), Space: 12.8 MB (93.35%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& f, int src, int dst, int k) { 
+    int findCheapestPrice(int n, vector<vector<int>>& f, int src, int des, int k) {
         vector<pair<int,int>>adj[n];
         for(int i=0;i<f.size();++i){
             adj[f[i][0]].push_back({f[i][1],f[i][2]});
-        } 
-        k+=2; 
+        }
+        k+=2;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+         pq.push({1,{0,src}});
         int ans=1e9; 
         vector<int>dis(n,1e9);
         dis[src]=0;
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        pq.push({1,{0,src}});
         while(!pq.empty()){
-             auto itr=pq.top();
-             pq.pop();
+            auto itr=pq.top();
+            pq.pop();
             int steps=itr.first;
             int cost=itr.second.first;
-            int node=itr.second.second;   
-            if(steps>k) continue;
-            if(node==dst && steps<=k){ 
-                // cout<<k<<"::"<<cost<<endl;
-                ans=min(ans,cost);
-            } 
-            if(steps>=k) continue;
-            for(auto itr: adj[node]){
-                int nm=itr.first;
-                int c=itr.second;
-                if(dis[nm]>cost+c && steps+1<=k){
-                    dis[nm]=cost+c;
-                    pq.push({steps+1,{dis[nm],nm}});
+            int node=itr.second.second; 
+            if(steps<=k){
+                if(node==des){
+                    ans=min(ans,cost);
                 }
-                
+            }
+            if(steps>=k)  
+                continue;
+            for(auto itt: adj[node]){
+                if(dis[itt.first]>cost+itt.second ){
+                    dis[itt.first]=cost+itt.second;
+                    pq.push({steps+1,{dis[itt.first],itt.first}});
+                }
             }
         }
         return ans==1e9?-1:ans;
