404c371a526526b43f3ef1ea07acb77b9e199534
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-04-09 18:32:52 +0530

    Time: 43 ms (87.47%), Space: 14.8 MB (95.60%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -2,28 +2,28 @@ int dp[205][205];
 class Solution { 
     private:
     int n,m; 
-    int solve(int i,int j,vector<vector<int>>&mat,int prev){
-          if(i<0 || j<0|| i>=n || j>=m || prev>=mat[i][j])
-                return 0;
-        if(dp[i][j]!=-1)
-              return dp[i][j];
-        return dp[i][j]=1+max({solve(i+1,j,mat,mat[i][j]),
-                             solve(i-1,j,mat,mat[i][j]),
-                                 solve(i,j+1,mat,mat[i][j]),
-                                     solve(i,j-1,mat,mat[i][j])});
+    int solve(int i,int j,vector<vector<int>>&grid,int prev){
+        if(i<0 || i>=n || j<0 || j>=m || prev>=grid[i][j])
+              return 0;
+        if(dp[i][j]!=-1) 
+            return dp[i][j];
+        
+       return dp[i][j]=1+max({solve(i+1,j,grid,grid[i][j]),
+                             solve(i-1,j,grid,grid[i][j]),
+                             solve(i,j+1,grid,grid[i][j]),
+                            solve(i,j-1,grid,grid[i][j])}); 
     }
 public:
     int longestIncreasingPath(vector<vector<int>>& mat) {
-          n=mat.size();
-          m=mat[0].size(); 
-        int ans=0; 
+        n=mat.size();
+        m=mat[0].size();
         memset(dp,-1,sizeof(dp));
-          for(int i=0;i<n;++i){
-              for(int j=0;j<m;++j){ 
-                  ans=max(ans,solve(i,j,mat,INT_MIN)); 
-                  // cout<<i<<"::"<<j<<endl;
-              }
-          }
+        int ans=0;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                ans=max(ans,solve(i,j,mat,INT_MIN));
+            }
+        }
         return ans;
     }
 };
