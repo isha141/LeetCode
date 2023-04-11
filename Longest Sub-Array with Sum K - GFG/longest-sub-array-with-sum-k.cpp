46c6b5a33b1bfd716b3d62f8a05a7bfc6feb76c3
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-04-11 13:01:33 +0530

    Added solution - LeetHub

--- Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
@@ -12,7 +12,7 @@ class Solution{
         int sum=0;
         int ans=0;
         map<int,int>mp; 
-        mp[0]=-1;
+        mp[0]=0;
         for(int i=0;i<n;++i){
             sum+=a[i];
             if(sum==k){
