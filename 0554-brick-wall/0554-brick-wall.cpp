f3fee46945167973e432e89ebfb45bb61fbda821
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-04-12 02:26:14 +0530

    Time: 47 ms (69.75%), Space: 24 MB (6.16%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.cpp
+++ 0554-brick-wall/0554-brick-wall.cpp
@@ -1,24 +1,19 @@
-class Solution { 
-    private:
-    int n,m;
+class Solution {
 public:
-    int leastBricks(vector<vector<int>>& wall) {
-         n=wall.size();
-         m=wall[0].size();
-        int maxi=0; 
-        long long mini=1e9;
+    int leastBricks(vector<vector<int>>& w) {
         map<int,int>mp;
-         for(int i=0;i<n;++i){
-             int sum=0;
-             map<int,int>ds;
-             for(int j=0;j<wall[i].size()-1;++j){ 
-                 sum+=wall[i][j];
-                 mp[sum]++;
-             }
-         }
+        int n=w.size();
+        for(auto itr: w){
+            int sum=0;
+            for(auto i=0;i<itr.size()-1;++i){
+                 sum+=itr[i];
+                mp[sum]++;
+            }
+        }
         int ans=0;
-        for(auto itr: mp)
+        for(auto itr: mp){
             ans=max(ans,itr.second);
+        }
         return n-ans;
     }
 };
