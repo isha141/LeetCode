953682e8cf3bc7727ac5b7423b441cfa38472301
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-04-16 00:28:05 +0530

    Time: 4 ms (53.94%), Space: 11.1 MB (46.13%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -11,40 +11,38 @@
  */
 class Solution { 
     private:
-   bool solve(TreeNode* root,int x,int y){
-        queue<TreeNode *>q;
-        q.push(root); 
-        map<int,pair<int,int>>mp; 
-        int level=0;
+    bool solve(TreeNode * root,int x,int y){
+        queue<TreeNode*>q;
+        q.push(root);
         while(!q.empty()){
-            int n=q.size();
-            while(n--){ 
-                auto itr=q.front(); 
+            int n=q.size(); 
+            bool fx=0;
+            bool fy=0;
+            while(n--){
+                auto itr=q.front();
                 q.pop();
-               TreeNode * node=itr;
-                if(node->left){
-                    mp[node->left->val]={node->val,level}; 
-                    q.push(node->left);
+                 if(itr->val==x) 
+                     fx=1;
+                if(itr->val==y){
+                    fy=1;
                 }
-                if(node->right){
-                    mp[node->right->val]={node->val,level};
-                     q.push(node->right);
+                if(itr->left && itr->right){
+                    if((itr->left->val==x && itr->right->val==y) || (itr->right->val==x && itr->left->val==y))
+                          return 0;
                 }
+                if(itr->left)
+                    q.push(itr->left);
+                if(itr->right)
+                    q.push(itr->right);
             }
-            level++;
+            if(fx && fy)
+                return 1;
         }
-        if(mp.find(x)==mp.end() || mp.find(y)==mp.end()){
-            return 0;
-        }
-        auto it1=mp[x];
-        auto it2=mp[y];
-        if(it1.first!=it2.first && it1.second==it2.second)
-               return 1;
         return 0;
     }
 public:
     bool isCousins(TreeNode* root, int x, int y) {
-        if(root==NULL) return 0;
+        if(root==NULL) return 1;
         return solve(root,x,y);
     }
 };
