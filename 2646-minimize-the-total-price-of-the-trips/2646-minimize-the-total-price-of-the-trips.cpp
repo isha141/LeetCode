ffae7386499b0d7d9c74ae10c01cb1f3be9fe58f
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-04-16 22:45:10 +0530

    Time: 70 ms (36.36%), Space: 39.9 MB (36.36%) - LeetHub

---
+++ 2646-minimize-the-total-price-of-the-trips/2646-minimize-the-total-price-of-the-trips.cpp
@@ -0,0 +1,45 @@
+class Solution { 
+    private: 
+    int dfs(int u,vector<int>adj[],int v,map<int,int>&mp,int p=-1){
+         if(u==v){
+             mp[u]++;
+        return 1;} 
+        for(auto itr: adj[u]){
+            if(itr==p)  continue;
+            if(dfs(itr,adj,v,mp,u)){
+                mp[u]++;
+                return 1;
+            }
+        }
+        return 0;
+    } 
+      pair<int,int> dfs2(int node,int par,vector<int>&p,vector<int>adj[],map<int,int>&mp){
+        int consider=(p[node]/2)*mp[node];
+        int dont_consider=(p[node])*mp[node];
+          for(auto itr: adj[node]){
+              if(itr!=par){
+                    auto iit=dfs2(itr,node,p,adj,mp);
+                  consider+=iit.second;
+                  dont_consider+=min(iit.first,iit.second);
+              }
+          }
+        return {consider,dont_consider};
+   }
+public:
+    int minimumTotalPrice(int n, vector<vector<int>>& e, vector<int>& p, vector<vector<int>>& t) { 
+          vector<int>adj[n+1]; 
+         map<int,int>mp;
+          for(int i=0;i<e.size();++i){
+              adj[e[i][0]].push_back(e[i][1]);
+              adj[e[i][1]].push_back(e[i][0]);
+          }
+          for(auto itr: t){
+              dfs(itr[0],adj,itr[1],mp);
+          }   
+        for(auto itr: mp) 
+            cout<<itr.first<<",,"<<itr.second<<endl;
+          auto itr=dfs2(0,-1,p,adj,mp); 
+        cout<<itr.first<<":::"<<itr.second<<endl;
+         return min(itr.first,itr.second);
+    }
+};
