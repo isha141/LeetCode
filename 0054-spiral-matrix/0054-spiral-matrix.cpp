d545490e775ecf99ac38f798179367fbe42f6636
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-05-09 13:09:23 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (91.37%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-         vector<int>ans;
-        int n=matrix.size();
-        int m=matrix[0].size();
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+         int n=mat.size();
+        int m=mat[0].size();
+        vector<int>ans;
         int row_start=0;
         int row_end=n-1;
         int col_start=0;
         int col_end=m-1;
         while(row_start<=row_end && col_start<=col_end){
-            for(int i=row_start;i<=col_end;++i){
-                ans.push_back(matrix[row_start][i]);
+            for(int i=col_start;i<=col_end;++i){
+                ans.push_back(mat[row_start][i]);
             }
-            row_start++;
+              row_start++;
             for(int i=row_start;i<=row_end;++i){
-                ans.push_back(matrix[i][col_end]);
+                ans.push_back(mat[i][col_end]);
             }
-            col_end--;
+            col_end--; 
             if(row_start<=row_end){
-                for(int i=col_end;i>=col_start;--i){
-                    ans.push_back(matrix[row_end][i]);
-                }
+            for(int i=col_end;i>=col_start;i--){
+                ans.push_back(mat[row_end][i]);
             }
             row_end--;
+            }
             if(col_start<=col_end){
-                for(int i=row_end;i>=row_start;--i){
-                    ans.push_back(matrix[i][col_start]);
-                }
+            for(int i=row_end;i>=row_start;--i){
+                ans.push_back(mat[i][col_start]);
             }
             col_start++;
+            }
         }
-        return ans;
+         return ans;
     }
 };
