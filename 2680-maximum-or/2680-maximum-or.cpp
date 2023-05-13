f6e1bd9cfbfa76e4c19f63ce00cde37216f7746d
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-05-14 00:14:01 +0530

    Time: 1614 ms (22.22%), Space: 111.9 MB (22.22%) - LeetHub

---
+++ 2680-maximum-or/2680-maximum-or.cpp
@@ -0,0 +1,24 @@
+long long dp[100005][20];
+class Solution { 
+    private: 
+    int n;
+     long long solve(int i,vector<int>&nums,int k){
+          if(i>=n)
+               return 0; 
+         if(k<0) return 0;
+          if(dp[i][k]!=-1)
+               return dp[i][k];
+        long  long ans=solve(i+1,nums,k) |nums[i];
+          for(int j=1;j<=k;++j){
+              long long temp=(1ll*nums[i]<<j);
+              ans=max(ans,solve(i+1,nums,k-j)|temp);
+          }
+          return dp[i][k]=ans;
+     }
+public:
+    long long maximumOr(vector<int>& nums, int k) {
+         n=nums.size(); 
+        memset(dp,-1,sizeof(dp));
+         return solve(0,nums,k);
+    }
+};
