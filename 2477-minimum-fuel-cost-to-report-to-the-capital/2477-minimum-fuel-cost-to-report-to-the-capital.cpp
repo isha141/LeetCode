640620a5487d1cad65225ef953e8e9c1946d0288
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-06-07 00:58:56 +0530

    Time: 596 ms (85.96%), Space: 189 MB (79.60%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,31 +1,29 @@
 class Solution { 
     private: 
     long long ans=0;
-    int solve(int node,vector<int>Adj[],vector<int>&vis,int seats){
-        vis[node]=1;
-        long long c=1;
-        for(auto itr: Adj[node]){
-            if(vis[itr]==0){
-                c+=solve(itr,Adj,vis,seats);
-            }
-        }
-        int car=c/seats;
-        if(c%seats)
-            car++;
-        if(node!=0)
-            ans+=car;
-        return c;
-    }
+    long long solve(int node,vector<int>adj[],int seats,vector<int>&vis,int dis){
+          vis[node]=1;
+         int people=1;
+         for(auto itr: adj[node]){
+               if(!vis[itr]){
+                    people+=solve(itr,adj,seats,vis,dis+1);
+               }
+         }
+         if(node!=0){
+             ans+=(people+seats-1)/seats; 
+         }
+           return people;
+     }
 public:
-    long long minimumFuelCost(vector<vector<int>>& r, int seats) {
-        int n=r.size();
-        vector<int>adj[n+1];
-        for(int i=0;i<n;++i){
-            adj[r[i][0]].push_back(r[i][1]);
-            adj[r[i][1]].push_back(r[i][0]);
-        } 
-        vector<int>vis(n+1,0);
-        solve(0,adj,vis,seats);
+    long long minimumFuelCost(vector<vector<int>>& r, int seats) { 
+          int n=r.size();
+          vector<int>adj[n+1];
+           for(int i=0;i<n;++i){
+                 adj[r[i][0]].push_back(r[i][1]);
+                 adj[r[i][1]].push_back(r[i][0]);
+           } 
+          vector<int>vis(n+1,0);
+           solve(0,adj,seats,vis,0); 
         return ans;
     }
 };
