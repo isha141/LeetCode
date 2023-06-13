92e4cebdc0571005b6283cb3e44fe03cfe4e4563
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-06-14 00:15:19 +0530

    Time: 1530 ms (5.12%), Space: 304 MB (23.17%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -1,55 +1,42 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& des) {
           if(des.size()==0)
                 return NULL; 
         set<TreeNode *>s;
-          map<int,TreeNode *>mp; 
-          map<int,TreeNode*>parent; 
-          map<int,TreeNode*>parent1;
+          map<int,TreeNode *>mp,parent;
           int n=des.size();
           for(int i=0;i<n;++i){
                 int par=des[i][0];
-                int curr=des[i][1];
-                 TreeNode *current,*paren;
+                int curr=des[i][1]; 
+                // cout<<par<<";;"<<curr<<endl;
+                TreeNode *current,*paren;
                 if(mp.find(curr)!=mp.end()){
                       current=mp[curr];
+                    // cout<<"cuur->"<<" "<<current->val;
                 }     
                 else{
                      current=new TreeNode(curr);
                 }
-                if(parent.find(par)!=parent.end()){
-                     paren=parent[par];
-                }
-                else if(parent1.find(par)!=parent1.end()){
-                      paren=parent1[par];
+                if(mp.find(par)!=mp.end()){
+                    paren=mp[par]; 
+                    // cout<<"paren"<<paren->val<<"***";
                 }
                 else{
-                     paren=new TreeNode(par);
-                }
-                if(des[i][2]==0){
-                    paren->right=current;
+                    paren=new TreeNode(par);
                 } 
-                else{
+                if(des[i][2]==1){
                     paren->left=current;
                 }
+                else{
+                    paren->right=current;
+                }
                 mp[curr]=current; 
-                mp[par]=paren;
-                parent[curr]=current; 
-                parent1[paren->val]=paren;
-                s.insert(current);
+                mp[par]=paren; 
+                parent[curr]=current;
+                 s.insert(current);
                 s.insert(paren);
+              // cout<<"***********0"<<endl;
          } 
         TreeNode *root;
          for(auto itr=s.begin();itr!=s.end();++itr){
