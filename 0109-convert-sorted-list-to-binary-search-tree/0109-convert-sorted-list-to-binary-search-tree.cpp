8c89a1c5845613961273173b4082420547221998
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-06-13 23:14:57 +0530

    Time: 28 ms (69.15%), Space: 28.3 MB (55.76%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -19,29 +19,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution {  
     private:
-    TreeNode * solve(vector<int>&ds,int low,int high){
-         if(low>high)
-              return NULL;
-        int mid=(low+high)/2;
-        TreeNode * root=new TreeNode(ds[mid]);
-        root->left=solve(ds,low,mid-1);
-        root->right=solve(ds,mid+1,high);
-        return root;
+    ListNode *head;
+    TreeNode * solve(int low,int high){
+          if(low>high)
+               return NULL;
+          int mid=(low+high)>>1;
+          TreeNode *leftnode=solve(low+1,mid);
+          TreeNode * root=new TreeNode(head->val);
+          head=head->next;
+          root->left=leftnode;
+          TreeNode *rightnode=solve(mid+1,high);
+          root->right=rightnode;
+          return root;
     }
 public:
     TreeNode* sortedListToBST(ListNode* head) {
-        if(head==NULL)
-             return NULL; 
-      ListNode * temp=head;
-        int count=0;
-        vector<int>ds;
-        while(temp!=NULL){
-            count++;
-            ds.push_back(temp->val);
-            temp=temp->next;
-        }
-        return solve(ds,0,count-1);
+          if(head==NULL) return NULL;
+          int high=0; 
+           this->head=head;
+           while(head!=NULL){
+                 high+=1;
+                head=head->next;
+           }
+          return solve(0,high-1);
     }
 };
