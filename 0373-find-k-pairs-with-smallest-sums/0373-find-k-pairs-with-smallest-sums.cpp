6a4ff26a38f64d5cd2859eb8633d7b340f3b5c5a
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-06-27 17:29:06 +0530

    Time: 286 ms (84.73%), Space: 123.6 MB (13.13%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& n1, vector<int>& n2, int k) {
-         vector<vector<int>> ans; 
-priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        for(int i=0;i<n2.size();++i){
-            pq.push({n1[0]+n2[i],{0,i}});
-        }
-        while(!pq.empty() && ans.size()!=k){
-             auto itr=pq.top();
-              pq.pop();
-             int i=itr.second.first;
-            int j=itr.second.second;
-           vector<int>ds={n1[i],n2[j]}; 
-             if(i+1<n1.size()){
-                pq.push({n2[j]+n1[i+1],{i+1,j}});
-            }
-            ans.push_back(ds);  
-        } 
+         vector<vector<int>>ans;
+          int n=n1.size();
+          int m=n2.size(); 
+          // cout<<n<<"....."<<m<<endl;       
+         priority_queue<pair<long long ,pair<int,int>>,vector<pair<long long ,pair<int,int>>>,greater<pair<long long ,pair<int,int>>>>pq;
+          for(int i=0;i<n;++i){
+              pq.push({n2[0]+n1[i],{i,0}});
+          } 
+           while(k-- && pq.size()){
+               auto itr=pq.top();
+               pq.pop();
+               int first=itr.second.first;
+               int second=itr.second.second;
+               ans.push_back({n1[first],n2[second]}); 
+               // cout<<first<<";;;"<<second<<endl;
+               if((second+1)<m){ 
+                   // cout<<first<<";**;"<<second<<endl;
+               pq.push({n1[first]+n2[second+1],{first,second+1}}); 
+               } 
+           }
         return ans;
     }
 };
