4d69998f5cf694ff349b2113d2f659205c9d05c2
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-06-28 14:42:12 +0530

    Added solution - LeetHub

--- Sum of Middle Elements of two sorted arrays - GFG/sum-of-middle-elements-of-two-sorted-arrays.cpp
+++ Sum of Middle Elements of two sorted arrays - GFG/sum-of-middle-elements-of-two-sorted-arrays.cpp
@@ -12,22 +12,63 @@ class Solution {
 public:
     int findMidSum(int ar1[], int ar2[], int n) {
             // code here  
-            priority_queue<int,vector<int>,greater<int>>mini;
-            for(int i=0;i<n;++i){
-                mini.push(ar1[i]);
-                mini.push(ar2[i]);
+            int i=0;
+            int j=0;
+            int count=0; 
+            if(n==1){
+                return (ar1[0]+ar2[0]);
             }
-            while(--n){
-                mini.pop();
+            while(i<n && j<n){
+                if(ar1[i]<=ar2[j]){
+                    i++;
+                    count+=1;
+                } 
+                else{
+                    j++; 
+                    count+=1;
+                }
+                if(count==n-1){
+                    int temp1=0;
+                    int temp2=0;
+                    if(i<n){
+                    if(ar1[i]<=ar2[j]){
+                        temp1=ar1[i];
+                        i++;
+                    }
+                    else{
+                        temp1=ar2[j];
+                        j++;
+                    } 
+                    }
+                    else{
+                        temp1=ar2[j];
+                          j++;
+                    }
+                    if(i<n){ 
+                        if(j<n && ar1[i]<=ar2[j]){
+                            temp2=ar1[i];
+                            i++;
+                        }
+                        else if(j==n){
+                            temp2=ar1[i]; 
+                            i++;
+                        }
+                        else{
+                            temp2=ar2[j];
+                            j++;
+                        }
+                    }
+                    else{ 
+                        // cout<<"isha"<<endl;
+                        temp2=ar2[j];
+                    } 
+                    // cout<<temp1<<"l;;"<<temp2<<endl;
+                 return (temp1+temp2);
+                    
+                }
             }
-            int temp1=mini.top(); 
-            mini.pop();
-            int temp2=mini.top();
-            mini.pop(); 
-            // cout<<temp1<<";;"<<temp2<<endl;
-            return (temp1+temp2);
-            
-    }
+        return 0; 
+    } 
 };
 
 //{ Driver Code Starts.
