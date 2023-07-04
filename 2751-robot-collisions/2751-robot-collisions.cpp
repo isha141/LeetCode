f23bdd0794c9dbacc20791e0848a5f4cac7ca4b7
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-04 15:11:53 +0530

    Time: 430 ms (67.37%), Space: 204.6 MB (67.99%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -1,54 +1,50 @@
 class Solution {
 public:
-    vector<int> survivedRobotsHealths(vector<int>& pos, vector<int>& heal, string dir) {
-           int n=pos.size();
-         vector<int>ans;
-          stack<pair<int,char>>st;  
-           vector<pair<int,int>>ds;
-          for(int i=0;i<n;++i){
-              ds.push_back({pos[i],i});
-          }
-          sort(ds.begin(),ds.end());
-          for(int i=0;i<n;++i){
-              int ind1=ds[i].second;
-              char ch=dir[ind1];
-                while(ch=='L' && !st.empty() && (st.top().second!=ch)){
-                    if(heal[ind1]<heal[st.top().first]){
-                        heal[ind1]=0; 
-                        int ind=st.top().first;
-                        heal[ind]-=1; 
-                        if(heal[ind]==0){
-                            st.pop();
-                        }  
-                        // cout<<ind1<<";;"<<endl;
-                        break;
-                    }
-                    else if(heal[ind1]<=heal[st.top().first]){
-                         st.pop();
-                          heal[ind1]=0; 
-                        break;
-                    }
-                    else{
-                         st.pop(); 
-                         heal[ind1]-=1;  
-                        // cout<<ind1<<"***"<<endl;
-                        // break;
-                    }
+    vector<int> survivedRobotsHealths(vector<int>& pos, vector<int>& h, string dir) { 
+        vector<pair<int,int>>ans; 
+        stack<pair<int,char>>st; 
+        int n=pos.size(); 
+        vector<pair<int,int>>ds;
+        for(int i=0;i<n;++i){
+            ds.push_back({pos[i],i});
+        }
+        sort(ds.begin(),ds.end());
+        for(int j=0;j<n;++j){
+            int i=ds[j].second;
+            char ch=dir[i];
+            while(ch=='L' && !st.empty() && st.top().second!=ch){
+                auto itr=st.top();
+                st.pop();
+                int ind=itr.first;
+                if(h[ind]>h[i]){
+                    h[ind]-=1;
+                    h[i]=0;
+                    st.push(itr);
+                    break;
+                } 
+                else if(h[ind]==h[i]){
+                    h[i]=0;
+                    break;
                 }
-              if(heal[ind1]!=0){
-                  st.push({ind1,dir[ind1]});
-              }
-          } 
-           stack<int>s; 
-          vector<pair<int,int>>check;
-         while(!st.empty()){
-             auto itr=st.top();
-              st.pop(); 
-             check.push_back({itr.first,heal[itr.first]});
-         }
-         sort(check.begin(),check.end());
-         for(auto itr: check)
-               ans.push_back(itr.second);
-        return ans;
+                else{
+                    h[i]-=1;
+                }
+            }
+            if(h[i]!=0){
+                st.push({i,ch});
+            }
+        }
+        while(!st.empty()){
+            auto itr=st.top();
+            st.pop();
+            ans.push_back({itr.first,h[itr.first]});
+        }  
+        vector<int>res;
+        sort(ans.begin(),ans.end());
+        for(auto itr: ans){
+            res.push_back(itr.second);
+        }
+        // reverse(ans.begin(),ans.end());
+        return res;
     }
 };
