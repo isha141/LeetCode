10b9851b1fba745c3f3dbfcc3fb906e541b8b935
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-04 18:30:42 +0530

    Time: 21 ms (27.69%), Space: 13.1 MB (99.28%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int i=n-1;
-        int j=m-1;
-        bool flag=1;
-        for(int i=0;i<n;++i){ 
-            if(matrix[i][0]==0){
-                flag=0;
-            }
-            for(int j=1;j<m;++j){
-                if(matrix[i][j]==0){
-                    matrix[0][j]=0;
-                    matrix[i][0]=0;
-                }
+    void setZeroes(vector<vector<int>>& mat) {
+         int n=mat.size();
+         int m=mat[0].size();
+         bool col=0;
+         for(int i=0;i<n;i++){
+             for(int j=0;j<m;++j){
+                 if(mat[i][j]==0){
+                     mat[i][0]=0;
+                     if(j!=0){
+                         mat[0][j]=0;
+                     }
+                     else{
+                         col=1;
+                     }
+                 }
+             }
+         }
+         for(int i=1;i<n;++i){
+             for(int j=1;j<m;++j){
+                 if(mat[i][j]!=0){
+                     if(mat[i][0]==0 || mat[0][j]==0){
+                         mat[i][j]=0;
+                     }
+                 }
+             }
+         }
+        if(mat[0][0]==0){
+            for(int i=0;i<m;++i){
+                mat[0][i]=0;
             }
         }
-        // for(int i=0;i<n;++i){
-        //     for(int j=0;j<m;++j){
-        //         cout<<matrix[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        for(int i=n-1;i>=0;--i){
-            for(int j=m-1;j>=1;--j){
-                if(matrix[0][j]==0 || matrix[i][0]==0){
-                    matrix[i][j]=0;
-                }
-            }
-            if(!flag){
-                matrix[i][0]=0;
+        if(col){
+            for(int j=0;j<n;++j){
+                mat[j][0]=0;
             }
         }
         return ;
