b48596ce22c4b92c1fd3cc4838d315839d23ea82
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-04 14:46:39 +0530

    Time: 8 ms (67.31%), Space: 10.1 MB (8.78%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        map<int,int>mp;
+        for(auto itr: nums)
+             mp[itr]++;
+         for(auto itr: mp){
+             if(itr.second==1)
+                   return itr.first;
+         }
+        return -1;
+    }
+    
+};
