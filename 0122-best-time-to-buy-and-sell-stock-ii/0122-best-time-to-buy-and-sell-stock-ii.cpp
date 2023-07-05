0dabac8ac71e49c401d0313590e1e5ebc4d6d4bf
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-06 01:39:40 +0530

    Time: 4 ms (85.48%), Space: 12.9 MB (79.37%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,27 +1,14 @@
-class Solution { 
-    private:
-    int n;
-    int solve(int i,vector<int>&p,bool flag,vector<vector<int>>&dp){
-        if(i>=n){
-            if(flag)
-                return -1e9;
-            return 0;
-        }  
-        int pick=0; 
-        int not_pick=0; 
-        if(dp[i][flag]!=-1) return dp[i][flag];
-        if(!flag){
-        pick=max(-p[i]+solve(i+1,p,!flag,dp),solve(i+1,p,flag,dp));
-        }
-        else{
-        not_pick=max(p[i]+solve(i+1,p,!flag,dp),solve(i+1,p,flag,dp)); 
-        }
-        return dp[i][flag]=max(pick,not_pick);
-    }
+class Solution {
 public:
     int maxProfit(vector<int>& p) {
-          n=p.size(); 
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-          return solve(0,p,0,dp);
+        int ans=0;
+        int n=p.size();
+        for(int i=1;i<n;++i){
+            if(p[i-1]<p[i]){
+                ans+=p[i]-p[i-1];
+                // i++;
+            }
+        }
+        return ans;
     }
 };
