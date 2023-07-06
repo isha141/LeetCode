59c86cc9349b01de02ec210837672eb35e42686d
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-06 15:33:13 +0530

    Time: 3 ms (42.25%), Space: 7.9 MB (57.63%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,27 +1,33 @@
-class Solution { 
+class Solution {
     private:
-    int n;
-    private:
-    int solve(int i,int n,vector<int>&nums,vector<int>&dp){
-        if(i>n) return 0;
-        if(dp[i]!=-1)
-              return dp[i];
-        int take=0;
-        int not_take=0;
-        take=nums[i]+solve(i+2,n,nums,dp);
-        not_take=solve(i+1,n,nums,dp);
-        return dp[i]=max(take,not_take);
+    int solve(vector<int>&nums){ 
+         int n=nums.size();
+        vector<int>dp(n+1,-1); 
+        if(n==1)
+             return nums[0];
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;++i){
+            int take=nums[i]+dp[i-2];
+            int not_take=dp[i-1];
+            dp[i]=max(take,not_take);
+        }
+        return dp[n-1];
     }
 public:
     int rob(vector<int>& nums) {
-        n=nums.size(); 
-        if(n==1) 
-            return nums[0]; 
-        vector<int>dp(n,-1);
-        int ans1=solve(0,n-2,nums,dp);  
-        for(int i=0;i<n;++i) dp[i]=-1;
-        // memset(dp,-1,sizeof(dp));
-        int ans2=solve(1,n-1,nums,dp);
+           int n=nums.size(); 
+           if(n==1)
+                return nums[0];
+          vector<int>v1;
+          vector<int>v2;
+          for(int i=0;i<n-1;++i)
+              v1.push_back(nums[i]);
+          for(int j=1;j<n;++j){
+              v2.push_back(nums[j]);
+          }
+          int ans1=solve(v1);
+          int ans2=solve(v2);
         return max(ans1,ans2);
     }
 };
