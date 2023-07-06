95a0451fa88c745187f4afc95649bc4b91debee8
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-06 16:12:51 +0530

    Time: 181 ms (22.58%), Space: 59.8 MB (5.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,34 +1,27 @@
-int dp[102][103];
-class Solution { 
+int dp[102][102];
+class Solution {
     private:
-    int solve(int m,int n){
-        if(m==0 && n==0)
-            return 1;  
-        if(m<0 || n<0) return 0;
-        if(dp[m][n]!=-1) return dp[m][n];
-        int left=solve(m-1,n);
-        int right=solve(m,n-1);
-        return dp[m][n]=left+right;
+    int n,m;
+    int solve(int i,int j,vector<vector<int>>vis){
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        if(i>=m || j>=n || vis[i][j]==1) return 0;
+        if(dp[i][j]!=-1) return dp[i][j]; 
+        vis[i][j]=1;
+        // int left=solve(i,j-1,vis);
+        int right=solve(i,j+1,vis);
+        // int up=solve(i-1,j,vis);
+        int down=solve(i+1,j,vis); 
+        vis[i][j]=0;
+        return dp[i][j]=(right+down);
     }
 public:
     int uniquePaths(int m, int n) { 
-        // memset(dp,-1,sizeof(dp));
-        // return solve(m-1,n-1); 
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        dp[0][0]=1;
-        for(int i=0;i<m;++i){
-            for(int j=0;j<n;++j){ 
-                if(i==0 && j==0) continue;
-                int left=0;
-                int right=0;
-                if(i>0){
-                    left=dp[i-1][j];
-                }
-                if(j>0)
-                    right=dp[i][j-1];
-                dp[i][j]=left+right;
-            }
-        }
-        return dp[m-1][n-1];
+        this->n=n;
+        this->m=m; 
+        memset(dp,-1,sizeof(dp));
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        return solve(0,0,vis);
     }
 };
