c62269c2e8c3ce3c1d613651ae994b291a1563ac
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-06 16:14:29 +0530

    Time: 192 ms (22.58%), Space: 59.8 MB (5.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -6,21 +6,20 @@ class Solution {
         if(i==m-1 && j==n-1){
             return 1;
         }
-        if(i>=m || j>=n || vis[i][j]==1) return 0;
-        if(dp[i][j]!=-1) return dp[i][j]; 
-        vis[i][j]=1;
-        // int left=solve(i,j-1,vis);
+        if(i>=m || j>=n ) return 0;
+        if(dp[i][j]!=0) return dp[i][j]; 
+        dp[i][j]++;
         int right=solve(i,j+1,vis);
         // int up=solve(i-1,j,vis);
         int down=solve(i+1,j,vis); 
-        vis[i][j]=0;
+        // vis[i][j]=0;
         return dp[i][j]=(right+down);
     }
 public:
     int uniquePaths(int m, int n) { 
         this->n=n;
         this->m=m; 
-        memset(dp,-1,sizeof(dp));
+        memset(dp,0,sizeof(dp));
         vector<vector<int>>vis(m,vector<int>(n,0));
         return solve(0,0,vis);
     }
