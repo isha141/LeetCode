94b21bd57911ac008249dbfcb00f3387bfde19e5
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-08 03:32:32 +0530

    Time: 19 ms (48.72%), Space: 10.7 MB (11.65%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -7,7 +7,7 @@ class Solution {
         if(i==n-1){
             return mat[i][j];
         } 
-        cout<<i<<";;"<<j<<endl;
+        // cout<<i<<";;"<<j<<endl;
         if(dp[i][j]!=INT_MAX) return dp[i][j];
         int temp=mat[i][j];
         // mat[i][j]=-103;
@@ -21,12 +21,31 @@ public:
     int minFallingPathSum(vector<vector<int>>& mat) {
          n=mat.size();
          m=mat[0].size();
-         int ans=INT_MAX;  
-         // memset(dp,-1,sizeof(dp));
+         // int ans=INT_MAX;  
          vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
          for(int i=0;i<m;++i){
-             ans=min(ans,solve(0,i,mat,dp));
+            dp[0][i]=mat[0][i];
          }
-         return ans;
+         for(int i=1;i<n;++i){
+             for(int j=0;j<m;++j){
+                 int left=mat[i][j];
+                 if(j-1>=0)
+                     left+=dp[i-1][j-1];
+                 else left=INT_MAX;
+                 int right=mat[i][j];
+                 right+=dp[i-1][j]; 
+                 int down=mat[i][j];
+                 if(j+1<m){
+                     down+=dp[i-1][j+1];
+                 }
+                 else down=INT_MAX;
+                 dp[i][j]=min(left,min(right,down));
+             }
+         }
+         int ans=INT_MAX;
+        for(int i=0;i<m;++i){
+            ans=min(ans,dp[n-1][i]);
+        }
+        return ans;
     }
 };
