6aa522cbff6830b728611394cc117d3f5752b6e7
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-08 03:01:39 +0530

    Time: 13 ms (10.45%), Space: 9.5 MB (7.06%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -16,7 +16,29 @@ public:
     int minimumTotal(vector<vector<int>>& tri) {
              n=tri.size();
              m=tri[0].size(); 
-             memset(dp,-1,sizeof(dp));
-            return solve(0,0,tri); 
+             vector<vector<int>>dp(n,vector<int>(204,1e9));
+             dp[0][0]=tri[0][0];
+             for(int i=1;i<n;++i){
+                 for(int j=0;j<tri[i].size();++j){
+                     int temp=tri[i][j];
+                     if(j<tri[i-1].size()){
+                         temp+=dp[i-1][j];
+                     } 
+                     else temp=INT_MAX;
+                     int down=tri[i][j];
+                     if(j>0){
+                         down+=dp[i-1][j-1];
+                     } 
+                     else
+                         down=INT_MAX;
+                     dp[i][j]=min(temp,down);
+                 } 
+             } 
+        int ans=INT_MAX;
+           for(int j=0;j<tri[n-1].size();++j){
+              ans=min(ans,dp[n-1][j]);
+           }
+        return ans;
+        
     }
 };
