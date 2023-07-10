f5b9a5292303a7b7e9ecd22bc81f4a2827fbcdbe
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-11 03:18:42 +0530

    Time: 4 ms (84.05%), Space: 7 MB (98.89%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -3,42 +3,37 @@ public:
     string simplifyPath(string path) {
          int n=path.size();
          string ans="";
-         stack<string>st;
          int i=0;
          while(i<n){
              if(path[i]=='/'){
+                 if(i+1<n && (ans.size()==0 || ans.back()!='/')) {
+                     ans+='/';
+                 }
+                 string temp="";
+                 int count=0;
                  i++;
-                 continue;
-             } 
-             else{
-                 string temp=""; 
+                 while(i<n && path[i]=='/') i++;
                  while(i<n && path[i]!='/'){
-                     temp+=path[i];
-                     i++;
+                         temp+=path[i];
+                     i++; 
+                 } 
+                 // cout<<i<<";;"<<temp<<endl; 
+                 if(temp==".") continue;
+                 if(temp==".."){ 
+                     if(ans.size()) ans.pop_back();
+                     while(ans.size() && ans.back()!='/') ans.pop_back();
+                     if(ans.size()==0)
+                     ans="/"; 
+                     // i++;
+                     temp="";
                  }
-                 if(temp=="."){
-                     continue;
-                 }
-                 else if(temp==".."){
-                     if(st.size()){
-                         st.pop();
-                     }
-                 }
-                 else{
-                     st.push(temp);
-                 }
+                 ans+=temp;
              }
-         }
-        while(!st.empty()){
-            auto itr=st.top();
-            // ans+='/';
-            ans="/"+itr+ans;
-            // ans+='/';
-            st.pop();
-        } 
-        cout<<ans<<" ";
-        if(ans.size()==0)
-            return "/";
-         return ans;
+         } 
+        // cout<<ans<<";;"<<endl;
+        if(ans.size()==0) return"/";
+        if(ans.size()==1) return ans;
+        if(ans.size() && ans.back()=='/') ans.pop_back();
+        return ans;
     }
 };
