da7918a38c23b94b4a03d406ea633bcdf82294c3
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-15 02:31:42 +0530

    Time: 87 ms (20.05%), Space: 30.1 MB (26.85%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,7 +1,8 @@
 int dp[1003][1003];
 class Solution { 
     int n;
-    int m;
+    int m; 
+    int mod=1e9+7;
     int solve(int i,int j,string &s,string &t){
         if(j==m)
              return 1;
@@ -21,7 +22,23 @@ public:
     int numDistinct(string s, string t) {
           n=s.size();
           m=t.size();
-          memset(dp,-1,sizeof(dp));
-         return solve(0,0,s,t);
+          vector<vector<long long>>dp(n+1,vector<long long>(m+1,0));
+          long ans=0; 
+        for(int i=0;i<n;++i){
+            dp[i][0]=1;
+        }
+          for(int i=1;i<=n;++i){
+              for(int j=1;j<=m;++j){ 
+                  int take=0;
+                  int not_take=0;
+                  if(s[i-1]==t[j-1]){
+                     dp[i][j]=1ll*(1ll*dp[i-1][j-1]+1ll*dp[i-1][j])%mod;
+                  }
+                  else{
+                      dp[i][j]=dp[i-1][j];
+                  }
+              }
+          }
+        return dp[n][m];
     }
 };
