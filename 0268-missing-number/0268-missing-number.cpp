0440287a2947cf3388fb16e1887af548df0f08e4
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-19 00:46:55 +0530

    Time: 15 ms (94.90%), Space: 18 MB (40.18%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       int n=nums.size();
+        int total=(n*(n+1))/2;
+        int sum=0;
+        for(auto itr: nums) sum+=itr;
+        return total-sum;
+    }
+};
