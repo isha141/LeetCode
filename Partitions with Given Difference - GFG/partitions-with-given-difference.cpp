02f01ecee4c561301e80f3d2fdf3e3dec7d34e8b
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-22 03:35:33 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -29,8 +29,28 @@ class Solution {
         if(sum<d) return 0;
         int temp=(sum-d);
         if(temp%2) return 0;
-        vector<vector<int>>dp(n,vector<int>((temp/2)+1,-1));
-        return solve(0,arr,n,temp/2,0,dp); 
+        vector<vector<int>>dp(n,vector<int>((temp/2)+1,0)); 
+        // for(int i=0;i<n;++i){
+            // dp[i][0]=1;
+        // } 
+        if(arr[0]==0){
+            dp[0][0]=2;
+        } 
+        else dp[0][0]=1;
+       if(arr[0]!=0 && arr[0]<=temp/2){
+            dp[0][arr[0]]=1;
+        }
+        for(int i=1;i<n;++i){
+            for(int target=0;target<=temp/2;++target){
+                int take=0;
+                if(arr[i]<=target){
+                    take=dp[i-1][target-arr[i]];
+                }
+                int not_take=dp[i-1][target];
+                dp[i][target]=(take+not_take)%mod;
+            }
+        }
+        return dp[n-1][temp/2];
         // return res/2;
     } 
 };
