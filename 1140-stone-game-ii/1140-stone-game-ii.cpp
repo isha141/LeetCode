68a0dbc0537ecd37a80ab4d27350b12c5de4881f
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-24 04:06:30 +0530

    Time: 23 ms (55.56%), Space: 8.3 MB (85.54%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,38 +1,40 @@
-int dp[105][105][2];
+int dp[101][101][2];
 class Solution { 
     private:
     int n;
-    long long solve(int i,vector<int>&p,int count,bool flag){
-        if(i>=n) return 0; 
-        long long take=0; 
-        if(dp[i][count][flag]!=-1)
-            return dp[i][count][flag];
-        if(flag==1)
-            take=INT_MAX;
+    int solve(int i,vector<int>&p,int m,bool flag){
+        if(i>=n)
+              return 0; 
+        int take=0; 
+        int not_take=INT_MAX;
+        if(flag)
+            take=INT_MAX; 
+        if(dp[i][m][flag]!=-1)
+             return dp[i][m][flag];
         if(!flag){
-             int c=0; 
-            long temp=0;
-            for(int j=1;(j<=2*count && (j+i-1)<n);++j){ 
-                 c+=1;
-                 temp+=p[j+i-1];
-                take=max(take,temp+solve(i+c,p,max(count,c),!flag));
+             int count=0; 
+             int sum=0;
+            for(int j=i;j<n && count<2*m;++j){
+                count+=1; 
+                sum+=p[j];
+                take=max(take,sum+solve(j+1,p,max(m,count),!flag));
             }
         }
         else{
-             int c=0; 
-             long temp=0;
-            for(int j=1;(j<=2*count && (j+i-1)<n);++j){ 
-                 c+=1; 
-                temp+=p[j+i-1];
-                take=min(take,solve(i+c,p,max(count,c),!flag));
-            } 
+             int count=0; 
+             int sum=0;
+            for(int j=i;j<n && count<2*m;++j){
+                count+=1;
+                sum+=p[j];
+                take=min(take,solve(j+1,p,max(m,count),!flag));
+            }
         }
-        return dp[i][count][flag]=take;
+        return dp[i][m][flag]=take;
     }
 public:
     int stoneGameII(vector<int>& p) {
-           n=p.size(); 
-           memset(dp,-1,sizeof(dp));
-           return solve(0,p,1,0);
+        n=p.size(); 
+        memset(dp,-1,sizeof(dp));
+         return solve(0,p,1,0);
     }
 };
