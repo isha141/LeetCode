8165fea814e606d5077105fba4d3922a86846581
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-29 20:28:02 +0530

    Time: 27 ms (100.00%), Space: 10 MB (88.02%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,33 +1,36 @@
-int dp[71][71][71];
-class Solution { 
+int dp[72][72][72];
+class Solution {
     private:
     int n,m;
-    int solve(int i,int j,int k,vector<vector<int>>&grid){
-        if(i<0 || j<0 || i>=n || j>=m || k<0 || k>=m )
+    int solve(int row,int c1,int c2,vector<vector<int>>&grid){
+        if(c1<0 || c1>=m || row>=n || c2<0 || c2>=m)
               return 0;
-        if(i==n-1){ 
-             if(j==k)
-            return grid[i][j]; 
-            return grid[i][j]+grid[i][k];
-        } 
-        if(dp[i][j][k]!=-1) 
-            return dp[i][j][k];
-        int temp=grid[i][j];
-        if(j!=k){
-            temp+=grid[i][k];
+        int take=0; 
+        if(dp[row][c1][c2]!=-1)
+              return dp[row][c1][c2];
+        if(c1==c2){
+            take=grid[row][c1];
         }
-        int left=temp+max(solve(i+1,j,k,grid),max(solve(i+1,j,k+1,grid),solve(i+1,j,k-1,grid)));
-        int right=temp+max(solve(i+1,j+1,k+1,grid),max(solve(i+1,j+1,k,grid),solve(i+1,j+1,k-1,grid)));
-        int down=temp+max(solve(i+1,j-1,k-1,grid),max(solve(i+1,j-1,k,grid),solve(i+1,j-1,k+1,grid)));
-        return dp[i][j][k]=max(left,max(down,right));
+        else{
+            take=grid[row][c1];
+            take+=grid[row][c2];
+        } 
+        int ans=0;
+        for(int col=-1;col<=1;++col){
+             for(int col1=-1;col1<=1;++col1){
+              int nc1=col+c1;
+              int nc2=col1+c2;
+              int res=take+solve(row+1,nc1,nc2,grid);
+              ans=max(ans,res); 
+         }
+        }
+        return dp[row][c1][c2]=ans;
     }
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-          n=grid.size();
-          m=grid[0].size(); 
-          int ans=0; 
-           memset(dp,-1,sizeof(dp));
-           ans=solve(0,0,m-1,grid);
-           return ans;
+         n=grid.size();
+         m=grid[0].size(); 
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,m-1,grid);
     }
 };
