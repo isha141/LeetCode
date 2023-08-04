73d01abc576d6026203a4145c424c45489ce1d38
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-08-04 16:24:51 +0530

    Time: 3 ms (91.38%), Space: 8.4 MB (73.04%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,31 +1,30 @@
-int dp[1007];
-class Solution { 
-    private:
-     map<string,int>mp; 
-    bool solve(int i,string &s){
-        if(i>=s.size())
-             return 1;  
+int dp[301];
+class Solution {
+    private: 
+    int n;
+    int solve(int i,string &s,map<string,int>&mp){
+        if(i>=n)
+              return 1;
         if(dp[i]!=-1)
-            return dp[i]; 
-        // cout<<i<<" "<<s.substr(i)<<endl;
-        string temp="";
-        for(int k=i;k<s.size();++k){
-             temp+=s[k];
+              return dp[i];
+        string temp=""; 
+        int ans=0;
+        for(int j=i;j<n;++j){
+            temp+=s[j];
             if(mp.find(temp)!=mp.end()){
-                dp[i]=1;
-                if(solve(k+1,s))    
-                return 1;
+              ans=ans|solve(j+1,s,mp);
             }
         }
-        return dp[i]=0;
-        // return 0;
+        return dp[i]=ans;
     }
 public:
-    bool wordBreak(string s, vector<string>& word) {
-        // map<string,int>mp;  
+    bool wordBreak(string s, vector<string>& word) { 
+        map<string,int>mp;
+        n=s.size();
+         for(auto it: word){
+             mp[it]++;
+         } 
         memset(dp,-1,sizeof(dp));
-        for(auto itr: word)
-            mp[itr]++;
-        return solve(0,s);
+        return solve(0,s,mp);
     }
 };
