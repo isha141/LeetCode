5018a44e44416314e3c23181a9184c73dfbdc555
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-08-13 10:14:38 +0530

    Time: 4 ms (52.85%), Space: 7.5 MB (90.12%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,40 +1,28 @@
-int dp[104][104];
-class Solution { 
+int dp[101][101];
+class Solution {
     private:
     int n,m;
-    int solve(int i,int j,vector<vector<int>>&ob){
-        if(i==n-1 && j==m-1)
+    int solve(int i,int j,vector<vector<int>>& ob){
+        if(i==n-1 && j==m-1){
              return 1;
-        if(i>=n || j>=m || ob[i][j]==1 ) return 0; 
-        if(dp[i][j]!=-1) return dp[i][j];
-        int left=solve(i,j+1,ob);
-        int down=solve(i+1,j,ob);
-        // ob[i][j]=temp;
-        return dp[i][j]=(left+ down);
+        }
+        if(i<0 || j<0 || i>=n || j>=m)
+             return 0;
+        if(ob[i][j]==1)
+              return 0;  
+        if(dp[i][j]!=-1)
+             return dp[i][j];
+        int right=solve(i+1,j,ob);
+        int down=solve(i,j+1,ob);
+        return dp[i][j]=(down + right);
     }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& ob) {
-           n=ob.size();
-           m=ob[0].size();  
-          if(ob[n-1][m-1]==1) return 0; 
-          if(ob[0][0]==1) return 0;
-           memset(dp,0,sizeof(dp));
-           for(int i=0;i<n;++i){
-                if(ob[i][0]==1) break;
-               dp[i][0]=1;
-           }
-           for(int i=0;i<m;++i){ 
-               if(ob[0][i]==1) break;
-               dp[0][i]=1;
-           }
-          for(int i=1;i<n;++i){ 
-              for(int j=1;j<m;++j){
-                  if(ob[i][j]==1) continue;
-                  int left=dp[i][j-1];
-                  int up=dp[i-1][j];
-                  dp[i][j]=(left+up);
-              }
-          }
-        return dp[n-1][m-1];
+        n=ob.size();
+        m=ob[0].size(); 
+        if(ob[n-1][m-1]==1)
+             return 0; 
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,ob);
     }
 };
