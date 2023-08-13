03c08e4409d17deb733b6b0a6c17398a8edbb9bf
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-08-13 10:19:54 +0530

    Time: 5 ms (28.33%), Space: 7.8 MB (25.64%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -20,9 +20,33 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& ob) {
         n=ob.size();
         m=ob[0].size(); 
-        if(ob[n-1][m-1]==1)
-             return 0; 
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,ob);
+        if(ob[0][0]==1 || ob[n-1][m-1]==1)
+             return 0;
+        vector<vector<int>>dp(n,vector<int>(m,0)); 
+        dp[0][0]=1;
+        for(int i=0;i<m;++i){
+             if(ob[0][i]!=1){
+                  dp[0][i]=1;
+             } 
+            else  
+                break;
+        }
+        for(int i=0;i<n;++i){
+             if(ob[i][0]!=1){
+                  dp[i][0]=1;
+             } 
+            else 
+                break;
+        }
+        for(int i=1;i<n;++i){
+            for(int j=1;j<m;j++){
+                if(ob[i][j]==1)
+                    continue; 
+                int right=dp[i][j-1];
+                int left=dp[i-1][j];
+                dp[i][j]=(left +right);
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
