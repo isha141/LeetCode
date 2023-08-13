a8c2d4d234f902d2c18d7039f7d6be3a31d71fb2
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-08-14 01:41:24 +0530

    Time: 149 ms (7.33%), Space: 81.2 MB (52.37%) - LeetHub

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
@@ -1,37 +1,38 @@
 class Solution { 
     private:
-     int ok(int mid,vector<int>&nums){
-           int c=0;
-          int i=0;
-           int n=nums.size();
-           while(i<n){
-               if(i+1<n && abs(nums[i]-nums[i+1])<=mid){
-                     c+=1;
-                     i+=2;
-               }
-               else{
-                   i++;
-               }
-           }
-          return c;
-     }
+    bool ok(int mid,vector<int>&nums,int p){
+        int count=0;
+        int i=0;
+        int n=nums.size();
+        while(i<n-1){
+            if(nums[i+1]-nums[i]<=mid){
+                count+=1;
+                i+=2;
+            }
+            else{
+                i+=1;
+            }
+        }
+        if(count>=p)
+             return 1;
+        return 0;
+    }
 public:
     int minimizeMax(vector<int>& nums, int p) {
-           int n=nums.size();
-           int ans=0;
-           sort(nums.begin(),nums.end());
-           int low=0;
-           int high=1e9; 
-           while(low<=high){
-               int mid=(low+high)>>1;
-               if(ok(mid,nums)>=p){
-                   ans=mid;
-                   high=mid-1;
-               }
-               else{
-                   low=mid+1;
-               }
-           }
-        return ans;
+         int n=nums.size();
+         sort(nums.begin(),nums.end());
+         int low=0;
+         int high=1e9; 
+         int ans=0;
+         while(low<=high){
+             int mid=(low+high)>>1;
+             if(ok(mid,nums,p)){
+                 ans=mid;
+                 high=mid-1;
+             }
+             else
+                low=mid+1;
+         }
+         return ans;
     }
 };
