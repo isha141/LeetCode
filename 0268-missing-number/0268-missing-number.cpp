a96dde298411be116bdd12ae7294981a49772e64
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-08-18 15:50:46 +0530

    Time: 22 ms (39.88%), Space: 18 MB (13.29%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,10 +1,31 @@
-class Solution {
+class Solution { 
+    private: 
+    int n;
+    int ok(int mid,vector<int>&nums){
+        int count=0;
+        for(int i=0;i<n;++i){
+            if(nums[i]<=mid)
+                  count+=1;
+        }
+        return count;
+    }
 public:
     int missingNumber(vector<int>& nums) {
-       int n=nums.size();
-        int total=(n*(n+1))/2;
-        int sum=0;
-        for(auto itr: nums) sum+=itr;
-        return total-sum;
+         n=nums.size();
+        int low=0;
+        int high=n; 
+        int ans=0;
+        while(low<=high){
+            int mid=(low+high)>>1; 
+            // cout<<mid<<";;";
+            if(ok(mid,nums)>mid){
+                low=mid+1;
+            }
+            else{ 
+                ans=mid;
+                high=mid-1;
+            }
+        }
+        return ans;
     }
 };
