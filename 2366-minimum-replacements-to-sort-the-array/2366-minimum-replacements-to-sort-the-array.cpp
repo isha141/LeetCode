4c341f061bd7ce2a11221103d9d08c3ee0ec34bb
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-05 16:59:06 +0530

    Time: 106 ms (5.56%), Space: 54.6 MB (49.53%) - LeetHub

---
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long minimumReplacement(vector<int>& nums) {
+         int n=nums.size();
+         long long ans=0;
+         int maxi=nums[n-1];
+         for(int i=n-2;i>=0;--i){
+             if(nums[i]>maxi){
+                int parts=ceil((nums[i]+maxi-1)/maxi);
+                ans+=parts-1;
+                maxi=nums[i]/parts;
+             }
+             else{
+                 maxi=nums[i];
+             }
+         }
+         return ans;
+    }
+};
