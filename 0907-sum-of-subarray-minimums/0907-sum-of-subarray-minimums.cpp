8c525e7f5000553c4ca54f204cdcff9821faae7b
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-07 15:32:44 +0530

    Time: 84 ms (54.66%), Space: 43.9 MB (29.18%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        int n=arr.size();
-        int ans=0;
-        int mod=1e9+7;
-        vector<int>pre(n,0);
-        vector<int>suff(n,0);
-        stack<pair<int,int>>s;
-        for(int i=0;i<n;++i){
-            int count=0;
-            while(!s.empty() && s.top().first>arr[i]){
-                count+=s.top().second;
-                s.pop();
-            }
-            pre[i]=count+1;
-            s.push({arr[i],count+1});
-        }
-        while(!s.empty())
-            s.pop();
-        for(int i=n-1;i>=0;--i){
-             int count=0;
-            while(!s.empty() && s.top().first>=arr[i]){
-                count+=s.top().second;
-                s.pop();
-            }
-            suff[i]=count+1;
-            s.push({arr[i],count+1});
-        }
-        // for(int i=0;i<n;++i){
-            // cout<<pre[i]<<" ";
-        // }
-        // cout<<endl;
-        // for(int i=0;i<n;++i){
-            // cout<<suff[i]<<" ";
-        // }
-        // cout<<endl;
-        for(int i=0;i<n;++i){
-            int temp=(1ll*pre[i]*suff[i])%mod;
-            temp=(1ll*temp*arr[i])%mod;
-            ans=(ans%mod+ temp%mod)%mod;
-        }
-        return ans;
+         int n=arr.size();
+         int sum=0;  
+         int mod=1e9+7;
+         stack<pair<int,int>>s;
+         vector<int>pre(n+1,0);
+         vector<int>suff(n+1,0);
+         pre[0]=1;
+         suff[n-1]=1;
+         s.push({arr[0],1});
+         for(int i=1;i<n;++i){ 
+              int ans=0;
+             while(!s.empty() && s.top().first>arr[i]){
+                 ans+=s.top().second;
+                 s.pop();
+             }
+             pre[i]=ans+1;
+             s.push({arr[i],ans+1});
+         }
+         while(!s.empty())
+             s.pop();
+         for(int i=n-1;i>=0;--i){
+             int ans=0;
+             while(!s.empty() && s.top().first>=arr[i]){
+                 ans+=s.top().second;
+                 s.pop();
+             }
+             suff[i]=ans+1;
+             s.push({arr[i],ans+1});
+         }
+          for(int i=0;i<n;++i){
+              int temp=(1ll*pre[i]*suff[i]*arr[i])%mod;
+              sum=(1ll*sum%mod+temp%mod)%mod;
+          }
+         return sum;
     }
 };
