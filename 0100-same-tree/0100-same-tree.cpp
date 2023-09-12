bf785c9e6164ee34465f24a6ef0f724e15bc6ae8
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-12 13:39:58 +0530

    Time: 0 ms (100.00%), Space: 10 MB (41.27%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,9 +12,10 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL || q==NULL)
-            return p==q;
-        
-        return p->val==q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        if(p==NULL)
+             return q==NULL;
+        if(q==NULL)
+               return p==NULL;
+         return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree (p->right,q->right);
     }
 };
