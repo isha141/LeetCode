b1a427ffbba0fb3d55ebd90ad9b4049ae469b389
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-12 15:08:11 +0530

    Time: 11 ms (83.07%), Space: 25.2 MB (51.41%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -9,28 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution { 
-    int prev=-1;
-    int ans=INT_MAX;
-    void solve(TreeNode *root){
-        if(root==NULL)
-             return; 
-        solve(root->left);
-        if(prev==-1){
-            prev=root->val;
-        }
-        else{
-            ans=min(ans,abs(root->val-prev)); 
-            prev=root->val;
-        } 
-        solve(root->right);
-        // prev=root->val;
-    }
+class Solution {
+    private:
+     int ans=INT_MAX;
+      TreeNode *prev=NULL;
+     void solve(TreeNode *root){
+            if(root==NULL)
+                  return ;
+            solve(root->left);
+             if(prev!=NULL){
+            ans=min(ans,abs(prev->val-root->val));
+            prev=root;
+             }
+            else{
+                prev=root;
+            }
+            solve(root->right);
+         return ;
+     }
 public:
     int getMinimumDifference(TreeNode* root) {
-         if(root==NULL) 
-             return 0;
-        solve(root);
+         if(root==NULL)
+               return 0;
+          solve(root);
          return ans;
     }
 };
