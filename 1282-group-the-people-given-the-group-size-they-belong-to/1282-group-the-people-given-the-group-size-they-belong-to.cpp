f380b726cdfc1ac5ecdaa4c59893eb432852df5d
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-13 12:54:57 +0530

    Time: 8 ms (68.04%), Space: 12.9 MB (62.91%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,14 +1,30 @@
 class Solution {
+    private:
+    int n;
+//     void solve(int i,vector<int>adj,vector<int>&ds,vector<vector<int>>&ans,int count){
+        
+//     }
 public:
-   vector<vector<int>> groupThePeople(vector<int>& gz) {
-  vector<vector<int>> res, groups(gz.size() + 1);
-  for (auto i = 0; i < gz.size(); ++i) {
-    groups[gz[i]].push_back(i);
-    if (groups[gz[i]].size() == gz[i]) {
-      res.push_back({});
-      swap(res.back(), groups[gz[i]]);
+    vector<vector<int>> groupThePeople(vector<int>& grp) {
+         n=grp.size();
+         vector<vector<int>>ans;
+         vector<int>ds;
+          vector<int>adj[n+1];
+         for(int i=0;i<n;++i){
+             adj[grp[i]].push_back(i);
+         }
+         for(int i=1;i<=n;++i){
+             if(adj[i].size()){
+                 vector<int>ds;
+                 for(auto itt: adj[i]){
+                     ds.push_back(itt);
+                     if(ds.size()==i){
+                         ans.push_back(ds);
+                         ds.clear();
+                     }
+                 }
+             }
+         }
+        return ans;
     }
-  }
-  return res;
-}
 };
