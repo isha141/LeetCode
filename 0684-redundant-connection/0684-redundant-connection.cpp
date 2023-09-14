4646257efe3f1e0584c7bcdbf2748d69a31910f1
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-14 11:27:32 +0530

    Time: 5 ms (66.16%), Space: 9.3 MB (26.70%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,38 +1,53 @@
-class Solution {
+class kruskal{
+     public:
+      vector<int>parent;
+      vector<int>rank;
+     kruskal(int n){
+            parent.resize(n);
+            rank.resize(n);
+            for(int i=0;i<n;++i){
+                parent[i]=i;
+                 rank[i]=0;
+            }
+     }
+    int findparent(int u){
+          if(u==parent[u])
+                return u;
+          return parent[u]=findparent(parent[u]);
+    }
+    void unionByRank(int u,int v){
+         u=findparent(u);
+         v=findparent(v);
+         if(rank[u]<rank[v]){
+             rank[v]+=1;
+              parent[u]=v;
+         }
+        else{
+              rank[u]+=1;
+              parent[v]=u;
+        }
+    }
+};
+class Solution { 
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& e) {
-        vector<int>ans; 
-        int n=e.size();
-        set<int>s; 
-        vector<int>indeg(n+1,0);
-        vector<int>adj[n+1];
-        for(int i=0;i<n;++i){
-            adj[e[i][0]].push_back(e[i][1]);
-            adj[e[i][1]].push_back(e[i][0]);
-            indeg[e[i][0]]++;
-            indeg[e[i][1]]++;
-        }
-        queue<int>q;
-        for(int i=0;i<=n;++i){
-            if(indeg[i]==1)
-                  q.push(i);
-        }
-        while(!q.empty()){
-            auto itr=q.front();
-            q.pop(); 
-            for(auto it: adj[itr]){
-                indeg[it]--;
-                if(indeg[it]==1)
-                    q.push(it);
-            }
-        }
-         for(int i=1;i<=n;++i){
-            if(indeg[i]>1)s.insert(i);
-        }
-        for(auto itr: e){
-            if(s.find(itr[0])!=s.end() && s.find(itr[1])!=s.end())
-                ans=itr;
-        }
-        return ans;
+           vector<int>ans;
+           int n=e.size();
+           kruskal adj(n+2);
+           if(e.size()==0){
+                 return ans;
+           }
+           for(auto itr: e){
+                int u=itr[0];
+                int v=itr[1];
+               if(adj.findparent(u)==adj.findparent(v)){
+                     ans.push_back(u);
+                     ans.push_back(v);
+               }
+               else{
+                   adj.unionByRank(u,v);
+               }
+           }
+          return ans;
     }
 };
