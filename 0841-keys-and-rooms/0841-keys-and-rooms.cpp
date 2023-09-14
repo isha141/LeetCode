187ae638d5f7ddaa9e1e89de81033c6a3dd95c79
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-14 16:46:54 +0530

    Time: 3 ms (98.11%), Space: 11 MB (15.44%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,30 +1,50 @@
 class Solution { 
-    void solve(int node, vector<int>adj[],vector<int>&vis){
-        vis[node]=1;
-        for(auto itr: adj[node]){
-            if(vis[itr]==0){
-                // vis[itr]=1;
-                solve(itr,adj,vis);
-            }
-        }
+    void solve(int node,vector<vector<int>>&rooms,vector<int>&vis,int count){ 
+           if(vis[node]==0){
+               vis[node]=1;
+               count+=1;
+           }
+          for(auto itr: rooms[node]){
+              if(vis[itr]==0){
+                  solve(itr,rooms,vis,count);
+              }
+          }
+        return ;
     }
 public:
-    bool canVisitAllRooms(vector<vector<int>>& r) {
-        int n=r.size();
-        vector<int>adj[n+1];
-        for(int i=0;i<n;++i){
-            vector<int>ds=r[i];
-            for(auto itr: ds){
-                adj[i].push_back(itr);
-            }
-        }
-        vector<int>vis(n,0);
-        int c=0; 
-        solve(0,adj,vis);
-        for(auto i=0;i<n;++i)
-            if(!vis[i]) return 0;
+    bool canVisitAllRooms(vector<vector<int>>& rooms) { 
         
-       return 1;
+        queue<int>q;
+        int n=rooms.size();
+         vector<int>vis(n,0);
+          vis[0]=1;
+          vector<int>ds=rooms[0];
+          for(auto iit: ds){
+                // cout<<iit<<";;"; 
+                vis[iit]=1;
+              q.push(iit);
+          }
+          int count=1;
+          while(!q.empty()){
+              auto itr=q.front();
+              q.pop();   
+               // cout<<itr<<"**";
+              // vis[itr]=1;
+              for(auto iit: rooms[itr]){ 
+                    // cout<<iit<<";;";
+                    if(!vis[iit]){
+                          // cout<<iit<<";;";
+                          vis[iit]=1;
+                        q.push(iit);
+                    }
+              }
+          }
+          for(auto i=0;i<n;++i){
+               // cout<<i<<"-->"<<vis[i]<<endl;
+              if(vis[i]==0)
+                    return 0;
+          }
+         return 1;
         
     }
 };
