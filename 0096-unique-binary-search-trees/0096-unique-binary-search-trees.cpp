abc96adbac144aa5b532669d7b9ec03925b9a897
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-15 12:00:08 +0530

    Time: 2 ms (59.69%), Space: 6.4 MB (14.97%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -2,21 +2,21 @@ int dp[20];
 class Solution {
     private:
     int solve(int n){
-        if(n<0)
-              return 0;
         if(n==0 || n==1)
               return 1;
+        int ans=0;
         if(dp[n]!=-1)
               return dp[n];
-        long long ans=0;
-         for(int i=1;i<=n;++i){
-             ans+=solve(i-1)*solve(n-i);
-         }
+        for(int i=1;i<=n;++i){
+            ans+=solve(i-1)*solve(n-i);
+        }
         return dp[n]=ans;
     }
 public:
-    int numTrees(int n) { 
-         memset(dp,-1,sizeof(dp));
-          return solve(n);
-    }    
+    int numTrees(int n) {
+        int ans=0;
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+        // return ans;
+    }
 };
