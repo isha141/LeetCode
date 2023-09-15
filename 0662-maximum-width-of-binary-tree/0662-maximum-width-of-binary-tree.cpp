ce8f7087e6f8483217af2e77d2a377738abaf3b0
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-15 13:05:58 +0530

    Time: 7 ms (61.49%), Space: 17.7 MB (5.48%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -9,39 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution { 
-    private: 
+class Solution {
     int ans=0;
-    int solve(TreeNode* root){
-        queue<pair<TreeNode*,long long>>q;
+    int solve(TreeNode *root){
+        queue<pair<TreeNode *,int>>q;
         q.push({root,0});
-        while(!q.empty()){
-            int n=q.size(); 
-            int first=0;
-            int last=0;
-            for(int i=0;i<n;++i){
-                   auto itr=q.front(); 
-                q.pop();
-                TreeNode* temp=itr.first;
-                long long val=itr.second;
-                if(i==0){
-                    first=val;
-                }
-                if(i==n-1){
-                    last=val;
-                }
-                if(temp->left)
-                    q.push({temp->left,1ll*2*(val-first)+1});
-                if(temp->right)
-                    q.push({temp->right,1ll*2*(val-first)+2});
+         while(!q.empty()){
+             int t=q.size();
+             int mini=INT_MAX;
+             int maxi=INT_MIN;
+             while(t--){
+                 auto itr=q.front();
+                 q.pop();
+                 int x=itr.second;
+                 TreeNode *root=itr.first;
+                 mini=min(mini,itr.second);
+                 maxi=max(maxi,itr.second);
+                 if(root->left)
+                     q.push({root->left,2*1ll*(x-mini)+1});
+                 if(root->right)
+                     q.push({root->right,2*1ll*(x-mini)+2});
              }
-            ans=max(ans,last-first+1);
-        }
+             ans=max(ans,maxi-mini+1);
+         }
         return ans;
     }
+    
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(root==NULL) return 0;
+        if(root==NULL)
+               return 0;
         return solve(root);
     }
 };
