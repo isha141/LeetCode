f7e3483fb101dbd0dc107d13862e23ad645c3673
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-18 15:03:35 +0530

    Time: 23 ms (97.47%), Space: 29.3 MB (53.92%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -12,75 +12,69 @@ public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if(root==NULL) return "";
         string ans="";
+        if(root==NULL)
+              return ans;
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
-            int n=q.size();
-            while(n--){
-                auto itr=q.front();
-                q.pop(); 
-                if(itr==NULL){
-                    ans+="#";
-                    ans+=',';
-                    continue;
-                }
-                ans+=to_string(itr->val);
+            auto it=q.front();
+            q.pop();
+            if(it==NULL){
+                ans+="#";
                 ans+=',';
-                if(itr->left)
-                    q.push(itr->left);
-                else
-                    q.push(NULL);
-                if(itr->right)
-                    q.push(itr->right);
-                else
-                    q.push(NULL);
+              continue;
+            }
+            ans+=to_string(it->val);
+            ans+=",";
+            if(it->left){
+                q.push(it->left);
+            }
+            else{
+                q.push(NULL);
+            }
+            if(it->right){
+                q.push(it->right);
+            }
+            else{
+                q.push(NULL);
             }
         }
-        cout<<ans<<":::"<<endl;
         return ans;
-
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        if(data.size()==0)
+          if(data.size()==0){
               return NULL;
-        stringstream s(data);
-        string str;
-        getline(s,str,',');
-        TreeNode * root=new TreeNode(stoi(str));
-        queue<TreeNode *>q;
-        q.push(root);
-        while(!q.empty()){
-            int n=q.size(); 
-            bool flag=0;
-            while(n--){
-                TreeNode * temp=q.front();
-                q.pop();
-                getline(s,str,','); 
-                // if(!flag){
-                if(str=="#"){
-                    temp->left=NULL;
-                    // continue;
-                }
-                else{
-                    temp->left=new TreeNode(stoi(str));
-                    q.push(temp->left);
-                }
-                 getline(s,str,','); 
-                    if(str=="#"){
-                        temp->right=NULL;
-                    }
-                    else{
-                        temp->right=new TreeNode(stoi(str));
-                        q.push(temp->right);
-                    }
-                }
-                flag=!flag;
-            }
-        return root;
+          }
+          stringstream s(data);
+          string word;
+          getline (s,word,',');
+          TreeNode *root=new TreeNode(stoi(word));
+          queue<TreeNode*>q;
+          q.push(root);
+          while(!q.empty()){
+              auto itr=q.front();
+              q.pop();
+              getline(s,word,',');
+              if(word=="#"){
+                  itr->left=NULL;
+              }
+              else{
+                  itr->left=new TreeNode(stoi(word));
+                  q.push(itr->left);
+              }
+              getline(s,word,',');
+              if(word=="#"){
+                  itr->right=NULL;
+              }
+              else{
+                  itr->right=new TreeNode(stoi(word));
+                  q.push(itr->right);
+              }
+          }
+           return root;
     }
 };
 
