463bb7f1d36ce8896937e49fdc7996ed40076933
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-22 19:00:15 +0530

    Time: 4 ms (39.88%), Space: 7.4 MB (9.66%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& mat) {
-          int n=mat.size();
-          int m=mat[0].size();
-          for(int i=0;i<n;++i){
-              for(int j=0;j<i;++j){
-                  swap(mat[i][j],mat[j][i]);
-              }
-          }
-          for(int i=0;i<n;++i){
-              reverse(mat[i].begin(),mat[i].end());
-          }
-          return ;
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;++i){
+            for(int j=0;j<i;++j){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        // [[1,2,3]]   // [[1,4,7]]
+        // [[4,5,6]]   // [[2,5,8]]
+        // [[7,8,9]]   // [[3,6,9]]
+         for(int i=0;i<n;++i){
+             reverse(matrix[i].begin(),matrix[i].end());
+         }
+        return ;
     }
 };
