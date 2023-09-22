6dbced85fc1b5edc44c7c9ea6d0118638b275ebe
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-23 01:33:05 +0530

    Time: 7 ms (88.39%), Space: 16.2 MB (12.20%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,41 +1,49 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int>ans;
-        int n=nums.size();
-        int count1=0;
-        int first=INT_MAX;
-        int second=INT_MAX;
-        int count2=0;
-        for(int i=0;i<n;++i){
-            if(count1==0 && nums[i]!=second){
-                first=nums[i];
-            }
-            if(first==nums[i]){ count1++; continue;}
-           else if(count2==0){
-                second=nums[i];
-            }
-            if(second==nums[i]){
-                count2++; 
-                continue;
-            }
-            else{
-                count1--; //[2,2,1,3]
-                count2--;
-            }
-        }
-         count1=0;
-        count2=0;
-        for(auto itr: nums){
-            if(itr==first) count1++;
-            if(itr==second) count2++;
-        }  
-        // cout<<first<<" "<<second<<endl;
-        if(count1>n/3)
+          int n=nums.size();
+          vector<int>ans;
+          int first=INT_MAX;
+          int second=INT_MAX;
+          int freq1=0;
+          int freq2=0;
+          for(auto itr: nums){
+               if(freq1==0 && second!=itr){
+                   first=itr;
+                   freq1+=1;
+               }
+              else if(first==itr){
+                  freq1+=1;
+                  continue;
+              }
+               else if(freq2==0 && first!=itr){
+                   second=itr;
+                    freq2+=1;
+               }
+              else if(second==itr){
+                   freq2+=1;
+              }
+              else{
+                  freq1--;
+                  freq2--;
+              }
+          }
+          freq1=0;
+          freq2=0;
+          for(auto itr: nums){
+              if(itr==first){
+                  freq1+=1;
+              }
+              else if(itr==second){
+                  freq2+=1;
+              }
+          }
+        if(freq1>(n/3)){
             ans.push_back(first);
-        if(count2>n/3)
+        }
+        if(freq2>(n/3)){
             ans.push_back(second);
-        sort(ans.begin(),ans.end());
+        }
         return ans;
     }
 };
