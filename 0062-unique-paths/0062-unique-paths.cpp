c56110789188e063d701294ccaed8d2cf7ed0446
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-23 01:57:52 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (65.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,12 @@
-int dp[102][102];
 class Solution {
-    private:
-    int solve(int i,int j){
-        if(i==0 && j==0)
-              return 1;
-        if(i<0 || j<0)
-              return 0;
-        if(dp[i][j]!=-1)
-              return dp[i][j];
-        int top=0;
-        int left=0;
-        top=solve(i-1,j);
-        left=solve(i,j-1);
-        return dp[i][j]=(top+left);
-    }
 public:
     int uniquePaths(int m, int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(m-1,n-1);
+        int N=(n+m-2);
+        long long res=1;
+        int r=m-1;
+        for(int i=1;i<m;++i){
+            res=res*(N-r+i)/i;
+        }
+         return res;
     }
 };
