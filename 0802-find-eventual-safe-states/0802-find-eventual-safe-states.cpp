734baad4c4f70e1160615ec038e940cb48562591
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-24 02:09:39 +0530

    Time: 217 ms (8.15%), Space: 70.3 MB (5.33%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,42 +1,43 @@
-class Solution { 
-    bool iscycle(int node,vector<int>adj[],vector<int>&vis,vector<int>&dfsvis){
-           vis[node]=1;
-            dfsvis[node]=1;
-           for(auto iit : adj[node]){
-               if(!vis[iit]){
-                   if(iscycle(iit,adj,vis,dfsvis))
+class Solution {
+    private:
+    int ok(int node,vector<int>adj[],vector<int>&vis,vector<int>&dfsvis){
+          vis[node]=1;
+          dfsvis[node]=1;
+          
+          for(auto iit: adj[node]){
+              if(!vis[iit]){
+                  if(ok(iit,adj,vis,dfsvis))
                         return 1;
-               }
-               else if(dfsvis[iit]){
-                   return 1;
-               }
-           }  
-         dfsvis[node]=0;
-        return 0;
+              }
+              else if(dfsvis[iit])
+                    return 1;
+          }
+          dfsvis[node]=0;
+         return 0;
     }
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& g) {
-            vector<int> ans;
-           map<int,int>mp;
-           int n=g.size();
-           vector<int>adj[n];
-           for(int i=0;i<n;++i){
-               vector<int>ds=g[i];
-                for(auto itr: ds){
-                    adj[i].push_back(itr);
-                }
-           } 
-            vector<int>vis(n,0),dfsvis(n,0);
-           for(int i=0;i<n;++i){
-               if(iscycle(i,adj,vis,dfsvis)){
-                   mp[i]++;
-               }
-           } 
-           for(int i=0;i<n;++i){
-               if(mp.find(i)==mp.end()){
-                   ans.push_back(i);
-               }
-           }
-          return ans;
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+          int n=graph.size();
+          vector<int>adj[n];
+          for(int i=0;i<n;++i){
+              vector<int>ds=graph[i];
+              for(auto iit:ds){
+                  adj[i].push_back(iit);
+              }
+          }
+          map<int,int>mp;
+          vector<int>vis(n,0);
+          vector<int>dfsvis(n,0);
+          vector<int>ans;
+          for(int i=0;i<n;++i){
+              if(ok(i,adj,vis,dfsvis)){
+                 mp[i]++;
+              }
+          }
+          for(int i=0;i<n;++i){
+              if(mp.find(i)==mp.end())
+                  ans.push_back(i);
+          }
+         return ans;
     }
 };
