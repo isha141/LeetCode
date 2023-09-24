2eb893588206161f8ffad3fd6300353b0d91961e
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-25 03:29:29 +0530

    Time: 0 ms (100.00%), Space: 11 MB (34.11%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,27 +1,29 @@
-class Solution {
+class Solution { 
+    private:
     int n; 
-     vector<vector<int>>ans; 
-    void solve(int i,vector<int>ds,vector<int>&can,int target){
-        if(target==0){
-            ans.push_back(ds);
-            return ;
-        } 
-        if(i>=n) return ;
-        if(target<0)
-            return ;
+    void solve(int i,vector<int>&can,int target,vector<int>&curr,vector<vector<int>>&ans){ 
+           if(target==0){
+               ans.push_back(curr);
+                return;
+           }
+          if(target<0)
+                return ;
         for(int j=i;j<n;++j){
-            if(j>i && can[j]==can[j-1]) continue;
-            ds.push_back(can[j]);
-            solve(j+1,ds,can,target-can[j]);
-            ds.pop_back();
+            if(j>i && can[j]==can[j-1])
+                  continue;
+             curr.push_back(can[j]);
+              solve(j+1,can,target-can[j],curr,ans);
+             curr.pop_back();
         }
+        return;
     }
 public:
     vector<vector<int>> combinationSum2(vector<int>& can, int target) {
-        n=can.size();
-        vector<int>ds;
-        sort(can.begin(),can.end());
-        solve(0,ds,can,target);
-        return ans;
+           n=can.size();
+          sort(can.begin(),can.end());
+          vector<vector<int>>ans;
+          vector<int>curr;
+          solve(0,can,target,curr,ans);
+          return ans;
     }
 };
