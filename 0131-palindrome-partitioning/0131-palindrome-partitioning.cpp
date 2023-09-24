994a36a139b6eb3fc59b6588ad300a423230df76
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-25 03:35:25 +0530

    Time: 78 ms (85.90%), Space: 49.6 MB (81.58%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,41 +1,37 @@
-class Solution { 
-    vector<vector<string>>ans; 
-    bool palin(string s){
+class Solution {
+    private: 
+    int n; 
+    bool ok(string &temp){
         int i=0;
-        if(s.size()==1) return 1;
-        int j=s.size()-1;
-        while(i<=j){
-            if(s[i]!=s[j]) return 0;
+        int j=temp.size()-1;
+        while(i<j){
+            if(temp[i]!=temp[j])
+                  return 0;
             i++;
             j--;
         }
-        return 1;
+         return 1;
     }
-    int n; 
-    void solve(int ind,string s,vector<string>&ds){
-         if(ind>=n){
-             ans.push_back(ds);
-             return ;
-         }
-        for(int i=ind;i<n;++i){ 
-           //  cout<<ind<<",,"<<i<<endl;
-           // cout<<ind<<"..."<<s.substr(ind,i-ind+1)<<endl;
-           //  cout<<"******"<<endl;
-            if(palin(s.substr(ind,i-ind+1))){
-                ds.push_back(s.substr(ind,i-ind+1)); 
-              
-                solve(i+1,s,ds);
-                ds.pop_back();
-            }
-        } 
-        return ;
+    void solve(int i,string &s,vector<string>&ds,vector<vector<string>>&ans){
+          if(i>=n){
+              ans.push_back(ds);
+                return;
+          }
+          for(int j=i;j<n;++j){
+              string temp=s.substr(i,j-i+1);
+              if(ok(temp)){
+                  ds.push_back(temp);
+                  solve(j+1,s,ds,ans);
+                   ds.pop_back();
+              }
+          }
     }
 public:
     vector<vector<string>> partition(string s) {
-        n=s.size(); 
-        vector<string>ds;
-        solve(0,s,ds);
-        return ans;
-        
+          n=s.size();
+          vector<vector<string>>ans;
+          vector<string>ds;
+          solve(0,s,ds,ans);
+          return ans;
     }
 };
