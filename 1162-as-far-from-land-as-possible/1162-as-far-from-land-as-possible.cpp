486ff866793a50dce3ee9e4ae64a3f8b42611930
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-24 14:48:35 +0530

    Time: 1131 ms (5.01%), Space: 20.1 MB (69.68%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,46 +1,30 @@
-class Solution { 
-    int n,m;  
-    int dx[4]={-1,1,0,0};
-    int dy[4]={0,0,1,-1};
-    private:
-    int solve(vector<vector<int>>&grid,queue<pair<int,int>>&q){
-        int c=0;
-        while(!q.empty()){
-            int t=q.size();
-            c++;
-            while(t--){
-                auto itr=q.front();
-                q.pop();
-                int x=itr.first;
-                int y=itr.second; 
-                grid[x][y]=1;
-                for(int k=0;k<4;++k){
-                    int nx=dx[k]+x;
-                    int ny=dy[k]+y;
-                    if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==0){
-                        q.push({nx,ny});
-                        grid[nx][ny]=1;
-                    }
-                }
-            } 
-            // c++;
-        }
-        return c-1;
-    }
+class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        n=grid.size();
-        m=grid[0].size(); 
-        queue<pair<int,int>>q;
+       int n=grid.size();
+       int m=grid[0].size();
+       int ans=-1;
+        vector<pair<int,int>>ds;
         for(int i=0;i<n;++i){
             for(int j=0;j<m;++j){
                 if(grid[i][j]==1){
-                    q.push({i,j});
+                    ds.push_back({i,j});
                 }
             }
         }
-       int ans=solve(grid,q); 
-        return ans==0?-1:ans;
-        
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(grid[i][j]==1)
+                    continue;
+                int res=INT_MAX;
+                for(auto itr: ds){
+                    int x=abs(itr.first-i);
+                    int y=abs(itr.second-j);
+                    res=min(res,x+y);
+                }
+                ans=max(ans,res);
+            }
+        }
+        return ans==INT_MAX?-1:ans;
     }
 };
