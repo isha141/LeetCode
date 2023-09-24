d8c134367228c490d1d65c4ea3746c49087c2313
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-25 03:03:06 +0530

    Time: 4 ms (74.71%), Space: 11.1 MB (47.98%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,27 @@
-class Solution { 
+class Solution {
+    private: 
     int n;
-    vector<vector<int>>ans; 
-    void solve(int ind,vector<int>&ds,vector<int>&can,int target){ 
+    void solve(int i,vector<int>&can,vector<int>&ds,int target,vector<vector<int>>&ans){
         if(target==0){
             ans.push_back(ds);
-            return ;
+            return;
         }
-         if(ind>=n){
-             return ;
-         }
-          if(target<0) return ;
-          ds.push_back(can[ind]);
-        solve(ind,ds,can,target-can[ind]);
-          ds.pop_back();
-        solve(ind+1,ds,can,target);
-        
+        if(target<0)
+              return ;
+        if(i>=n)
+              return ;
+        ds.push_back(can[i]);
+        solve(i,can,ds,target-can[i],ans);
+        ds.pop_back();
+        solve(i+1,can,ds,target,ans);
+        return ;
     }
 public:
-    vector<vector<int>> combinationSum(vector<int>& can, int target) { 
-        n =can.size();
-        vector<int>ds; 
-        sort(can.begin(),can.end());
-        solve(0,ds,can,target);
-        return ans;
+    vector<vector<int>> combinationSum(vector<int>& can, int target) {
+          n=can.size();
+         vector<vector<int>>ans;
+         vector<int>ds;
+         solve(0,can,ds,target,ans);
+         return ans;
     }
 };
