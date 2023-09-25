254f09ddaf7cadcd56cccbef51ab84292f727f0b
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-26 03:01:30 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (16.76%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,23 +2,22 @@ class Solution {
     private:
     vector<vector<int>>ans;
     int n;
-    void solve(int i,vector<int>&nums,map<int,int>&mp){
+    void solve(int i,vector<int>&nums){
         if(i>=n){
-             ans.push_back(nums);
-              return ;
+            ans.push_back(nums);
+             return;
         }
-        for(int j=i;j<n;++j){
-            swap(nums[i],nums[j]);
-            solve(i+1,nums,mp);
-            swap(nums[i],nums[j]);
-        }
-        return ;
+         for(int j=i;j<n;++j){
+             swap(nums[i],nums[j]);
+             solve(i+1,nums);
+             swap(nums[i],nums[j]);
+         }
+        return;
     }
 public:
-    vector<vector<int>> permute(vector<int>& nums) { 
-         map<int,int>mp;
+    vector<vector<int>> permute(vector<int>& nums) {
          n=nums.size();
-          solve(0,nums,mp); 
-        return ans;
+        solve(0,nums);
+         return ans;
     }
 };
