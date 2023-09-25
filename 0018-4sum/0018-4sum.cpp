b2754610bd05feda1b8c8a5d65c5c2f709e7419c
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-26 01:56:43 +0530

    Time: 59 ms (72.92%), Space: 13.6 MB (39.77%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>ans;
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;++i){ 
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-            for(int j=i+1;j<n;++j){ 
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-                int sum=nums[i]+nums[j];
-                int low=j+1;
-                int high=n-1;
-                while(low<high){
-                    if((1ll*nums[low]+nums[high]+sum)==target){
-                        ans.push_back({nums[i],nums[j],nums[low],nums[high]});
-                    int prev=low;
-                    while(low<high && nums[prev]==nums[low]){
-                         low++;
-                    }
-                    prev=high;
-                    while(high>low && nums[prev]==nums[high]){
-                        high--;
-                    } 
-                    }
-                    else if((1ll*nums[low]+nums[high]+sum)<target){
-                    low++;
-                   }
-                   else{
-                    high--;
-                   } 
-                }
-            }
-        }
-        return ans;
+         sort(nums.begin(),nums.end());
+         int n=nums.size();
+         vector<vector<int>>ans;
+         for(int i=0;i<n;){
+             for(int j=i+1;j<n;){
+                 long long sum=nums[i]+nums[j];
+                 long long rem=target-(sum);
+                 int low=j+1;
+                 int high=n-1;
+                 while(low<high){
+                     if(nums[low]+nums[high]==rem){
+                         ans.push_back({nums[i],nums[j],nums[low],nums[high]});
+                         int k=low;
+                         while(k<n && nums[k]==nums[low])
+                             k++;
+                         low=k;
+                         k=high;
+                         while(k>low && nums[k]==nums[high])
+                             k--;
+                         high=k;
+                     }
+                     else if(nums[low]+nums[high]<rem)
+                           low++;
+                     else
+                         high--;
+                 }
+                 int k=j;
+                 while(k<n && nums[j]==nums[k])
+                       k++;
+                 j=k;
+             }
+            int k=i;
+            while(k<n && nums[i]==nums[k])
+                       k++;
+           i=k;
+         }
+         return ans;
     }
 };
