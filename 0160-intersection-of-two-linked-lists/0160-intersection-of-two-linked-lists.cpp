76f79d9abcdf205acd30c1cbc3c5b915ed6251f9
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-25 11:02:10 +0530

    Time: 40 ms (42.05%), Space: 14.8 MB (29.17%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -7,45 +7,28 @@
  * };
  */
 class Solution {
-    private:
-    int solve(ListNode *h1,ListNode *h2){
-          int len1=0;
-          int len2=0;
-          while(h1!=NULL){
-              len1+=1;
-              h1=h1->next;
-          }
-          while(h2!=NULL){
-              len2+=1;
-              h2=h2->next;
-          }
-        return (len1-len2);
-    }
 public:
-    ListNode *getIntersectionNode(ListNode *h1, ListNode *h2) {
-           if(h1==NULL)
-                 return h2;
-           if(h2==NULL)
-                 return h1;
-           int diff=solve(h1,h2);
-           if(diff<0){
-                 while(diff!=0){
-                     h2=h2->next;
-                     diff++;
-                 }
-           }
-           else{
-               while(diff>0){
-                   h1=h1->next;
-                   diff--;
-               }
-           } 
-            while(h1 && h2){
-                  if(h1==h2)
-                        return h1;
-                h1=h1->next;
-                h2=h2->next;
-            }
-          return  NULL;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+         if(headA==NULL || headB==NULL)
+               return headA;
+         ListNode *root1=headA;
+         ListNode *root2=headB;
+        int count=0;
+         while(count<=2){
+             if(root1==NULL){
+                 count+=1;
+                 root1=headB;
+             }
+             if(root2==NULL){
+                 count+=1;
+                 root2=headA;
+             }
+             if(root1==root2){
+                  return root2;
+             }
+             root1=root1->next;
+              root2=root2->next;
+         }
+         return NULL;
     }
 };
