c344f83a9e5c1d6a763ea1bf6663ed8c5b43e983
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-29 18:10:36 +0530

    Time: 31 ms (86.22%), Space: 16.3 MB (40.68%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,29 +1,28 @@
-int dp[205][205];
 class Solution { 
     private:
-    int n,m; 
-    int solve(int i,int j,vector<vector<int>>&grid,int prev){
-        if(i<0 || i>=n || j<0 || j>=m || prev>=grid[i][j])
-              return 0;
-        if(dp[i][j]!=-1) 
-            return dp[i][j];
-        
-       return dp[i][j]=1+max({solve(i+1,j,grid,grid[i][j]),
-                             solve(i-1,j,grid,grid[i][j]),
-                             solve(i,j+1,grid,grid[i][j]),
-                            solve(i,j-1,grid,grid[i][j])}); 
+    int n,m;
+    int solve(int i,int j,vector<vector<int>>&matrix,int prev,vector<vector<int>>&dp){
+        if(i<0 || i>=n || j<0 || j>=m || matrix[i][j]<=prev)
+             return 0;
+        if(dp[i][j]!=-1)
+              return dp[i][j];
+        return dp[i][j]=1+max({solve(i-1,j,matrix,matrix[i][j],dp),
+                               solve(i+1,j,matrix,matrix[i][j],dp),
+                               solve(i,j+1,matrix,matrix[i][j],dp),
+                               solve(i,j-1,matrix,matrix[i][j],dp)
+                              });
     }
 public:
-    int longestIncreasingPath(vector<vector<int>>& mat) {
-        n=mat.size();
-        m=mat[0].size();
-        memset(dp,-1,sizeof(dp));
-        int ans=0;
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                ans=max(ans,solve(i,j,mat,INT_MIN));
-            }
-        }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+           n=matrix.size();
+           m=matrix[0].size();
+           int ans=0;
+           vector<vector<int>>dp(n,vector<int>(m,-1));
+           for(int i=0;i<n;++i){
+               for(int j=0;j<m;++j){
+                   ans=max(ans,solve(i,j,matrix,-1,dp));
+               }
+           }
         return ans;
     }
 };
