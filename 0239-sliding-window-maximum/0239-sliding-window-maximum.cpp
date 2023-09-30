d11835bd7ffb1ddc60b3f7ce217c8843e43902ce
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-01 02:56:52 +0530

    Time: 216 ms (46.60%), Space: 137.2 MB (41.03%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-         int n=nums.size();
-          vector<int>ans;
-          vector<int>ds;  
-          map<int,int>mp;
-          priority_queue<int>pq;
-          int maxi=0;
-          for(int i=0;i<k;++i){
-               pq.push(nums[i]);
-              mp[nums[i]]++;
-              maxi=max(maxi,nums[i]);
-          }
-          ans.push_back(maxi);
-          for(int i=k;i<n;++i){
-              pq.push(nums[i]);
-              mp[nums[i-k]]--;
-              mp[nums[i]]++;
-              while(!pq.empty() && mp[pq.top()]==0)
-                   pq.pop();
-              ans.push_back(pq.top());
-          }
+        vector<int>ans;
+        deque<int>q;
+        int n=nums.size();
+        for(int i=0;i<n;++i){
+               while(!q.empty() && q.front()<=(i-k))
+                   q.pop_front();
+              while(!q.empty() && nums[q.back()]<=nums[i])
+                  q.pop_back();
+              q.push_back(i);
+              if(i>=k-1){
+                  ans.push_back(nums[q.front()]);
+              }
+              q.push_back(i);
+        }
         return ans;
     }
 };
