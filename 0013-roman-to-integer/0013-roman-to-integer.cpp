7784ede6b1aabb1937862ce9951f9853f621f6e9
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-02 01:25:50 +0530

    Time: 18 ms (20.89%), Space: 8.4 MB (32.35%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n=s.size();
+        map<char,int>mp;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+        int ans=0;
+        for(int i=0;i<n;++i){
+            if(i-1>=0){
+                if(mp[s[i-1]]<mp[s[i]]){
+                    ans-=mp[s[i-1]];
+                    int temp=mp[s[i]];
+                    temp-=mp[s[i-1]];
+                    ans+=temp;
+                    continue;
+                }
+            }
+            ans+=mp[s[i]];
+        }
+        return ans;
+    }
+};
