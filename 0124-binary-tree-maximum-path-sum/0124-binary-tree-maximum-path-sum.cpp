87074d01ddec61f5e25b71cfb4e7af10345bac72
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-07 23:53:02 +0530

    Time: 17 ms (74.56%), Space: 27.9 MB (38.52%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -10,20 +10,21 @@
  * };
  */
 class Solution {
-    private:
     int ans=INT_MIN;
     int solve(TreeNode *root){
-        if(root==NULL)
-              return 0;
+          if(root==NULL)
+                return 0;
+          // if(root->left==NULL && root->right==NULL)
+                // return root->val;
         int l=solve(root->left);
         int r=solve(root->right);
         ans=max(ans,l+r+root->val);
-        return max(0,max(l,r)+root->val);
+         return max(0,max(l,r)+root->val);
     }
 public:
     int maxPathSum(TreeNode* root) {
-         if(root==NULL)
-               return 0;
+           if(root==NULL)
+                return 0;
           solve(root);
           return ans;
     }
