81b19cb93888bd65c32b6233418d3128582c7cd7
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-11 02:57:06 +0530

    Time: 26 ms (23.74%), Space: 7.6 MB (83.36%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,45 +1,34 @@
-int dp[505][505];
-class Solution { 
-    private:  
+int dp[502][502];
+class Solution {
+    private:
     int n,m;
-    int solve(int i,int j,string &w1,string &w2){ 
-        if(i>=n && j>=m){
-            return 0;
-        }
-        if(j>=m){
-             return n-i;
-        }
-        if(i>=n ){
+    int solve(int i,int j,string &w1,string &w2){
+         if(i>=n && j>=m){
+              return 0;
+         }
+         if(i>=n){
              return m-j;
-        }  
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(w1[i]==w2[j])
-            return dp[i][j]=solve(i+1,j+1,w1,w2);
-        return dp[i][j]=1+min(solve(i,j+1,w1,w2),min(solve(i+1,j,w1,w2),solve(i+1,j+1,w1,w2)));
-        // return min(ins,min(del,rep));
+         }
+         if(j>=m){
+              return n-i;
+         }
+          if(dp[i][j]!=-1)
+               return dp[i][j];
+         int equal=INT_MAX;
+        int not_equal=INT_MAX;
+        if(w1[i]==w2[j]){
+            equal=solve(i+1,j+1,w1,w2);
+        }
+        else{
+            not_equal=min(1+solve(i,j+1,w1,w2),min(1+solve(i+1,j,w1,w2),1+solve(i+1,j+1,w1,w2)));
+        }
+        return dp[i][j]=min(equal,not_equal);
     }
 public:
     int minDistance(string w1, string w2) {
-            n=w1.size();
-            m=w2.size(); 
-        memset(dp,0,sizeof(dp));  
-        for(int i=0;i<=n;++i){
-            dp[i][0]=i;
-        }
-        for(int j=0;j<=m;++j){
-            dp[0][j]=j;
-        }
-        for(int i=1;i<=n;++i){ 
-            for(int j=1;j<=m;++j){
-                if(w1[i-1]==w2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
-                } 
-                else{
-                    dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
-                }
-            }
-        }
-        return dp[n][m];
+        n=w1.size();
+        m=w2.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,w1,w2);
     }
 };
