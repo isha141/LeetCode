e0c443af7727a1c8f6f7da0e0ecfb437054e59b6
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-12 01:00:45 +0530

    Time: 30 ms (23.26%), Space: 7.6 MB (82.10%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,54 +1,26 @@
-int dp[502][502];
+int dp[504][504];
 class Solution {
     private:
     int n,m;
-    int solve(int i,int j,string &w1,string &w2){
-         if(i>=n && j>=m){
-              return 0;
-         }
-         if(i>=n){
-             return m-j;
-         }
-         if(j>=m){
-              return n-i;
-         }
-          if(dp[i][j]!=-1)
+    int solve(int i,int j,string &a,string &b){
+          if(i<0 && j<0)
+                return 0;
+          if(i<0) return j+1;
+          if(j<0) return i+1;
+         if(dp[i][j]!=-1)
                return dp[i][j];
-         int equal=INT_MAX;
-        int not_equal=INT_MAX;
-        if(w1[i]==w2[j]){
-            equal=solve(i+1,j+1,w1,w2);
-        }
-        else{
-            not_equal=min(1+solve(i,j+1,w1,w2),min(1+solve(i+1,j,w1,w2),1+solve(i+1,j+1,w1,w2)));
-        }
-        return dp[i][j]=min(equal,not_equal);
+         int take=INT_MAX;
+         int not_take=INT_MAX;
+         if(a[i]==b[j])
+          take=solve(i-1,j-1,a,b);
+        not_take=1+min(solve(i-1,j,a,b),min(solve(i,j-1,a,b),solve(i-1,j-1,a,b)));
+         return dp[i][j]=min(take,not_take);
     }
 public:
-    int minDistance(string w1, string w2) {
-        n=w1.size();
-        m=w2.size();
-        memset(dp,0,sizeof(dp));
-        for(int i=0;i<=n;++i){
-            dp[i][0]=i;  // deleteions
-        }
-        for(int j=0;j<=m;++j){
-            dp[0][j]=j;
-        }
-        for(int i=1;i<=n;++i){
-            for(int j=1;j<=m;++j){
-                int equal=1e9;
-                int not_equal=1e9;
-                if(w1[i-1]==w2[j-1]){
-                    equal=dp[i-1][j-1];
-                }
-                else{
-                    not_equal=1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
-                }
-                dp[i][j]=min(equal,not_equal);
-            }
-        }
-        return dp[n][m];
-        
+    int minDistance(string a, string b) {
+         n=a.size();
+         m=b.size();
+         memset(dp,-1,sizeof(dp));
+         return solve(n-1,m-1,a,b);
     }
 };
