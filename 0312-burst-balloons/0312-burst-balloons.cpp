1945650f41f666f4c00a9a1e9a9033781465c197
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-12 02:05:44 +0530

    Time: 266 ms (67.86%), Space: 10.6 MB (21.06%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,28 +1,20 @@
-int dp[303][303];
-class Solution { 
-    private:
-    int n;
-    int solve(int i,int j,vector<int>&nums){
-         if(i>j) return 0;
-          int ans=0;
-          if(dp[i][j]!=-1)
-            return dp[i][j];
-          for(int k=i;k<=j;++k){
-              int vals=1ll*nums[k];
-              // if(i-1>=0)
-                   vals*=nums[i-1];
-              // if(j+1<n)
-                    vals*=nums[j+1];
-              ans=max(ans,vals+solve(i,k-1,nums)+solve(k+1,j,nums));
-          }
-        return dp[i][j]=ans;
-    }
+class Solution {
 public:
-    int maxCoins(vector<int>& nums) { 
-        n=nums.size();
+    int maxCoins(vector<int>& nums) {
+        int n=nums.size();
          nums.push_back(1);
-        nums.insert(nums.begin(),1);   
-         memset(dp,-1,sizeof(dp));
-        return solve(1,n,nums);
+        nums.insert(nums.begin(),1);
+         vector<vector<int>>dp(n+2,vector<int>(n+3,0));
+         for(int i=n;i>=1;--i){
+             for(int j=1;j<=n;++j){
+                  if(i>j) continue;
+                 int maxi=INT_MIN;
+                 for(int k=i;k<=j;++k){
+                     maxi=max(maxi,nums[k]*nums[i-1]*nums[j+1]+dp[i][k-1]+dp[k+1][j]);
+                 }
+                 dp[i][j]=maxi;
+             }
+         }
+        return dp[1][n];
     }
 };
