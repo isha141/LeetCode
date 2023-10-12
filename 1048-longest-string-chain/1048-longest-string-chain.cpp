b0164a1654f36c6ae7eae4216ebe55caab671be7
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-12 09:44:24 +0530

    Time: 136 ms (36.95%), Space: 14 MB (80.59%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,53 +1,49 @@
-bool cmp(string &a,string &b){
-    if(a.size()==b.size())
-          return a<b;
-    return a.size()<b.size();
-}
-class Solution { 
+int dp[1002];
+class Solution {
     private:
-    int n; 
     bool ok(string &a,string &b){
+        int n=a.size();
+        int m=b.size();
+        if(m-n!=1) return 0;
+        int count=0;
         int i=0;
         int j=0;
-        int m=a.size();
-        int t=b.size();
-        if((t-m)!=1)
-              return 0;
-        while(i<m){
-               bool flag=0;
-              while(j<t && b[j]!=a[i]){
-                  j++;
-              }
-            if(j<t && b[j]==a[i]){
-                i++; 
+        while(i<n && j<m){
+            if(a[i]==b[j]){
+                i++;
                 j++;
             }
-            else
-                return 0;
-        }
-        if(i>=m)return 1;
-         return 0;
-    }
-    int solve(int i,vector<string>&words,string temp,map<string,int>&mp){
-        if(i>=n)
-              return 0;
-        if(mp.find(temp)!=mp.end())
-              return mp[temp];
-       int ans=0; 
-        for(int j=i;j<n;++j){
-            if((temp.size()==0) || ok(temp,words[j])){
-                 // cout<<j<<"->"<<temp<<"-->"<<words[j]<<endl;
-                ans=max(ans,1+solve(j+1,words,words[j],mp));
+            else{
+                j++;
+                count+=1;
             }
         }
-        // cout<<i<<"-->"<<ans<<"-->"<<temp<<endl;
-        return mp[temp]=ans;
+        while(i<n){
+              count+=1;
+            i++;
+        }
+        while(j<m){
+            count+=1;
+            j++;
+        }
+        if(count==1) return 1;
+        return 0;
+        
+    }
+    int solve(int i,vector<string>&word,string temp){
+        if(dp[i]!=-1)
+              return dp[i];
+        int ans=0;
+        for(int j=0;j<word.size();++j){
+            if(temp.size()==0 || ok(temp,word[j])){
+                ans=max(ans,1+solve(j,word,word[j]));
+            }
+        }
+        return dp[i]=ans;
     }
 public:
-    int longestStrChain(vector<string>& words) {
-           n=words.size(); 
-           map<string,int>mp;
-          sort(words.begin(),words.end(),cmp); 
-          return solve(0,words,"",mp);
+    int longestStrChain(vector<string>& words) { 
+        memset(dp,-1,sizeof(dp));
+        return solve(0,words,"");
     }
 };
