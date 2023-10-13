2f5fc97d836cc4e404b2b1e8887c45c188001d30
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-14 01:38:32 +0530

    Time: 87 ms (73.47%), Space: 54.8 MB (10.72%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     long long minimumReplacement(vector<int>& nums) {
-         int n=nums.size();
-         long long ans=0;
-         int maxi=nums[n-1];
-         for(int i=n-2;i>=0;--i){
-             if(nums[i]>maxi){
-                int parts=((nums[i]+maxi-1)/maxi);
-                ans+=parts-1;
-                maxi=nums[i]/parts;
-             }
-             else{
-                 maxi=nums[i];
-             }
-         }
+       long long ans=0;
+        int n=nums.size();
+       int maxi=nums[n-1];
+        for(int i=n-2;i>=0;--i){
+            if(nums[i]>maxi){
+                int part=(nums[i]+maxi-1)/maxi;
+                ans+=part-1;
+                maxi=nums[i]/part;
+            }
+            else{
+                maxi=nums[i];
+            }
+        }
          return ans;
     }
 };
