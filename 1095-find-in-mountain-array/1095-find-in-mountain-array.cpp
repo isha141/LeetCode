70cc8d9b8064dc6d009e5cfc4ee5c9492544a27f
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-13 18:13:02 +0530

    Time: 3 ms (78.73%), Space: 7.3 MB (44.88%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -10,42 +10,50 @@
 
 class Solution {
 public:
-   int findInMountainArray(int target, MountainArray A) {
-        int n = A.length(), l, r, m, peak = 0;
-        // find index of peak
-        l  = 0;
-        r = n - 1;
-        while (l < r) {
-            m = (l + r) / 2;
-            if (A.get(m) < A.get(m + 1))
-                l = peak = m + 1;
-            else
-                r = m;
-        }
-        // find target in the left of peak
-        l = 0;
-        r = peak;
-        while (l <= r) {
-            m = (l + r) / 2;
-            if (A.get(m) < target)
-                l = m + 1;
-            else if (A.get(m) > target)
-                r = m - 1;
-            else
-                return m;
-        }
-        // find target in the right of peak
-        l = peak;
-        r = n - 1;
-        while (l <= r) {
-            m = (l + r) / 2;
-            if (A.get(m) > target)
-                l = m + 1;
-            else if (A.get(m) < target)
-                r = m - 1;
-            else
-                return m;
-        }
-        return -1;
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+          int n=mountainArr.length();
+         int low=0;
+          int high=n-2;
+         int peak=0;
+         while(low<=high){
+             int mid=(low+high)>>1;
+             if(mountainArr.get(mid)<mountainArr.get(mid+1)){
+                 peak=mid;
+                 low=mid+1;
+             }
+             else{
+                 high=mid-1;
+                 
+             }
+         }
+          low=0;
+          high=peak; 
+          
+          while(low<=high){
+              int mid=(low+high)>>1;
+              // cout<<mid<<"--"<<endl;
+              if(mountainArr.get(mid)==target)
+                    return mid;
+              if(mountainArr.get(mid)>target){
+                  high=mid-1;
+              }
+              else{
+                  low=mid+1;
+              }
+          } 
+        low=peak+1;
+        high=n-1;
+          while(low<=high){
+              int mid=(low+high)>>1;
+              if(mountainArr.get(mid)==target)
+                    return mid;
+              if(mountainArr.get(mid)>target){
+                   low=mid+1;
+              }
+              else{
+                    high=mid-1;
+              }
+          }
+          return -1;
     }
 };
