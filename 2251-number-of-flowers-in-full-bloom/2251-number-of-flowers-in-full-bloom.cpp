2330651c950641726d3a10956b6a8bf022cb2ffd
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-14 01:06:39 +0530

    Time: 356 ms (6.37%), Space: 91.9 MB (14.09%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
-          int n=flowers.size();
-          int m=people.size();
-          vector<int>ans;
-          vector<int>p=people;
-          sort(flowers.begin(),flowers.end());
-          sort(people.begin(),people.end());     
-          map<int,int>mp;
-          priority_queue<int,vector<int>,greater<int>>pq;
-          int j=0;
-          for(int i=0;i<m;i++){
-               while(j<n && flowers[j][0]<=people[i]){
-                   pq.push(flowers[j][1]);
-                   j++;
-               }
-              while(!pq.empty() && pq.top()<people[i]){
-                  pq.pop();
-              }
-              mp[people[i]]=pq.size();
-          }
-          for(auto i=0;i<m;++i){
-              ans.push_back(mp[p[i]]);
-          }
+        vector<int>ans;
+        vector<int>pp(people.begin(),people.end());
+        sort(people.begin(),people.end());
+        sort(flowers.begin(),flowers.end());
+        int i=0;
+        int n=flowers.size();
+        int m=people.size();
+        map<int,int>mp;
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int j=0;j<m;++j){
+            while(i<n && flowers[i][0]<=people[j]){
+                pq.push({flowers[i][1]});
+                i++;
+            }
+            while(!pq.empty() && pq.top()<people[j])
+                pq.pop();
+            mp[people[j]]=pq.size();
+        }
+        for(auto i=0;i<m;++i){
+            ans.push_back(mp[pp[i]]);
+        }
          return ans;
     }
 };
