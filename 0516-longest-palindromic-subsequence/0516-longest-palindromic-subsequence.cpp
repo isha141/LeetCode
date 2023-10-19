82238e12159b98b56be0227a58b6ae26eb135d81
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-20 02:07:10 +0530

    Time: 188 ms (7.08%), Space: 10.9 MB (79.99%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,25 +1,24 @@
-int dp[1003][1003];
-class Solution { 
-    private:
+int dp[1001][1001];
+class Solution {
     int n;
     int solve(int i,int j,string &a,string &b){
         if(i>=n || j>=n) 
             return 0;
-        int  take=0;
-        int not_take=0; 
-        if(dp[i][j]!=-1) return dp[i][j];
+        int take=0;
+        int not_take=0;
+        if(dp[i][j]!=-1)
+              return dp[i][j];
         if(a[i]==b[j])
-              take=1+solve(i+1,j+1,a,b);
-        else
-            not_take=max(solve(i+1,j,a,b),solve(i,j+1,a,b));
+            take=1+solve(i+1,j+1,a,b);
+        not_take=max(solve(i+1,j,a,b),solve(i,j+1,a,b));
         return dp[i][j]=max(take,not_take);
     }
 public:
     int longestPalindromeSubseq(string s) {
-         n=s.size(); 
-        string a=s;
-        reverse(a.begin(),a.end()); 
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,a,s);
+          n=s.size();
+          string a=s;
+          reverse(a.begin(),a.end());
+          memset(dp,-1,sizeof(dp));
+          return solve(0,0,a,s);
     }
 };
