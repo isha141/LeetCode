a7ff5b1ed19244cf4f1ed3c580238ab998ed399d
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-20 02:18:24 +0530

    Time: 23 ms (23.78%), Space: 7.6 MB (84.31%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,20 +1,20 @@
-int dp[504][504];
+int dp[502][502];
 class Solution {
     private:
-    int n,m;
+    int n;
+    int m;
     int solve(int i,int j,string &a,string &b){
-          if(i<0 && j<0)
-                return 0;
-          if(i<0) return j+1;
-          if(j<0) return i+1;
-         if(dp[i][j]!=-1)
-               return dp[i][j];
-         int take=INT_MAX;
-         int not_take=INT_MAX;
-         if(a[i]==b[j])
-          take=solve(i-1,j-1,a,b);
-        not_take=1+min(solve(i-1,j,a,b),min(solve(i,j-1,a,b),solve(i-1,j-1,a,b)));
-         return dp[i][j]=min(take,not_take);
+        if(i<0)
+              return j+1;
+        if(j<0) return i+1;
+        int take=INT_MAX;
+        int not_take=INT_MAX;
+        if(dp[i][j]!=-1)
+              return dp[i][j];
+        if(a[i]==b[j])
+            take=solve(i-1,j-1,a,b);
+         not_take=1+min(solve(i-1,j,a,b),min(solve(i,j-1,a,b),solve(i-1,j-1,a,b)));
+        return dp[i][j]=min(take,not_take);
     }
 public:
     int minDistance(string a, string b) {
