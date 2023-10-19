0a342ec966f53869687ecef84b4d11b91ffbaa41
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-20 02:10:55 +0530

    Time: 262 ms (5.05%), Space: 73.4 MB (13.35%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -18,7 +18,17 @@ public:
           n=s.size();
           string a=s;
           reverse(a.begin(),a.end());
-          memset(dp,-1,sizeof(dp));
-          return solve(0,0,a,s);
+          vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+          for(int i=1;i<=n;++i){
+              for(int j=1;j<=n;++j){
+                  int take=0;
+                  if(s[i-1]==a[j-1]){
+                      take=1+dp[i-1][j-1];
+                  }
+                  int not_take=max(dp[i-1][j],dp[i][j-1]);
+                  dp[i][j]=max(take,not_take);
+              }
+         }
+         return dp[n][n];
     }
 };
