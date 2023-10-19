40166a5f0bcfc9f5684369b4c52875f3c0476215
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-20 03:08:10 +0530

    Time: 20 ms (38.32%), Space: 14.7 MB (24.75%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -24,7 +24,19 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         n=prices.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,prices,k,0);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        for(int i=n-1;i>=0;--i){
+            for(int buy=0;buy<=1;++buy){
+                for(int j=1;j<=k;++j){
+                    if(buy==0){
+                        dp[i][buy][j]=max(-prices[i]+dp[i+1][1][j],dp[i+1][0][j]);
+                    }
+                    else{
+                        dp[i][buy][j]=max(prices[i]+dp[i+1][0][j-1],dp[i+1][1][j]);
+                    }
+                }
+            }
+        }
+        return dp[0][0][k];
     }
 };
