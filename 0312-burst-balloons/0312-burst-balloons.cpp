f8fb590718bd41c1dc225a29d96a6d7880ff2f7a
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-20 16:18:05 +0530

    Time: 220 ms (90.20%), Space: 8.6 MB (89.54%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,20 +1,24 @@
+int dp[301][301];
 class Solution {
+    private:
+    int solve(int i,int j,vector<int>&nums){
+        if(i>j) return 0;
+        int ans=0;
+        int maxi=0;
+        if(dp[i][j]!=-1)
+             return dp[i][j];
+        for(int k=i;k<=j;++k){
+            ans=nums[i-1]*nums[k]*nums[j+1]+solve(i,k-1,nums)+solve(k+1,j,nums);
+            maxi=max(maxi,ans);
+        }
+        return dp[i][j]=maxi;
+    }
 public:
     int maxCoins(vector<int>& nums) {
         int n=nums.size();
-         nums.push_back(1);
+        nums.push_back(1);
         nums.insert(nums.begin(),1);
-         vector<vector<int>>dp(n+2,vector<int>(n+3,0));
-         for(int i=n;i>=1;--i){
-             for(int j=1;j<=n;++j){
-                  if(i>j) continue;
-                 int maxi=INT_MIN;
-                 for(int k=i;k<=j;++k){
-                     maxi=max(maxi,nums[k]*nums[i-1]*nums[j+1]+dp[i][k-1]+dp[k+1][j]);
-                 }
-                 dp[i][j]=maxi;
-             }
-         }
-        return dp[1][n];
+        memset(dp,-1,sizeof(dp));
+        return solve(1,n,nums);
     }
 };
