da670fed49a527caceb83fc7eeee8bcb33f36961
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-21 00:13:21 +0530

    Time: 7 ms (45.08%), Space: 13.6 MB (34.62%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,50 +1,55 @@
 class Solution { 
-    private:  
+    private:
+    int dx[4]={-1,1,0,0};
+    int dy[4]={0,0,1,-1};
+    int c=0;
     int n,m;
-    int dx[4]={1,-1,0,0};
-    int dy[4]={0,0,-1,1};
-    int solve(queue<pair<int,int>>&q,vector<vector<int>>& grid){
-        int c=0;
-        while(!q.empty()){
-            int t=q.size();
-            while(t--){
-                auto itr=q.front();
-                q.pop();
-                int x=itr.first;
-                int y=itr.second; 
-                grid[x][y]=0; 
-                for(int k=0;k<4;++k){
-                    int nx=dx[k]+x;
-                    int ny=dy[k]+y;
-                    if(nx>=0 && ny>=0 && nx<n  && ny<m && grid[nx][ny]!=0){
-                        q.push({nx,ny});
+    void solve(vector<vector<int>>&grid,queue<pair<int,int>>&q){
+          // queue<pair<int,int>>q;
+          // q.push({i,j});
+          while(!q.empty()){
+              int t=q.size();
+              bool flag=0;
+              while(t--){
+                  auto itr=q.front();
+                  q.pop();
+                  int x=itr.first;
+                  int y=itr.second;
+                  grid[x][y]=0;
+                  for(int k=0;k<4;++k){
+                      int nx=dx[k]+x;
+                      int ny=dy[k]+y;
+                      if(nx>=0 && nx<n && ny>=0 && ny<m && grid[nx][ny]==1){
                           grid[nx][ny]=0;
-                    }
-                }
-            }
-            if(q.size())
-            c++;
-        }
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                if(grid[i][j]==1) return -1;
-            }
-        }
-        return c;
+                          flag=1;
+                          q.push({nx,ny});
+                      }
+                  }
+              }
+              if(flag)
+              c+=1;
+          }
     }
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-         n=grid.size();
-         m=grid[0].size();
-        queue<pair<int,int>>q;
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                if(grid[i][j]==2){
+          n=grid.size();
+          m=grid[0].size();
+          queue<pair<int,int>>q;
+         for(int i=0;i<n;++i){
+             for(int j=0;j<m;++j){
+                 if(grid[i][j]==2){
                     q.push({i,j});
-                    grid[i][j]=0;
-                }
-            }
-        }
-        return solve(q,grid);
+                 }
+             }
+         }
+         solve(grid,q);
+         for(int i=0;i<n;++i){
+             for(int j=0;j<m;++j){
+                 if(grid[i][j]==1){
+                     return -1;
+                 }
+             }
+         }
+        return max(0,c);
     }
 };
