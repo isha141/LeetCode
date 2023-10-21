dbf49739568b2d0e4a823b9427b214cdeb0ef594
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-21 22:52:22 +0530

    Time: 384 ms (42.88%), Space: 16.1 MB (52.60%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,48 +1,35 @@
 class Solution {
-    private:
-    bool ok(string &a,string &b){
-        int c=0;
-        int n=a.size();
-        for(int i=0;i<n;++i){
-            if(a[i]!=b[i])
-                  c+=1;
-        }
-        return (c==1);
-    }
 public:
     int ladderLength(string begin, string end, vector<string>& word) {
-          set<string>s;
-          for(auto it: word){
-              s.insert(it);
-          }
-          if(s.find(end)==s.end())
-                return 0;
-          queue<string>q;
-          q.push(begin);
-          int ans=1;
-          while(!q.empty()){
-              int t=q.size();
-              while(t--){
-                  auto itr=q.front();
-                  q.pop();
-                  for(auto i=0;i<itr.size();++i){
-                      string temp=itr;
-                      for(char ch='a';ch<='z';++ch){
-                          temp[i]=ch;
-                          if(s.find(temp)==s.end())
-                              continue;
-                          if(temp==end){
-                               return ans+1;
-                          }
-                          if(s.find(temp)!=s.end()){
-                              s.erase(temp);
-                              q.push(temp);
-                          }
-                      }
-                  }
-              }
-              ans+=1;
-          }
-        return 0;
+        set<string>s;
+        for(auto itr: word){
+            s.insert(itr);
+        }
+        if(s.find(end)==s.end())
+              return 0;
+        queue<string>q;
+        q.push(begin);
+        int ans=0;
+        while(q.size()){
+            int t=q.size();
+            while(t--){
+                auto itr=q.front();
+                q.pop();
+                if(itr==end)
+                     return ans+1;
+                for(int i=0;i<itr.size();++i){
+                    string temp=itr;
+                    for(char ch='a';ch<='z';++ch){
+                        temp[i]=ch;
+                        if(s.find(temp)!=s.end()){
+                            q.push(temp);
+                            s.erase(temp);
+                        }
+                    }
+                }
+            }
+            ans+=1;
+        }
+         return 0;
     }
 };
