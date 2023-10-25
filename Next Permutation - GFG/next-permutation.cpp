9a9fff410d349067fef84f723950566ec83fb0f7
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-25 19:10:11 +0530

    Added solution - LeetHub

---
+++ Next Permutation - GFG/next-permutation.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> nextPermutation(int n, vector<int> arr){
+        // code here
+         vector<int>ans;
+         int first=-1;
+         int second=-1;
+         for(int i=n-2;i>=0;--i){
+             if(arr[i]<arr[i+1]){
+                 first=i;
+                 break;
+             }
+         }
+         if(first==-1){
+          ans=arr;
+          sort(ans.begin(),ans.end());
+            return ans;
+         }
+         for(int  i=n-1;i>=0;--i){
+             if(arr[i]>arr[first]){
+                 second=i;
+                 break;
+             }
+         }
+         swap(arr[first],arr[second]);
+        //  cout<<first<<"-->"<<second<<endl;
+        //  for(int i=0;i<n;++i)
+        //  cout<<arr[i]<<"-=>";
+         sort(arr.begin()+first+1,arr.end());
+         return arr;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        vector<int> ans = ob.nextPermutation(N, arr);
+        for(int u: ans)
+            cout<<u<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
