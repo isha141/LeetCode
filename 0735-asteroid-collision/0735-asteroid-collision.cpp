b9bc01f74dab445d1a1bc0e17fce9156dd4590cb
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-26 00:22:35 +0530

    Time: 11 ms (55.01%), Space: 18.5 MB (8.54%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& as) {
-           vector<int>ans;
-            int n=as.size();
-           stack<pair<int ,char>>s;
-             for(int i=0;i<n;++i){
-                 int temp=as[i];
-                 char ch='R';
-                 if(temp<0){
-                     ch='L';
-                 }
-                 bool flag=0;
-           while(!s.empty() && s.top().second!=ch && ch=='L'){
-               auto itr=s.top();
-                s.pop();
-               int val=itr.first;
-               if(abs(val)==abs(temp)){
-                   flag=1;
-                   break;
-               }
-               if(abs(val)>abs(temp)){ 
-                   s.push({val,itr.second});
-                   flag=1;
-                   break;
-               }
-           } 
-          if(!flag){
-              s.push({temp,ch});
+    vector<int> asteroidCollision(vector<int>& aster) {
+          vector<int>ans;
+          int n=aster.size();
+          stack<pair<char,int>>s;
+          for(int i=0;i<n;++i){
+              char ch='L';
+              if(aster[i]>0){
+                  ch='R';
+              }
+              while(!s.empty() && s.top().first!=ch && s.top().second<abs(aster[i]) && ch=='L'){
+                  s.pop();
+              }
+               bool flag=0;
+              if(s.size()){
+                  if(s.top().first!=ch && ch=='L'){
+                      if(s.top().second==abs(aster[i])){
+                          flag=1;
+                          s.pop();
+                      }
+                      else if(s.top().second>abs(aster[i])){
+                          flag=1;
+                      }
+                  }
+              }
+              if(flag==0){
+                  s.push({ch,(aster[i])});
+              }
           }
-        } 
-        while(!s.empty()){
-            ans.push_back(s.top().first);
-             s.pop();
-        } 
-        reverse(ans.begin(),ans.end());
-         return ans;
+          while(!s.empty()){
+              auto itr=s.top();
+              s.pop();
+              ans.push_back(itr.second);
+          }
+          reverse(ans.begin(),ans.end());
+          return ans;
     }
 };
