b9acac81ea121d6cfad7336ddbd3ba62d836f597
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-26 23:26:19 +0530

    Time: 4 ms (75.91%), Space: 9.2 MB (47.83%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,44 +1,21 @@
-int dp[203][204];
-class Solution { 
-    private: 
-    int n,m;
-     int solve(int i,int j,vector<vector<int>>&tri){
-         if(i>=n)
-             return 0;
-         if(dp[i][j]!=-1){
-             return dp[i][j];
-         }
-         int left=tri[i][j]+solve(i+1,j,tri);
-         int down=tri[i][j]+solve(i+1,j+1,tri);
-         return dp[i][j]=min(left,down);
-     }
+int dp[201][201];
+class Solution {
+    private:
+    int n;
+    int solve(int i,int col,vector<vector<int>>&tri){
+        if(i>=n)
+              return 0;
+        if(dp[i][col]!=-1)
+              return dp[i][col];
+        int take=0;
+        int not_take=0;
+        take=tri[i][col]+min(solve(i+1,col+1,tri),solve(i+1,col,tri));
+        return dp[i][col]=take;
+    }
 public:
     int minimumTotal(vector<vector<int>>& tri) {
-             n=tri.size();
-             m=tri[0].size(); 
-             vector<vector<int>>dp(n,vector<int>(204,1e9));
-             dp[0][0]=tri[0][0];
-             for(int i=1;i<n;++i){
-                 for(int j=0;j<tri[i].size();++j){
-                     int temp=tri[i][j];
-                     if(j<tri[i-1].size()){
-                         temp+=dp[i-1][j];
-                     } 
-                     else temp=INT_MAX;
-                     int down=tri[i][j];
-                     if(j>0){
-                         down+=dp[i-1][j-1];
-                     } 
-                     else
-                         down=INT_MAX;
-                     dp[i][j]=min(temp,down);
-                 } 
-             } 
-        int ans=INT_MAX;
-           for(int j=0;j<tri[n-1].size();++j){
-              ans=min(ans,dp[n-1][j]);
-           }
-        return ans;
-        
+        n=tri.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,tri);
     }
 };
