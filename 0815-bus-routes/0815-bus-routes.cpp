fcda882c604a5caf9a350f1f5c3806c148613f68
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-11-21 15:02:28 +0530

    Time: 1310 ms (5.31%), Space: 275.4 MB (5.05%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.cpp
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    int numBusesToDestination(vector<vector<int>>& r, int source, int target) {
-        int n=r.size();
-        vector<int>adj[100002]; 
-        for(int i=0;i<r.size();++i){
-             vector<int>ds=r[i];
-             for(auto itr: ds){
-                 adj[itr].push_back(i);
-             }
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        vector<int>adj[1000002];
+        for(auto i=0;i<routes.size();++i){
+            for(auto itr: routes[i]){
+                adj[itr].push_back(i);
+            }
         }
+         if(source==target) return 0;
         if(adj[source].size()==0) return -1;
         if(adj[target].size()==0) return -1;
         queue<int>q;
         q.push(source);
-        int ans=0;
-        vector<int>dis(100002,INT_MAX);
+        vector<int>dis(1000002,INT_MAX);
         dis[source]=0;
+        int ans=0;
         while(!q.empty()){
-             int t=q.size();
-             while(t--){
-               auto itr=q.front();
-                 q.pop();
-                 if(itr==target)
+            int t=q.size();
+            while(t--){
+                auto itr=q.front();
+                q.pop();
+                if(itr==target)
                       return ans;
-                 for(auto iit: adj[itr]){
-                     for(int j=0;j<r[iit].size();++j){
-                         if(dis[r[iit][j]]>dis[itr]+1){
-                             dis[r[iit][j]]=dis[itr]+1;
-                             q.push(r[iit][j]);
-                         }
-                     }
-                 }
-             }
+                for(auto it: adj[itr]){
+                    for(int j=0;j<routes[it].size();++j){
+                        if(dis[routes[it][j]]>dis[itr]+1){
+                            q.push(routes[it][j]);
+                            dis[routes[it][j]]=dis[itr]+1;
+                        }
+                    }
+                }
+            }
             ans+=1;
         }
-        return -1;
+         return -1;
     }
 };
